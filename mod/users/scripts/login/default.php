b215790fc70cc3b8ebb112156dad16809026cdd5
Matt McNaney <matt@tux.appstate.edu>
2003-08-04 20:29:16 +0000

    default login script

---
+++ mod/users/scripts/login/default.php
@@ -0,0 +1,13 @@
+<?php
+
+$DB = new PHPWS_DB("mod_users");
+$DB->addWhere("username", strtolower($username));
+$DB->addWhere("password", md5($password));
+$DB->addColumn("id");
+$ID = $DB->select("one");
+if (!isset($ID))
+     $logged = FALSE;
+     else
+     $logged = TRUE;
+
+?>
