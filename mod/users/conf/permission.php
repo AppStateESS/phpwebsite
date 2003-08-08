c5b9a57b40ad0a7b8211e04f9983b7cedfdcd8c4
Matt McNaney <matt@tux.appstate.edu>
2003-08-08 19:41:06 +0000

    new permission file

---
+++ mod/users/conf/permission.php
@@ -0,0 +1,11 @@
+<?php
+
+$permissions['user']["add_edit_users"]   = "Add / Edit Users";
+$permissions['user']["delete_users"]     = "Delete Users";
+$permissions['user']["add_edit_groups"]  = "Add / Edit Groups";
+$permissions['user']["delete_groups"]   = "Delete Groups";
+$permissions['user']["edit_permissions"] = "Edit Permissions";
+
+
+$itemPermissions['user'] = TRUE;
+?>
