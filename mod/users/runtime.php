d786712902faec6c633388dd3e1cdf92efad404d
Matt McNaney <matt@tux.appstate.edu>
2003-08-11 12:37:28 +0000

    changed login for the anonymous user

--- mod/users/runtime.php
+++ mod/users/runtime.php
@@ -1,7 +1,8 @@
 <?php
 
-if (!isset($_SESSION['User'])) $_SESSION['User'] = new PHPWS_User;
+if (!isset($_SESSION['User']))
+     PHPWS_User::logAnonymous();
 
-if (!PHPWS_Layout::get("CNT_user_small")) $_SESSION['User']->getLogin();
+if (!PHPWS_Layout::get("CNT_user_small")) PHPWS_User::getLogin();
 
 ?>
