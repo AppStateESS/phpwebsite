1331bf45fec67f0e2d3acdc4c9d3782d6ba122e8
Matt McNaney <matt@tux.appstate.edu>
2003-08-13 20:50:43 +0000

    alteration of itemname for users

--- mod/users/conf/permission.php
+++ mod/users/conf/permission.php
@@ -1,11 +1,10 @@
 <?php
 
-$permissions['user']["add_edit_users"]   = "Add / Edit Users";
-$permissions['user']["delete_users"]     = "Delete Users";
-$permissions['user']["add_edit_groups"]  = "Add / Edit Groups";
-$permissions['user']["delete_groups"]   = "Delete Groups";
-$permissions['user']["edit_permissions"] = "Edit Permissions";
+$permissions['users']["add_edit_users"]   = "Add / Edit Users";
+$permissions['users']["delete_users"]     = "Delete Users";
+$permissions['users']["add_edit_groups"]  = "Add / Edit Groups";
+$permissions['users']["delete_groups"]   = "Delete Groups";
+$permissions['users']["edit_permissions"] = "Edit Permissions";
 
-
-$itemPermissions['user'] = TRUE;
+$itemPermissions['users'] = TRUE;
 ?>
