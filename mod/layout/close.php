5c95eb6a1e1b46b66ab3f4a1ca4f215a34010701
Matt McNaney <matt@tux.appstate.edu>
2003-08-18 18:00:21 +0000

    dumped the junk

--- mod/layout/close.php
+++ mod/layout/close.php
@@ -1,8 +1,3 @@
 <?php
-
 echo PHPWS_Layout::display();
-//echo phpws_debug::testarray($_SESSION['Layout']);
-//echo phpws_debug::testarray($_SESSION['Layout_Boxes']);
-
-//PHPWS_Core::killAllSessions();
 ?>
