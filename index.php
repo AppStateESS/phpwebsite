05a4ef1ade2b85327b0e8cce871b2c0046032690
Matt McNaney <matt@tux.appstate.edu>
2003-10-16 15:15:31 +0000

    Because of config changes, could remove excess code

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <?php
 
+require_once "config/core/config.php";
+
 /* Show all errors */
 error_reporting (E_ALL);
 
 ob_start();
-/* Determine if this is a hub or branch site and load the
- * appropiate config file. $hub_dir will be included if
- * coming from a branch site
- */
-
-if (!isset($hub_dir)){
-     $branchName = $hub_dir = NULL;
-}
-loadConfig($hub_dir);
 
 /* Security against those with register globals = on */
 if (ini_get('register_globals')){
@@ -20,9 +13,6 @@ if (ini_get('register_globals')){
     unset($requestVarName);
   unset($nullIT);
 }
-
-/* Loads Pear config file */
-include PHPWS_SOURCE_DIR . "conf/pear_config.php";
 
 /* Initialize core defines */
 require_once PHPWS_SOURCE_DIR . "class/Init.php";
@@ -37,22 +27,5 @@ PHPWS_Core::report();
 
 if (isset($_REQUEST['reset']))
      PHPWS_Core::killAllSessions();
-
-/**
- * loads the config file
- */
-function loadConfig($hub_dir=NULL){
-  /* Check for config file and define source directory. */
-  if(is_file($hub_dir . "conf/config.php")){
-    include($hub_dir . "conf/config.php");
-    define("PHPWS_SOURCE_DIR", $source_dir);
-    define("CONFIG_FILE", $source_dir . "conf/config.php");
-    define("SESSION_NAME", md5($site_hash));
-  }
-  else {
-    header("location:setup/set_config.php");
-    exit();
-  }
-}
 
 ?>
