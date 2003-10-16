9593f402d2d79e7c5d9bd4afbf8703b73028160a
Matt McNaney <matt@tux.appstate.edu>
2003-10-16 19:48:21 +0000

    added and removed some errors

--- config/core/error.php
+++ config/core/error.php
@@ -3,19 +3,20 @@ translate("core");
 // Generic
 $errors[PHPWS_UNKNOWN]               = _('Unknown Error');
 $errors[PHPWS_FILE_NOT_FOUND]        = _('File not found');
-$errors[PHPWS_CLASS_NOT_CONSTRUCTED] = _('Tried to access a class that is not constructed.');
+$errors[PHPWS_CLASS_NOT_EXIST]       = _('Class does not exist');
 
 // Database.php 100 - 199
 $errors[PHPWS_DB_ERROR_TABLE]        = _('Table name not set');
-$errors[PHPWS_DB_NO_CLASS]           = _('Missing or non-existant class name');
 $errors[PHPWS_DB_NO_VALUES]          = _('No values were set before the query');
 
-// Manager.php 200 - 299
-$errors[MANAGER_ERR_MODULE_NOT_SET]  = _('Manager module not set');
-$errors[MANAGER_ERR_TABLE_NOT_SET]   = _('Manager table name not set');
-$errors[MANAGER_ERR_CLASS_NOT_SET]   = _('Include set without class');
-$errors[MANAGER_INC_FILE_NOT_FOUND]  = _('Include set but not found');
-$errors[MANAGER_CLASS_NON_EXISTS]    = _('Class could not be created');
+// List.php 200 - 299
+$errors[PHPWS_LIST_MOD_NOT_SET]      = _("Module not set");
+$errors[PHPWS_LIST_CLASS_NOT_SET]    = _("Class not set");
+$errors[PHPWS_LIST_TABLE_NOT_SET]    = _("Table not set");
+$errors[PHPWS_LIST_COLUMNS_NOT_SET]  = _("Columns not set");
+$errors[PHPWS_LIST_NAME_NOT_SET]     = _("Name not set");
+$errors[PHPWS_LIST_OP_NOT_SET]       = _("Op not set");
+$errors[PHPWS_LIST_NO_ITEMS_PASSED]  = _("No items passed");
 
 
 ?>
