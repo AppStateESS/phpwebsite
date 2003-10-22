a0bbcb7e556e54e1cb41ac49fcc56bab58c09fcc
Matt McNaney <matt@tux.appstate.edu>
2003-10-22 20:38:35 +0000

    new error messages

--- mod/users/conf/error.php
+++ mod/users/conf/error.php
@@ -2,17 +2,19 @@
 translate("users");
 $errors = array(
 		USER_ERROR                => _("Unknown error"),
-		USER_ERR_DUP_USERNAME     => _("Duplicate user name"),
-		USER_ERR_DUP_GROUPNAME    => _("Duplicate group name"),
+		USER_ERR_DUP_USERNAME     => _("An user with this name already exists"),
+		USER_ERR_DUP_GROUPNAME    => _("A group with this name already exists"),
 		USER_ERR_PERM_TABLE       => _("Permission table name already exists"),
 		USER_ERR_PERM_MISS        => _("Permission table not found"),
 		USER_ERR_PERM_FILE        => _("Module's permission file is missing"),
 		USER_ERR_BAD_USERNAME     => _("Username is improperly formatted"),
 		USER_ERR_PASSWORD_MATCH   => _("Passwords do not match"),
-		USER_ERR_PASSWORD_LENGTH  => _("Password must be [var1] in length"),
+		USER_ERR_PASSWORD_LENGTH  => _print(_("Password must be at least [var1] characters in length"), array(PASSWORD_LENGTH)),
 		USER_ERR_PASSWORD_EASY    => _("Password is too easy to guess"),
 		USER_ERR_LABEL_NOT_FOUND  => _("Demographic label not found"),
-		USER_ERR_UNKNOWN_INPUT    => _("Unknown input type")
+		USER_ERR_UNKNOWN_INPUT    => _("Unknown input type"),
+		USER_ERR_NO_MODULE        => _("Module does not exist"),
+		USER_ERR_BAD_VAR          => _("Invalid variable name")
 		);
 
 ?>
