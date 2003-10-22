70af82fae2c6ec75922e06f08b60de8150d77f7c
Matt McNaney <matt@tux.appstate.edu>
2003-10-22 20:39:19 +0000

    all work moved out of index and into the classes

--- mod/users/index.php
+++ mod/users/index.php
@@ -1,40 +1,23 @@
 <?php
 
-$DB = new PHPWS_DB("user_permissions");
-if (!isset($_REQUEST['action'])){
-  return;
-}
+if (!isset($_REQUEST['action'])) return;
 
 if (!class_exists("PHPWS_User")){
      PHPWS_Error::log("PHPWS_CLASS_NOT_CONSTRUCTED", "core", NULL, "<b>Class:</b> PHPWS_Users");
      return;
 }
 
-PHPWS_Core::initModClass("users", "User_Functions.php");
+PHPWS_Core::initModClass("users", "Action.php");
+
 foreach ($_REQUEST['action'] as $area=>$command);
 
 switch ($area){
  case "user":
-   switch ($command){
-   case "loginBox":
-     if (!PHPWS_Core::isLastPost()){
-       if (!User_Functions::loginUser($_POST['block_username'], $_POST['block_password']))
-	 User_Functions::badLogin();
-     }
-     break;
-
-   case "logout":
-     PHPWS_Core::killAllSessions();
-     PHPWS_Core::home();
-     break;
-   } // End of open command switch
+   User_Action::userAction($command);
    break;
 
  case "admin":
-   if (!$_SESSION['User']->allow("users"))
-     PHPWS_User::disallow();
-   else 
-     User_Functions::adminAction($command);
+   User_Action::adminAction($command);
    break;
 }// End area switch
 
