1d44db1897e5c61525bb236a9b5f799d145d5b02
Matt McNaney <matt@tux.appstate.edu>
2003-10-31 20:24:17 +0000

    na

--- setup/preconfig.php
+++ setup/preconfig.php
@@ -1,14 +1,12 @@
 <?php
 
-define("PHPWS_SOURCE_DIR", "./");
-
-if (preg_match("/win32/i", $_SERVER['SERVER_SOFTWARE']))
+if (isset($_SERVER['windir']))
   ini_set("include_path", ".;.\\lib\\pear\\");
 else
   ini_set("include_path", ".:./lib/pear/");
 
 
-
+define("PHPWS_SOURCE_DIR", "./");
 define("PHPWS_LOG_DIRECTORY", "./logs/");
 define("LOG_PERMISSION", 644);
 define("LOG_TIME_FORMAT", "%X %x");
