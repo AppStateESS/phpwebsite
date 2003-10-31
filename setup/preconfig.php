11e2f5f20a17ebe4ff30734eea0e5aca29a61234
Matt McNaney <matt@tux.appstate.edu>
2003-10-31 20:02:21 +0000

    windows fix

--- setup/preconfig.php
+++ setup/preconfig.php
@@ -1,12 +1,14 @@
 <?php
 
-
-  if (preg_match("/win32/i", $_SERVER['SERVER_SOFTWARE']))
-    ini_set("include_path", ".;lib\\pear\\");
-  else
-    ini_set("include_path", ".:lib/pear/");
-
 define("PHPWS_SOURCE_DIR", "./");
+
+if (preg_match("/win32/i", $_SERVER['SERVER_SOFTWARE']))
+  ini_set("include_path", ".;".PHPWS_SOURCE_DIR."lib\\pear\\");
+else
+  ini_set("include_path", ".:" . PHPWS_SOURCE_DIR . "lib/pear/");
+
+
+
 define("PHPWS_LOG_DIRECTORY", "./logs/");
 define("LOG_PERMISSION", 644);
 define("LOG_TIME_FORMAT", "%X %x");
