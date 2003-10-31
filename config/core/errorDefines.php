c94549aed078a8063f6e095fa453b26fd5158596
Matt McNaney <matt@tux.appstate.edu>
2003-10-31 16:56:23 +0000

    bunch o files needed

---
+++ config/core/errorDefines.php
@@ -0,0 +1,53 @@
+<?php
+/* Error defines */
+
+/************* Generic  *****************/
+define("PHPWS_UNKNOWN",               -1);
+define("PHPWS_FILE_NOT_FOUND",        -2);
+define("PHPWS_CLASS_NOT_EXIST",       -3);
+define("PHPWS_DIR_NOT_WRITABLE",      -4);
+define("PHPWS_VAR_TYPE",              -5);
+define("PHPWS_STRICT_TEXT",           -6);
+define("PHPWS_INVALID_VALUE",         -7);
+
+
+/*********** Database.php ***************/
+
+define("PHPWS_DB_ERROR_TABLE",        -100);
+define("PHPWS_DB_NO_VALUES",          -101);
+
+/************* List.php *****************/
+define("PHPWS_LIST_MOD_NOT_SET",      -200);
+define("PHPWS_LIST_CLASS_NOT_SET",    -201);
+define("PHPWS_LIST_TABLE_NOT_SET",    -202);
+define("PHPWS_LIST_COLUMNS_NOT_SET",  -203);
+define("PHPWS_LIST_NAME_NOT_SET",     -204);
+define("PHPWS_LIST_OP_NOT_SET",       -205);
+define("PHPWS_LIST_NO_ITEMS_PASSED",  -206);
+
+/************* Form.php *****************/
+define("PHPWS_FORM_BAD_NAME",         -301);
+define("PHPWS_FORM_MISSING_NAME",     -302);
+define("PHPWS_FORM_WRONG_ELMT_TYPE",  -303);
+define("PHPWS_FORM_NAME_IN_USE",      -304);
+define("PHPWS_FORM_MISSING_TYPE",     -305);
+define("PHPWS_FORM_NO_ELEMENTS",      -306);
+define("PHPWS_FORM_NO_TEMPLATE",      -307);
+define("PHPWS_FORM_NO_FILE",          -308);
+define("PHPWS_FORM_IMG_TOO_BIG",      -309);
+define("PHPWS_FORM_WIDTH_TOO_BIG",    -310);
+define("PHPWS_FORM_HEIGHT_TOO_BIG",   -311);
+define("PHPWS_FORM_UNKNOWN_TYPE",     -312);
+define("PHPWS_FORM_INVALID_MATCH",    -313);
+
+define("PHPWS_FORM_ERROR_FILE_POST",  -320);
+
+
+/*************** Item.php *****************/
+define("PHPWS_ITEM_ID_TABLE",         -400);
+define("PHPWS_ITEM_NO_RESULT",        -401);
+
+
+/*************** Module.php ***************/
+define("PHPWS_NO_MOD_FOUND",          -500);   
+?>
---
+++ config/core/formConfig.php
@@ -0,0 +1,31 @@
+<?php
+
+/*************************** FORM ****************************/
+/* These are the default values for various form elements */
+// default number of rows in a textarea
+define("DFLT_ROWS", 5);
+
+//default number of cols in a textarea
+define("DFLT_COLS", 40);
+
+define("USE_DEFAULT_SIZES", FALSE);
+
+//default size of text field spaces
+define("DFLT_TEXT_SIZE", 40);
+
+// default max character limit of text fields
+define("DFLT_MAX_SIZE", 255);
+
+// default number of rows to show in a multiple select
+define("DFLT_MAX_SELECT", 4);
+
+// max width in pixels allowed for uploaded images
+define("MAX_IMAGE_WIDTH", 640);
+
+// max height in pixels allowed for uploaded images
+define("MAX_IMAGE_HEIGHT", 480);
+
+// max image size in kilobytes allowed for uploaded images
+define("MAX_IMAGE_SIZE", 80000);
+
+?>
