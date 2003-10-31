6d63431994eb82a67908d1cb8adeaeaa7eff2d05
Matt McNaney <matt@tux.appstate.edu>
2003-10-31 16:51:51 +0000

    moved actual logging off to Core

--- core/class/Error.php
+++ core/class/Error.php
@@ -42,25 +42,17 @@ class PHPWS_Error {
   }
 
   function log($value, $module=NULL, $funcName=NULL, $extraInfo=NULL){
+    if ((bool)PHPWS_LOG_ERRORS == FALSE)
+      return;
+
     if (!PEAR::isError($value)) 
       $error = &PHPWS_Error::get($value, $module, $funcName, $extraInfo);
     else
       $error = $value;
 
-    if (!is_writable(PHPWS_LOG_DIRECTORY))
-      exit("Unable to write to log directory");
-
-    if ((bool)PHPWS_LOG_ERRORS == FALSE)
-      return;
-
     $final = PHPWS_Error::printError($error);
 
-    $conf = array('mode' => 0600, 'timeFormat' => '%X %x');
-    $log  = &Log::singleton('file', PHPWS_LOG_DIRECTORY . 'error.log', 'Error', $conf, PEAR_LOG_NOTICE);
-
-    $log->log("$final", PEAR_LOG_NOTICE);
-
-    $log->close();
+    PHPWS_Core::log($final, "error.log", _("Error"));
   }
 
 
