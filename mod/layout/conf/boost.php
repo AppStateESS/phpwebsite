85e9d3848d7ebf49cab04ec527df95c9dccf059f
Matt McNaney <matt@tux.appstate.edu>
2003-11-03 21:43:43 +0000

    installation file

---
+++ mod/layout/conf/boost.php
@@ -0,0 +1,6 @@
+<?php
+$proper_name = "Layout";
+$version = "2.0";
+$import_sql = TRUE;
+
+?>
---
+++ mod/users/conf/boost.php
@@ -0,0 +1,7 @@
+<?php
+$proper_name = "Users";
+$version = "2.0";
+$register = TRUE;
+$import_sql = TRUE;
+
+?>
