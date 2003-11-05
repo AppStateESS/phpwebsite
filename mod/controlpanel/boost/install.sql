a0750ff9cf5d124c704e5baa0c51a6581fe315bd
Matt McNaney <matt@tux.appstate.edu>
2003-11-05 20:37:28 +0000

    had wrong tables saved

--- mod/controlpanel/boost/install.sql
+++ mod/controlpanel/boost/install.sql
@@ -1,37 +1,24 @@
-CREATE TABLE mod_controlpanel_link (
-  id int NOT NULL default '0',
-  owner varchar(20) default '',
-  editor varchar(20) default '',
-  ip text,
-  label text NOT NULL,
-  groups text,
-  created int NOT NULL default '0',
-  updated int NOT NULL default '0',
-  hidden smallint NOT NULL default '0',
-  approved smallint NOT NULL default '0',
-  module text NOT NULL,
-  url text NOT NULL,
-  description text,
-  admin smallint NOT NULL default '0',
-  image text,
-  PRIMARY KEY(id)
-);
 
+CREATE TABLE controlpanel_link (
+ id INT NOT NULL PRIMARY KEY,
+ tab CHAR(255) NOT NULL,
+ active SMALLINT NOT NULL,
+ itemname CHAR(50) NOT NULL,
+ restricted SMALLINT NOT NULL,
+ url TEXT,
+ description TEXT,
+ image CHAR(255),
+ link_order SMALLINT NOT NULL
+ );
 
-CREATE TABLE mod_controlpanel_tab (
+CREATE TABLE controlpanel_tab (
   id int NOT NULL default '0',
-  owner varchar(20) default '',
-  editor varchar(20) default '',
-  ip text,
-  label text NOT NULL,
-  groups text,
-  created int NOT NULL default '0',
-  updated int NOT NULL default '0',
-  hidden smallint NOT NULL default '0',
-  approved smallint NOT NULL default '0',
-  title varchar(255) default NULL,
-  links text,
-  grid int NOT NULL default '0',
-  taborder int NOT NULL default '0',
+  title CHAR(255) NOT NULL default '',
+  label CHAR(255) NOT NULL default '',
+  link CHAR(255) NOT NULL default '',
+  tab_order smallint NOT NULL default '0',
+  color CHAR(50) default NULL,
+  itemname CHAR(255) NOT NULL default '',
+  style CHAR(50) default NULL,
   PRIMARY KEY  (id)
 );
