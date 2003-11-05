275120ff1cd8fef85c93e033c5c61b8a16364376
Matt McNaney <matt@tux.appstate.edu>
2003-11-05 20:40:13 +0000

    file for adding links to control panel

---
+++ mod/users/conf/controlpanel.php
@@ -0,0 +1,12 @@
+<?php
+
+$links[] = array("label"       => _("User Administration"),
+		 "restricted"  => TRUE,
+		 "url"         => "index.php?module=users&action[admin]=main",
+		 "description" => _("Lets you create and edit users and groups."),
+		 "image"       => "users.png",
+		 "tab"         => "admin"
+		 );
+
+
+?>
