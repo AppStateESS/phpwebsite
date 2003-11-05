cbfafd0690e723bf4d01d7ba34bdad979cf16760
Matt McNaney <matt@tux.appstate.edu>
2003-11-05 20:36:48 +0000

    new error messages

--- mod/boost/conf/config.php
+++ mod/boost/conf/config.php
@@ -2,7 +2,8 @@
 
 /********** Error Codes ***************/
 define("BOOST_ERR_NOT_MODULE",     -1);
-
+define("BOOST_ERR_NO_INSTALLSQL",  -2);
+define("BOOST_NO_MODULES_SET",     -3);
 
 
 
--- mod/boost/conf/error.php
+++ mod/boost/conf/error.php
@@ -1,6 +1,6 @@
 <?php
-$errors[BOOST_ERR_NOT_MODULE]    = "Object sent to boost was not of the PHPWS_Module class.";
-$errors[BOOST_ERR_NO_INSTALLSQL] = "Unable to locate SQL import file.";
-
+$errors[BOOST_ERR_NOT_MODULE]    = _("Object sent to boost was not of the PHPWS_Module class.");
+$errors[BOOST_ERR_NO_INSTALLSQL] = _("Unable to locate SQL import file.");
+$errors[BOOST_NO_MODULES_SET]    = _("Modules have not been set.");
 
 ?>
