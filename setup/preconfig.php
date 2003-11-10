ce72c0f9c8228d841bac5b920b4e1f2b3b2bfcfd
Matt McNaney <matt@tux.appstate.edu>
2003-11-10 14:37:57 +0000

    better windows search

--- setup/preconfig.php
+++ setup/preconfig.php
@@ -1,10 +1,11 @@
 <?php
 
-if (isset($_SERVER['windir']) || preg_match("/microsoft/i", $_SERVER['SERVER_SOFTWARE']))
+if (isset($_SERVER['WINDIR']) || preg_match("/(microsoft|win32)/i", $_SERVER['SERVER_SOFTWARE'])){
   ini_set("include_path", ".;.\\lib\\pear\\");
-else
+}
+else {
   ini_set("include_path", ".:./lib/pear/");
-
+}
 
 define("PHPWS_SOURCE_DIR", "./");
 define("PHPWS_LOG_DIRECTORY", "./logs/");
