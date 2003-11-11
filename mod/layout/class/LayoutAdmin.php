72b84302df7e7e2e38b2744c52f08f30f3330210
Matt McNaney <matt@tux.appstate.edu>
2003-11-11 21:47:45 +0000

    added BASIC move box capability. More to come

--- mod/layout/class/LayoutAdmin.php
+++ mod/layout/class/LayoutAdmin.php
@@ -7,7 +7,6 @@ class Layout_Admin{
   function main(){
     $content = array();
     PHPWS_Core::initModClass("controlpanel", "Panel.php");
-    Layout::addStyle("layout");
 
     if (isset($_REQUEST['tab']))
       Layout_Admin::action($_REQUEST['tab'], $content);
@@ -17,10 +16,10 @@ class Layout_Admin{
       $currentTab = $panel->getCurrentTab();
 
       if(isset($currentTab))
-	$content = Layout_Admin::action($currentTab, $content);
+	Layout_Admin::action($currentTab, $content);
       else {
 	$panel->setCurrentTab(DEFAULT_LAYOUT_TAB);
-	$content = Layout_Admin::action(DEFAULT_LAYOUT_TAB, $content);
+	Layout_Admin::action(DEFAULT_LAYOUT_TAB, $content);
       }
     }
 
@@ -49,27 +48,74 @@ class Layout_Admin{
     case "meta":
 
       break;
+
+    case "moveBox":
+      $result = Layout_Admin::moveBox();
+      if ($result === TRUE)
+	exit(header("location:" . $_SERVER['HTTP_REFERER']));
+      break;
+
+    case "changeBoxSettings":
+      Layout_Admin::saveBoxSettings();
+      $message= _("Settings changed");
+      Layout_Admin::boxesForm($content, $message);
+      break;
       
     }
   }
 
+  function saveBoxSettings(){
+    if ($_REQUEST['move_boxes'] == 1)
+      $_SESSION['Move_Boxes'] = TRUE;
+    else
+      PHPWS_Core::killSession("Move_Boxes");
+  }
 
-  function boxesForm(&$content){
+  function boxesForm(&$content, $message = NULL){
     PHPWS_Core::initCoreClass("Form.php");
 
-
     $form = new PHPWS_Form("boxes");
+    $form->add("module", "hidden", "layout");
+    $form->add("action[admin]", "hidden", "changeBoxSettings");
     $form->add("move_boxes", "radio", array(0, 1));
+    if (isset($_SESSION['Move_Boxes']))
+      $form->setMatch("move_boxes", 1);
     $template = $form->getTemplate();
 
     $template['MOVE_BOX_LABEL'] = _("Adjust Site Layout");
     $template['MOVE_BOXES_ON']  = _("On");
     $template['MOVE_BOXES_OFF']  = _("Off");
-
+    $template['MESSAGE'] = $message;
     $content[] = PHPWS_Template::process($template, "layout", "BoxControl.tpl");
 
   }
 
+  function moveBoxesTag($box){
+    PHPWS_Core::initCoreClass("Form.php");
+
+    $themeVars = Layout::getThemeVariables();
+
+    $form = new PHPWS_Form;
+    $form->add("module", "hidden", "layout");
+    $form->add("action[admin]", "hidden", "moveBox");
+    $form->add("box_source", "hidden", $box['id']);
+    $form->add("box_dest", "select", $themeVars);
+    $form->reindexValue("box_dest");
+    $form->setMatch("box_dest", $box['theme_var']);
+    $form->add("move", "submit", _("Move"));
+
+    $template = $form->getTemplate();
+    return PHPWS_Template::process($template, "layout", "move_box_select.tpl");
+  }
+
+  function moveBox(){
+    PHPWS_Core::initModClass("layout", "Box.php");
+    $box = new Layout_Box($_POST['box_source']);
+    $box->setThemeVar(strtoupper($_POST['box_dest']));
+    $result = $box->save();
+    Layout::initLayout(TRUE);
+    return TRUE;
+  }
 }
 
 ?>
