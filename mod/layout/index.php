0b3eda5b60bf23b899c8c032e5d695351e37017b
Matt McNaney <matt@tux.appstate.edu>
2003-11-11 21:49:06 +0000

    More move box stuff

--- mod/layout/index.php
+++ mod/layout/index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 if ($_REQUEST['module'] != "layout" || !isset($_REQUEST['action'])){
   layoutDefault();
 }
@@ -11,11 +10,18 @@ foreach ($_REQUEST['action'] as $section => $action);
 
 switch ($section){
  case "admin":
+   Layout::addStyle("layout");
   switch ($action){
   case "main":
     Layout_Admin::main();
     break;
-  } // END action switch
+
+  default:
+    $content = array();
+    Layout_Admin::action($action, $content);
+    Layout_Admin::adminPanel(implode("", $content));
+    break;
+ } // END action switch
   break;
 } // END section switch
 
