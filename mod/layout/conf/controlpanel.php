89c3f4d0415ceff54bcb2e7782d223d50a1ad0f3
Matt McNaney <matt@tux.appstate.edu>
2003-11-11 16:22:11 +0000

    layout link

---
+++ mod/layout/conf/controlpanel.php
@@ -0,0 +1,10 @@
+<?php
+
+$links[] = array("label"       => _("Layout"),
+		 "restricted"  => TRUE,
+		 "url"         => "index.php?module=layout&action[admin]=main",
+		 "description" => _("Control the layout of your site."),
+		 "image"       => "layout.png",
+		 "tab"         => "admin"
+		 );
+?>
