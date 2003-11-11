dbce71796be002190c00450b1eda19627c4bb738
Matt McNaney <matt@tux.appstate.edu>
2003-11-11 16:22:39 +0000

    start of layout admin

--- mod/layout/index.php
+++ mod/layout/index.php
@@ -1,5 +1,26 @@
 <?php
 
 
+if ($_REQUEST['module'] != "layout" || !isset($_REQUEST['action'])){
+  layoutDefault();
+}
 
+PHPWS_Core::initModClass("layout", "LayoutAdmin.php");
+
+foreach ($_REQUEST['action'] as $section => $action);
+
+switch ($section){
+ case "admin":
+  switch ($action){
+  case "main":
+    Layout_Admin::main();
+    break;
+  } // END action switch
+  break;
+} // END section switch
+
+
+function layoutDefault(){
+  exit(header("location:./../../index.php"));
+}
 ?>
