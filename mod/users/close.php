4a7d9e9d84752a052f352f2b7793e0d3a84b6477
Matt McNaney <matt@tux.appstate.edu>
2003-11-24 18:40:44 +0000

    killing unused sessions

---
+++ mod/users/close.php
@@ -0,0 +1,12 @@
+<?php
+
+// Destroy unused sessions
+if (PHPWS_Core::getCurrentModule() != "users"){
+  PHPWS_Core::killSession('Member_Pager');
+  PHPWS_Core::killSession('All_Demo');
+  PHPWS_Core::killSession('User_Manager');
+  PHPWS_Core::killSession('Group_Manager');
+}
+
+
+?>
