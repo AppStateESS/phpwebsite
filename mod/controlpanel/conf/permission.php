d829f1f10e8145ba881d21d4c06c910040eb7939
Matt McNaney <matt@tux.appstate.edu>
2003-11-26 17:08:08 +0000

    although it isn't doing anything, need to add it for testing

---
+++ mod/controlpanel/conf/permission.php
@@ -0,0 +1,8 @@
+<?php
+
+$permissions['controlpanel']['remove_tab'] = _("Remove Tab");
+$permissions['controlpanel']['add_tab']    = _("Add Tab");
+$permissions['controlpanel']['move_link']  = _("Move Link");
+
+$itemPermissions = FALSE;
+?>
