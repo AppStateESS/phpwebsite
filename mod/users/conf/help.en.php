c4ae7c60697caa22f1bddfc67eb0b5ff2b50e6c0
Matt McNaney <matt@tux.appstate.edu>
2003-12-05 21:49:38 +0000

    testing help

---
+++ mod/users/conf/help.en.php
@@ -0,0 +1,8 @@
+<?php
+
+$default_user_group['TITLE'] = "Default User Group";
+$default_user_group['CONTENT'] = "" .
+"If set, all users will be automatically adopt the permissions of this group."; 
+
+
+?>
