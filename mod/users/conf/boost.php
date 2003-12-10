cff3cf4f3be8087c76e642acfe03705e7e67c357
Matt McNaney <matt@tux.appstate.edu>
2003-12-10 16:37:24 +0000

    few changes. users doesn't need a file directory

--- mod/users/conf/boost.php
+++ mod/users/conf/boost.php
@@ -1,7 +1,9 @@
 <?php
 $proper_name = "Users";
-$version = "2.0";
-$register = TRUE;
-$import_sql = TRUE;
+$version     = "2.0.1";
+$register    = TRUE;
+$import_sql  = TRUE;
+$image_dir   = TRUE;
+$file_dir    = FALSE;
 
 ?>
