b00721295d2c56aeb3bd4c69e66bc203d3615a8c
Matt McNaney <matt@tux.appstate.edu>
2003-12-10 16:28:26 +0000

    installation files for boost interface

---
+++ mod/boost/conf/boost.php
@@ -0,0 +1,9 @@
+<?php
+$proper_name = "Boost";
+$version = "1.6.0";
+$register = TRUE;
+$import_sql = FALSE;
+$image_dir = FALSE;
+$file_dir = FALSE;
+
+?>
---
+++ mod/boost/conf/controlpanel.php
@@ -0,0 +1,10 @@
+<?php
+
+$links[] = array("label"       => _("Boost"),
+		 "restricted"  => TRUE,
+		 "url"         => "index.php?module=boost&action[admin]=main",
+		 "description" => _("Boost allows the installation and upgrading of modules."),
+		 "image"       => "boost.png",
+		 "tab"         => "admin"
+		 );
+?>
