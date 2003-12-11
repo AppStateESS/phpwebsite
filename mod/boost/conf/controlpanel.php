5de5646a1663e9a064d12cb6f9fd47203521466d
Matt McNaney <matt@tux.appstate.edu>
2003-12-11 15:26:29 +0000

    corrected path

--- mod/boost/conf/controlpanel.php
+++ mod/boost/conf/controlpanel.php
@@ -2,7 +2,7 @@
 
 $links[] = array("label"       => _("Boost"),
 		 "restricted"  => TRUE,
-		 "url"         => "index.php?module=boost&action[admin]=main",
+		 "url"         => "index.php?module=boost&action=admin",
 		 "description" => _("Boost allows the installation and upgrading of modules."),
 		 "image"       => "boost.png",
 		 "tab"         => "admin"
