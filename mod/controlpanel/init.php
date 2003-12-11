fc00fb7733edcb4c9a0e49ee394eedea31a774ec
Matt McNaney <matt@tux.appstate.edu>
2003-12-11 15:35:31 +0000

    moved

--- mod/controlpanel/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-PHPWS_Core::initModClass("controlpanel", "ControlPanel.php");
-PHPWS_Core::initModClass("controlpanel", "Tab.php");
-PHPWS_Core::initModClass("controlpanel", "Link.php");
-
-?>
--- mod/layout/runtime.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-  Layout::initLayout();
-
-?>
