1be8923fac88f8b8c9460cbe75368d1539642dd1
Matt McNaney <matt@tux.appstate.edu>
2003-12-17 21:40:59 +0000

    added some new error messages

--- mod/controlpanel/conf/error.php
+++ mod/controlpanel/conf/error.php
@@ -1,6 +1,8 @@
 <?php
 
-$errors = array (CP_BAD_TABS=> _("Tabs must be in an array.")
+$errors = array (CP_BAD_TABS       => _("Tabs must be in an array."),
+		 CP_MISSING_TITLE  => _("Tab is missing a title.");
+		 CP_MISSING_LINK   => _("Tab is missing a link address.");
 		 );
 
 ?>
