aa6dbe1b5fe116395a7c3cd2cf3ec5a7a8956766
Matt McNaney <matt@tux.appstate.edu>
2003-12-17 20:49:30 +0000

    about view added, etc

--- mod/boost/index.php
+++ mod/boost/index.php
@@ -4,14 +4,28 @@ if (!isset($_REQUEST['action'])) return;
 
 $content = array();
 PHPWS_Core::initModClass("boost", "Form.php");
+PHPWS_Core::initModClass("controlpanel", "Panel.php");
+
+$boostPanel = & new PHPWS_Panel("boost");
+Boost_Form::setTabs($boostPanel);
 
 switch ($_REQUEST['action']){
  case "admin":
-   $content[] = Boost_Form::admin();
+   $content[] = Boost_Form::listModules(Boost_Form::boostTab($boostPanel));
+   break;
+
+ case "check":
+   PHPWS_Core::initModClass("boost", "Action.php");
+   $content[] = Boost_Action::checkupdate($_REQUEST['opmod']);
+   break;
+
+ case "aboutView":
+   PHPWS_Core::initModClass("boost", "Boost.php");
+   PHPWS_Boost::aboutView($_REQUEST['aboutmod']);
    break;
 }// End area switch
 
-$finalContent = implode("", $content);
-
+$boostPanel->setContent(implode("", $content));
+$finalContent = $boostPanel->display();
 Layout::add(PHPWS_ControlPanel::display($finalContent));
 ?>
