85047ba3c422511d4e94f5a8f4f7b73e357d064d
Matt McNaney <matt@tux.appstate.edu>
2003-12-17 21:51:54 +0000

    added version information. pointing at a test file

--- mod/users/conf/boost.php
+++ mod/users/conf/boost.php
@@ -1,9 +1,10 @@
 <?php
-$proper_name = "Users";
-$version     = "2.0.1";
-$register    = TRUE;
-$import_sql  = TRUE;
-$image_dir   = TRUE;
-$file_dir    = FALSE;
-
+$proper_name  = "Users";
+$version      = "2.0.1";
+$register     = TRUE;
+$import_sql   = TRUE;
+$image_dir    = TRUE;
+$file_dir     = FALSE;
+$version_http = "http://phpwebsite.appstate.edu/test/update.ini";
+$about        = TRUE;
 ?>
