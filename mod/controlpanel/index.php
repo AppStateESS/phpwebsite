8b341036be53a3a359c41466945049abab24a4b1
Matt McNaney <matt@tux.appstate.edu>
2004-04-26 21:18:18 +0000

    removed some code that wasn't getting used

--- mod/controlpanel/index.php
+++ mod/controlpanel/index.php
@@ -1,18 +1,15 @@
 <?php
 
-if (isset($_GET['active']))
-     PHPWS_ControlPanel::setCurrentTab("controlpanel", $_GET['active']);
-
-if(!isset($_SESSION['PHPWS_ControlPanel']))
-  $_SESSION['PHPWS_ControlPanel'] = new PHPWS_ControlPanel;
-
-if (isset($_GET['cp_image_toggle']))
+if (isset($_GET['cp_image_toggle'])){
   PHPWS_ControlPanel_Tab::toggleImage($_GET['tab']);
-
-if (isset($_GET['cp_desc_toggle']))
+}
+     
+if (isset($_GET['cp_desc_toggle'])){
   PHPWS_ControlPanel_Tab::toggleDesc($_GET['tab']);
-
-if ($_SESSION['User']->isLogged())
-     Layout::add(PHPWS_ControlPanel::display());
+}
+     
+if ($_SESSION['User']->isLogged()){
+  Layout::add(PHPWS_ControlPanel::display());
+}
 
 ?>
