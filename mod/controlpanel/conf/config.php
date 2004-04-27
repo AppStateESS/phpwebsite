7ef3a116befa6d231863155b28b738ef3931280c
Matt McNaney <matt@tux.appstate.edu>
2004-04-27 20:42:23 +0000

    new error messages

--- mod/controlpanel/conf/config.php
+++ mod/controlpanel/conf/config.php
@@ -4,6 +4,7 @@ define("CP_DEFAULT_TAB",   "tab/tab.tpl");
 
 define("CP_BAD_TABS",      -1);
 define("CP_MISSING_TITLE", -2);
-define("CP_MISSING_LINK", -3);
+define("CP_MISSING_LINK",  -3);
+define("CP_NO_TABS",       -4);
 
 ?>
--- mod/controlpanel/conf/error.php
+++ mod/controlpanel/conf/error.php
@@ -1,8 +1,9 @@
 <?php
 
 $errors = array (CP_BAD_TABS       => _("Tabs must be in an array."),
-		 CP_MISSING_TITLE  => _("Tab is missing a title.");
-		 CP_MISSING_LINK   => _("Tab is missing a link address.");
+		 CP_MISSING_TITLE  => _("Tab is missing a title."),
+		 CP_MISSING_LINK   => _("Tab is missing a link address."),
+		 CP_NO_TABS        => _("Unable to pull control panel tabs from database.")
 		 );
 
 ?>
