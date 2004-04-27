aca4f276e4312124f00d8506b322bd0e7d53492c
Matt McNaney <matt@tux.appstate.edu>
2004-04-27 20:43:22 +0000

    user's unregister file that removes permission tables on a modules uninstall

---
+++ mod/users/boost/unregister.php
@@ -0,0 +1,18 @@
+<?php
+
+function users_unregister($module, &$content){
+  PHPWS_Core::initModClass("users", "Permission.php");
+
+  $content[] = _("Removing permissions table.");
+  if (Users_Permission::removePermissions($module)){
+    $content[] = _("Permissions table removed successfully.");
+    return TRUE;
+  }
+  else {
+    $content[] = _("Permissions table not removed successfully.");
+    return FALSE;
+  }
+}
+
+
+?>
