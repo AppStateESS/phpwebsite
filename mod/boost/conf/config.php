82af78d6dca34e36710ead69ee85ad720c4fc74d
Matt McNaney <matt@tux.appstate.edu>
2004-04-27 20:40:04 +0000

    new error messages, although I'm not sure they are getting used

--- mod/boost/conf/config.php
+++ mod/boost/conf/config.php
@@ -1,9 +1,10 @@
 <?php
 
 /********** Error Codes ***************/
-define("BOOST_ERR_NOT_MODULE",     -1);
-define("BOOST_ERR_NO_INSTALLSQL",  -2);
-define("BOOST_NO_MODULES_SET",     -3);
+define("BOOST_ERR_NOT_MODULE",       -1);
+define("BOOST_ERR_NO_INSTALLSQL",    -2);
+define("BOOST_NO_MODULES_SET",       -3);
+define("BOOST_FAILED_PRE94_INSTALL", -4);
 
 
 
