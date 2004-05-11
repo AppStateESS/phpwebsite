b5e54dfe514ab142e8ffa8d399ed3b7426a15b12
Matt McNaney <matt@tux.appstate.edu>
2004-05-11 19:29:26 +0000

    Users was reporting an error, even if the module didn't have a permission table

--- mod/users/boost/register.php
+++ mod/users/boost/register.php
@@ -4,15 +4,22 @@ function users_register($module, &$content){
   PHPWS_Core::initModClass("users", "Permission.php");
 
   $content[] = _("Creating permissions table.");
-  if (Users_Permission::createPermissions($module)){
+  $result = Users_Permission::createPermissions($module);
+  
+  if (PEAR::isError($result)){
+    if ($result->getCode() == USER_ERR_PERM_FILE){
+      $content[] = _("Permissions file not found.");
+      PHPWS_Boost::addLog("users", _("Permissions file not found."));
+    }
+    else {
+      $content[] = _("Permissions table not created successfully.");
+      PHPWS_Error::log($result);
+    }
+    return FALSE;
+  } else {
     $content[] = _("Permissions table created successfully.");
     return TRUE;
   }
-  else {
-    $content[] = _("Permissions table not created successfully.");
-    return FALSE;
-  }
-
 }
 
 ?>
