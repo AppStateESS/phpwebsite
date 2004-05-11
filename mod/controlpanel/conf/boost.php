6a9ee8d838842749ca244532861d3b49344014a1
Matt McNaney <matt@tux.appstate.edu>
2004-05-11 19:25:59 +0000

    tidy

--- mod/controlpanel/conf/boost.php
+++ mod/controlpanel/conf/boost.php
@@ -1,8 +1,9 @@
 <?php
 $proper_name = "Control Panel";
-$version = "2.0";
-$register = TRUE;
-$import_sql = TRUE;
-$image_dir = FALSE;
-$file_dir = FALSE;
+$version     = "2.0";
+$register    = TRUE;
+$unregister  = TRUE;
+$import_sql  = TRUE;
+$image_dir   = FALSE;
+$file_dir    = FALSE;
 ?>
