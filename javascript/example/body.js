32f78db7e102be82cbd4e93e453c95c06bd877f9
Matt McNaney <matt@tux.appstate.edu>
2004-05-14 19:00:45 +0000

    example javascript

---
+++ javascript/example/body.js
@@ -0,0 +1,1 @@
+<input type="button" value="Click Me" onClick="example(document.{FORM_NAME}.{TEXT_FIELD}.value)" />
---
+++ javascript/example/head.js
@@ -0,0 +1,1 @@
+<script language="javascript" src="javascript/example/example.js"></script>
