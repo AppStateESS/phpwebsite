f423123f91af334c144484d50f870e08067e0592
Matt McNaney <matt@tux.appstate.edu>
2004-05-19 21:19:25 +0000

    the default panel was pointed at the wrong file

--- mod/controlpanel/conf/config.php
+++ mod/controlpanel/conf/config.php
@@ -1,6 +1,5 @@
 <?php
-define("CP_DEFAULT_PANEL", "panel/panel.tpl");
-define("CP_DEFAULT_TAB",   "tab/tab.tpl");
+define("CP_DEFAULT_PANEL", "panel.tpl");
 
 define("CP_BAD_TABS",      -1);
 define("CP_MISSING_TITLE", -2);
