21d0606e6b6d7965d93e7dd5b815681ce5b9600b
Matt McNaney <matt@tux.appstate.edu>
2004-07-13 19:35:58 +0000

    changed addresses to user module

--- mod/users/conf/controlpanel.php
+++ mod/users/conf/controlpanel.php
@@ -2,7 +2,7 @@
 
 $link[] = array("label"       => _("User Administration"),
 		 "restricted"  => TRUE,
-		 "url"         => "index.php?module=users&action[admin]=main",
+		 "url"         => "index.php?module=users&amp;action=admin",
 		 "description" => _("Lets you create and edit users and groups."),
 		 "image"       => "users.png",
 		 "tab"         => "admin"
@@ -10,7 +10,7 @@ $link[] = array("label"       => _("User Administration"),
 
 $link[] = array("label"       => _("User Settings"),
 		 "restricted"  => FALSE,
-		 "url"         => "index.php?module=users&action[user]=main",
+		 "url"         => "index.php?module=users&action=user",
 		 "description" => _("Lets you edit your personal user information."),
 		 "image"       => "users.png",
 		 "tab"         => "my_settings"
