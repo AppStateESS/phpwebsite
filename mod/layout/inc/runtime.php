8a406bf5d834f90cb04f7862deafda3049a16fa5
Matt McNaney <matt@tux.appstate.edu>
2004-07-13 19:29:16 +0000

    removed test junk

--- mod/layout/inc/runtime.php
+++ mod/layout/inc/runtime.php
@@ -1,11 +1,1 @@
-<?php
-
-  Layout::initLayout();
-
-/*
-Layout::add(array("TITLE"=>"testing1", "CONTENT"=>"Text goes here"), "MyContent");
-Layout::add(array("TITLE"=>"testing2", "CONTENT"=>"More Text goes here"), "MyContent2");
-Layout::add(array("TITLE"=>"testing3", "CONTENT"=>"More Text goes here"), "MyContent3");
-Layout::add(array("TITLE"=>"testing4", "CONTENT"=>"More Text goes here"), "MyContent4");
-*/
-?>
+<?php Layout::initLayout(); ?>
