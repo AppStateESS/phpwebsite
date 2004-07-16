20e71edeeb9f0fd4a8748650816586e4e5303b92
Matt McNaney <matt@tux.appstate.edu>
2004-07-16 19:19:43 +0000

    javascript file for users

---
+++ mod/users/javascript/authorize.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+//<![CDATA[
+
+function drop(auth_id){
+   if (confirm('{DROP_Q}')){
+      alert("yes");
+   } else {
+      alert("no");
+   }
+}
+
+//]]>
+</script>
