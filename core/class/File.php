f6f3bd7cb91741db72c87ed5ec2ea42c0667eaf1
Matt McNaney <matt@tux.appstate.edu>
2004-07-16 19:15:40 +0000

    moved file functions to new file Functions.php in the inc/ directory

--- core/class/File.php
+++ core/class/File.php
@@ -301,38 +301,4 @@ class PHPWS_File {
     }
   }// END FUNC chkgd2()
 }
-
-if (!function_exists("file_get_contents")){
-  function file_get_contents($filename){
-    if (!is_file($filename))
-      return FALSE;
-
-    $fd = @fopen($filename, "rb");
-    
-    if ($fd){
-      $data = trim(fread ($fd, filesize ($filename)));
-      fclose ($fd);
-      return $data;
-    } else
-      return FALSE;
-  }
-}
-
-if (!function_exists("file_put_contents")){
-  function file_put_contents($filename, $data){
-    if($fp = @fopen($fileName, "wb")){
-      fwrite($fp, $data);
-      fclose($fp);
-      return TRUE;
-    } else
-      return FALSE;
-  }
-}
-
-if (!function_exists ("mime_content_type")) {
- function mime_content_type ($file) {
-  return exec ("file -bi " . escapeshellcmd($file));
- }
-}
-
 ?>
