846b3c309c4dbc34e79f1d8e194266fa017dd1a1
Matt McNaney <matt@tux.appstate.edu>
2004-07-22 15:11:57 +0000

    admin options added

--- mod/controlpanel/index.php
+++ mod/controlpanel/index.php
@@ -7,9 +7,15 @@ if (isset($_GET['cp_image_toggle'])){
 if (isset($_GET['cp_desc_toggle'])){
   PHPWS_ControlPanel_Tab::toggleDesc($_GET['tab']);
 }
-     
-if ($_SESSION['User']->isLogged()){
+
+if (isset($_REQUEST['action']) && Current_User::allow("controlpanel")){
+  PHPWS_Core::initModClass("controlpanel", "Action.php");
+
+  if ($_REQUEST['action'] == "admin")
+    CP_Action::adminAction();
+} elseif ($_SESSION['User']->isLogged()){
   Layout::add(PHPWS_ControlPanel::display());
 }
+
 
 ?>
