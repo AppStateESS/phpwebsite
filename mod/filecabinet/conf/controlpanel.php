24d3d0170f66cf1349a3908a9954ab382ac1bb03
Matt McNaney <matt@tux.appstate.edu>
2004-07-26 19:20:33 +0000

    put the proper ampersand in the link

--- mod/filecabinet/conf/controlpanel.php
+++ mod/filecabinet/conf/controlpanel.php
@@ -2,7 +2,7 @@
 
 $link[] = array("label"       => _("File Cabinet"),
 		 "restricted"  => TRUE,
-		 "url"         => "index.php?module=filecabinet&action=main",
+		 "url"         => "index.php?module=filecabinet&amp;action=main",
 		 "description" => _("Manages images and documents uploaded to your site."),
 		 "image"       => "cabinet.png",
 		 "tab"         => "admin"
