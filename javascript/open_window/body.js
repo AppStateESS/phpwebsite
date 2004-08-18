343fac6cd93f4a565313ce3897eafc8966751c90
Matt McNaney <matt@tux.appstate.edu>
2004-08-18 19:34:30 +0000

    moving

--- javascript/open_window/body.js
+++ javascript/open_window/body.js
@@ -1,1 +1,1 @@
-<a href="javascript:open_window('{address}', {width}, {height})";>{label}</a>
+<a href="javascript:open_window('{address}', {width}, {height})">{label}</a>
--- javascript/test/head.js
+++ javascript/test/head.js
@@ -3,6 +3,5 @@
 function testJS(){
   document.cookie = "js_check=1";
 }
-window.onload = testJS;
 //]]>
 </script>
