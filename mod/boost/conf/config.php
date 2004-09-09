a999dc44812fcb91bdec3f646214fcd2eb8ca316
Matt McNaney <matt@tux.appstate.edu>
2004-09-09 20:38:35 +0000

    new error messages

--- mod/boost/conf/config.php
+++ mod/boost/conf/config.php
@@ -5,6 +5,7 @@ define("BOOST_ERR_NOT_MODULE",       -1);
 define("BOOST_ERR_NO_INSTALLSQL",    -2);
 define("BOOST_NO_MODULES_SET",       -3);
 define("BOOST_FAILED_PRE94_INSTALL", -4);
+define("BOOST_FAILED_PRE94_UPGRADE", -5);
 
 
 
--- mod/boost/conf/error.php
+++ mod/boost/conf/error.php
@@ -3,5 +3,6 @@ $errors[BOOST_ERR_NOT_MODULE]       = _("Object sent to boost was not of the PHP
 $errors[BOOST_ERR_NO_INSTALLSQL]    = _("Unable to locate SQL import file.");
 $errors[BOOST_NO_MODULES_SET]       = _("Modules have not been set.");
 $errors[BOOST_FAILED_PRE94_INSTALL] = _("Failed installation of module.");
+$errors[BOOST_FAILED_PRE94_UPGRADE] = _("Failed upgrade of module.");
 
 ?>
