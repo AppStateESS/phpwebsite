8bc69c02da0bb5ae0122212de31173dcbac0feb6
Matt McNaney <matt@tux.appstate.edu>
2004-09-09 20:36:17 +0000

    added the html_entity_decode mock up stolen from php.net with some adjustment.

--- inc/Functions.php
+++ inc/Functions.php
@@ -37,4 +37,27 @@ if (!function_exists ("mime_content_type")) {
  }
 }
 
+// Taken from php.net
+// Written by dave at codexweb dot co dot za
+// Edited by Matthew McNaney
+if (!function_exists('html_entity_decode')) {
+  if (!defined("ENT_COMPAT")) define("ENT_COMPAT", 1);
+  if (!defined("ENT_NOQUOTES")) define("ENT_NOQUOTES", 4);
+  if (!defined("ENT_QUOTES")) define("ENT_QUOTES", 2);
+
+  function html_entity_decode ($string, $opt = ENT_COMPAT) {
+
+   $trans_tbl = get_html_translation_table (HTML_ENTITIES);
+   $trans_tbl = array_flip ($trans_tbl);
+
+   if ($opt == 2)
+     $trans_tbl["&#039;"] = $trans_tbl["&apos;"] = "'";
+
+   if ($opt == 4)
+     unset($trans_tbl["&quot;"]);
+   return strtr ($string, $trans_tbl);
+  }
+
+}
+
 ?>
