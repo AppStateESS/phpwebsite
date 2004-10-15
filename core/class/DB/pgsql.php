475e70bda164a01e2ef108491eeb377612d28f05
Matt McNaney <matt@tux.appstate.edu>
2004-10-15 20:45:39 +0000

    DB factory files

---
+++ core/class/DB/pgsql.php
@@ -0,0 +1,51 @@
+<?php
+
+class PHPWS_SQL {
+
+  var $offset = "OFFSET";
+
+  function export(&$info){
+    switch ($info['type']){
+
+    case "int8":
+    case "int4":
+      $setting = "INT";
+      $info['flags'] = preg_replace("/unique primary/", "PRIMARY KEY", $info['flags']);
+      break;
+
+    case "int2":
+      $setting = "SMALLINT";
+      break;
+
+    case "text":
+    case "blob":
+      $setting = "TEXT";
+      $info['flags'] = NULL;
+      break;
+    
+    case "bpchar":
+      $setting = "CHAR(255)";
+
+      if (empty($info['flags']))
+	$info['flags'] = "NULL";
+      break;
+    
+    case "date":
+      $setting = "DATE";
+      break;
+    
+    case "real":
+      $setting = "FLOAT";
+      break;
+    
+    case "timestamp":
+      $setting = "TIMESTAMP";
+      $info['flags'] = NULL;
+      break;
+    }
+    return $setting;
+  }
+
+}
+
+?>
