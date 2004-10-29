dd5a5b45ab6a58e9e7c55315faaad556e2e38858
Matt McNaney <matt@tux.appstate.edu>
2004-10-29 20:08:56 +0000

    new files

---
+++ mod/version/conf/error.php
@@ -0,0 +1,13 @@
+<?php
+
+translate("version");
+
+$errors = array(
+		VERSION_MISSING_ID     => _("Version function requires an item id."),
+		VERSION_NO_TABLE       => _("Source table not found."),
+		VERSION_NOT_MODULE     => _("Unknown module"),
+		VERSION_WRONG_SET_VAR  => _("Set variable must be an object or an array."),
+		VERSION_MISSING_SOURCE => _("Missing source data.")
+		);
+
+?>
