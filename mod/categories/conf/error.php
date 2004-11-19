a468bff93b3862c2700bd6ad23fd0892d744a1c3
Matt McNaney <matt@tux.appstate.edu>
2004-11-19 19:10:05 +0000

    error files

---
+++ mod/categories/conf/error.php
@@ -0,0 +1,8 @@
+<?php
+
+$errors[CATEGORY_NOT_FOUND]   = _("Category ID not found");
+$errors[CAT_DB_PROBLEM]       = _("There was a problem accessing the database.");
+$errors[CAT_NO_MOD_TABLE]     = _("Module does not have a category table.");
+$errors[CAT_ITEM_MISSING_VAL] = _("Category item is missing variables.");
+
+?>
---
+++ mod/categories/conf/errorDefines.php
@@ -0,0 +1,8 @@
+<?php
+
+define("CATEGORY_NOT_FOUND", -1);
+define("CAT_DB_PROBLEM",     -2);
+define("CAT_NO_MOD_TABLE",   -3);
+define("CAT_ITEM_MISSING_VAL", -4);
+
+?>
