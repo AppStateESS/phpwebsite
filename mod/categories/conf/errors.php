25ea9da612729f2d80caf410f541be3a39f0aa77
Matt McNaney <matt@tux.appstate.edu>
2004-11-19 19:09:57 +0000

    renamed

--- mod/categories/conf/errors.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$errors[CATEGORY_NOT_FOUND]  = _("Category ID not found");
-$errors[CAT_DB_PROBLEM]      = _("There was a problem accessing the database.");
-$errors[CAT_NO_MOD_TABLE]    = _("Module does not have a category table.");
-
-?>
