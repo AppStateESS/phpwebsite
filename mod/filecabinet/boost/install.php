774651ccbf9cfb2aa60788c0221375c52c46a3b3
Matt McNaney <matt@tux.appstate.edu>
2004-12-02 14:04:40 +0000

    supressing warnings

--- mod/filecabinet/boost/install.php
+++ mod/filecabinet/boost/install.php
@@ -1,7 +1,7 @@
 <?php
 
-mkdir(PHPWS_SOURCE_DIR . "images/filecabinet/images", "0700");
-mkdir(PHPWS_SOURCE_DIR . "images/filecabinet/documents", "0700");
-mkdir(PHPWS_SOURCE_DIR . "images/filecabinet/temp", "0700");
+@mkdir(PHPWS_SOURCE_DIR . "images/filecabinet", "0700");
+@mkdir(PHPWS_SOURCE_DIR . "images/filecabinet/documents", "0700");
+@mkdir(PHPWS_SOURCE_DIR . "images/filecabinet/temp", "0700");
 
 ?>
