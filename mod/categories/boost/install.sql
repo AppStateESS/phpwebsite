1d411f5cbaf6580619d48901fb092507bea1ffb7
Matt McNaney <matt@tux.appstate.edu>
2004-12-03 18:14:28 +0000

    updated

--- mod/categories/boost/install.sql
+++ mod/categories/boost/install.sql
@@ -3,16 +3,16 @@ CREATE TABLE categories (
   title varchar(255) NOT NULL default '',
   description text,
   parent int NOT NULL default '0',
-  image varchar(255) default NULL,
-  thumbnail varchar(255) default NULL,
+  icon varchar(255) default NULL,
   PRIMARY KEY  (id)
 );
 
 CREATE TABLE category_items (
-  cat_id int(11) NOT NULL default '0',
-  item_id int(11) NOT NULL default '0',
+  cat_id int NOT NULL default '0',
+  version_id int NOT NULL default '0',
+  item_id int NOT NULL default '0',
   module varchar(40) NOT NULL default '',
   item_name varchar(40) NOT NULL default '',
-  label varchar(255) NOT NULL default '',
+  title varchar(255) NOT NULL default '',
   link varchar(255) NOT NULL default ''
 );
