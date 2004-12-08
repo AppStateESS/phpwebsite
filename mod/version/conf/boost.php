218542816bc693f6d858224f7e2401becd44952a
Matt McNaney <matt@tux.appstate.edu>
2004-12-08 21:09:30 +0000

    added version's boost file

---
+++ mod/version/conf/boost.php
@@ -0,0 +1,13 @@
+<?php
+$proper_name  = "Version";
+$version      = "0.0.1";
+$register     = FALSE;
+$unregister   = FALSE;
+$import_sql   = FALSE;
+$image_dir    = FALSE;
+$file_dir     = FALSE;
+$version_http = "http://phpwebsite.appstate.edu/test/update.ini";
+$about        = TRUE;
+$priority     = 50;
+
+?>
