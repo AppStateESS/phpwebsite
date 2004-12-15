870c6fb48ac8aeeb8ae36bd1d0ff33b0f3b40a22
Matt McNaney <matt@tux.appstate.edu>
2004-12-15 20:28:40 +0000

    fixed comment wording - minor change

--- core/List.php
+++ core/List.php
@@ -1,9 +1,9 @@
 <?php
 
 /***********************************************************
- * This is a Crutch file to support pre 0.9.4 modules.
+ * This is a Crutch file to support pre 0.x.x modules.
  * If you are not running any ANY modules that depend on
- * 0.9.3 or lower, then you may safely remove this file.
+ * 0.x.x or lower, then you may safely remove this file.
  *
  * We recommend you back this file up just in case.
  */
