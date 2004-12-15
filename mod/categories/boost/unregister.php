a1867702f5227dd8eb982ecbc34bf17e5cfdb8c3
Matt McNaney <matt@tux.appstate.edu>
2004-12-15 20:57:15 +0000

    removed old code

--- mod/categories/boost/unregister.php
+++ mod/categories/boost/unregister.php
@@ -3,11 +3,6 @@
 function categories_unregister($module, &$content){
   PHPWS_Core::initModClass("categories", "Categories.php");
 
-  $tableName = Categories::categoryTableName($module);
-
-  $db = & new PHPWS_DB($tableName);
-  $db->dropTable();
-
   Categories::removeModule($module);
 }
 
