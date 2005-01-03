4a79fa9cc18f25f5516f524e0ed21c152b59eebe
Matt McNaney <matt@tux.appstate.edu>
2005-01-03 21:27:34 +0000

    fixed double quotes added PAGE_TITLE_DIVIDER global. Determines what character is printed between page title additions

--- mod/layout/conf/config.php
+++ mod/layout/conf/config.php
@@ -5,40 +5,44 @@
 // MAKE SURE to carefully inspect all of your current themes' theme.php files
 // BEFORE changing this to TRUE.
 
-$table = "
-<table cellpadding='3' cellspacing='1' style='background-color : black'>
+$table = '
+<table cellpadding="3" cellspacing="1" style="background-color : black">
   <tr>
-    <td style='background-color : black; color : white'><b>{TITLE}</b></td>
+    <td style="background-color : black; color : white"><b>{TITLE}</b></td>
   </tr>
   <tr>
-    <td style='background-color : white ; color : black'>{CONTENT}</td>
+    <td style="background-color : white ; color : black">{CONTENT}</td>
   </tr>
-</table>";
+</table>';
 
-$display_error_message = "
-<div align=\"center\"><img src=\"images/core/sorry.gif\" /></div>
+$display_error_message = '
+<div align="center"><img src="images/core/sorry.gif" /></div>
 <b>We seem to be experiencing technical difficulties.
-Back in a few.</b><br />";
+Back in a few.</b><br />';
 
 
-define("DEFAULT_TEMPLATE", $table);
-define("DEFAULT_NEW_BOX_VAR", "SIDEPANEL");
-define("DEFAULT_THEME_VAR", "BODY");
-define("DEFAULT_LAYOUT_HOLD", 20);
-define("DEFAULT_CONTENT_VAR", "_MAIN");
-define("MAX_ORDER_VALUE", 99999);
+define('DEFAULT_TEMPLATE', $table);
+define('DEFAULT_NEW_BOX_VAR', 'SIDEPANEL');
+define('DEFAULT_THEME_VAR', 'BODY');
+define('DEFAULT_LAYOUT_HOLD', 20);
+define('DEFAULT_CONTENT_VAR', '_MAIN');
+define('MAX_ORDER_VALUE', 99999);
+define('PAGE_TITLE_DIVIDER', '-');
 
-define("DISPLAY_ERROR_MESSAGE", $display_error_message);
+
+define('DISPLAY_ERROR_MESSAGE', $display_error_message);
 
 
-if (!defined("ALLOW_THEME_PHP_INSERTION"))
-     define("ALLOW_THEME_PHP_INSERTION", FALSE);
+if (!defined('ALLOW_THEME_PHP_INSERTION'))
+     define('ALLOW_THEME_PHP_INSERTION', FALSE);
 
 /********** Errors ****************/
 
-define("LAYOUT_SESSION_NOT_SET",    -1);
-define("LAYOUT_NO_CONTENT",         -2);
-define("LAYOUT_NO_THEME",           -3);
-define("LAYOUT_BAD_JS_DATA",        -4);
-define("LAYOUT_JS_FILE_NOT_FOUND",  -5);
+define('LAYOUT_SESSION_NOT_SET',    -1);
+define('LAYOUT_NO_CONTENT',         -2);
+define('LAYOUT_NO_THEME',           -3);
+define('LAYOUT_BAD_JS_DATA',        -4);
+define('LAYOUT_JS_FILE_NOT_FOUND',  -5);
+
+
 ?>
