e706e1f09e32d81c83358028da4deb00fb0a105f
Matt McNaney <matt@tux.appstate.edu>
2005-01-05 14:29:06 +0000

    Not needed now.

--- mod/categories/javascript/category_list.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-function confirmDelete(category_id){
-  if (confirm('{QUESTION}'))
-    location.href = "index.php?module=categories&action=admin&subaction=deleteCategory&category_id=" + category_id;
-}
-</script>
