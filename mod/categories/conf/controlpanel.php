a6a720283e41d4e053943b313a4f4a731c8e1dc4
Matt McNaney <matt@tux.appstate.edu>
2005-01-06 21:02:32 +0000

    changed a & to &amp; for xhtml compatibility

--- mod/categories/conf/controlpanel.php
+++ mod/categories/conf/controlpanel.php
@@ -3,7 +3,7 @@
 $link[] = array("label"       => _("Categories"),
                 "restricted"  => TRUE,
                 "url"         =>
-                "index.php?module=categories&action=admin",
+                "index.php?module=categories&amp;action=admin",
 		"description" => _("Administrate your site's categories."),
 		"image"       => "categories.png",
 		"tab"         => "content"
