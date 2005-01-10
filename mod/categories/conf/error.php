f9337880e34b80f532cacf5563399aba1e1bff79
Matt McNaney <matt@tux.appstate.edu>
2005-01-10 16:08:20 +0000

    added error message

--- mod/categories/conf/error.php
+++ mod/categories/conf/error.php
@@ -1,8 +1,9 @@
 <?php
 
-$errors[CATEGORY_NOT_FOUND]   = _("Category ID not found");
-$errors[CAT_DB_PROBLEM]       = _("There was a problem accessing the database.");
-$errors[CAT_NO_MOD_TABLE]     = _("Module does not have a category table.");
-$errors[CAT_ITEM_MISSING_VAL] = _("Category item is missing variables.");
+$errors[CATEGORY_NOT_FOUND]    = _('Category ID not found');
+$errors[CAT_DB_PROBLEM]        = _('There was a problem accessing the database.');
+$errors[CAT_NO_MOD_TABLE]      = _('Module does not have a category table.');
+$errors[CAT_ITEM_MISSING_VAL]  = _('Category item is missing variables.');
+$errors[CAT_NO_IDS_IN_VERSION] = _('Missing either a version or item id.');
 
 ?>
--- mod/categories/conf/errorDefines.php
+++ mod/categories/conf/errorDefines.php
@@ -1,8 +1,9 @@
 <?php
 
-define("CATEGORY_NOT_FOUND", -1);
-define("CAT_DB_PROBLEM",     -2);
-define("CAT_NO_MOD_TABLE",   -3);
-define("CAT_ITEM_MISSING_VAL", -4);
+define('CATEGORY_NOT_FOUND',    -1);
+define('CAT_DB_PROBLEM',        -2);
+define('CAT_NO_MOD_TABLE',      -3);
+define('CAT_ITEM_MISSING_VAL',  -4);
+define('CAT_NO_IDS_IN_VERSION', -5);
 
 ?>
