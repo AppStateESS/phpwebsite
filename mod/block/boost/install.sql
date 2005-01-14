4899f106777bc2deade345c6e2e06560ed81f68a
Matt McNaney <matt@tux.appstate.edu>
2005-01-14 20:19:42 +0000

    added new table

--- mod/block/boost/install.sql
+++ mod/block/boost/install.sql
@@ -2,6 +2,12 @@ CREATE TABLE block (
 id INT DEFAULT '0' NOT NULL ,
 title VARCHAR( 255 ) ,
 content TEXT NOT NULL ,
-module VARCHAR( 40 ) NOT NULL ,
 PRIMARY KEY ( id )
+);
+
+CREATE TABLE block_pinned (
+block_id INT DEFAULT '0' NOT NULL ,
+module VARCHAR( 40 ) NOT NULL ,
+itemname VARCHAR( 40 ) NOT NULL ,
+item_id INT DEFAULT '0' NOT NULL
 );
