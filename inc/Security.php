5fd935f96da3aa1ebd2fde47d38c624396433678
Matt McNaney <matt@tux.appstate.edu>
2005-01-20 21:33:58 +0000

    changed the array cleaning to use the 0.x method for embedded arrays

--- inc/Security.php
+++ inc/Security.php
@@ -23,21 +23,19 @@ if (ini_get('session.use_trans_sid')) {
 define('SESSION_NAME', md5(SITE_HASH . $_SERVER['REMOTE_ADDR']));
 
 // Attempt to clean out the xss tags
-clearRequest();
 
-function clearRequest()
-{
-  $scriptPattern = array('/(%3C|<|&lt;|&#60;)\s*(script|\?)/iU');
-  foreach ($_REQUEST as $key => $value) {
-    $_REQUEST[$key] = preg_replace($scriptPattern, '', $value);
-    if (isset($_GET[$key])) {
-      $_GET[$key] = strip_tags($_REQUEST[$key]);
-    }
+/* Clean out bad script, php, and newline characters */
+cleanArray($_REQUEST);
+cleanArray($_GET);
 
-    if (isset($_POST[$key])) {
-      $_POST[$key] = $_REQUEST[$key];
-    }
+function cleanArray(&$clean) {
+  if (is_array($clean)) {
+    array_walk ($clean, 'cleanArray');
+  } else {
+    $scriptPattern = array('/(%3C|<|&lt;|&#60;)\s*(script|\?)/iU');
+    $clean = preg_replace($scriptPattern, '', $clean);
   }
+  return;
 }
 
 ?>
