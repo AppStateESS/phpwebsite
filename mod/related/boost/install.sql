1906b967d762fa23895923ff0348587d84f3a68c
Matt McNaney <matt@tux.appstate.edu>
2005-01-25 20:56:37 +0000

    changed main_id column to item_id to match with key class

--- mod/related/boost/install.sql
+++ mod/related/boost/install.sql
@@ -6,7 +6,7 @@ CREATE TABLE related_friends (
 
 CREATE TABLE related_main (
   id int NOT NULL default '0',
-  main_id int NOT NULL default '0',
+  item_id int NOT NULL default '0',
   module varchar(40) NOT NULL default '',
   item_name varchar(40) NOT NULL default '',
   title varchar(255) NOT NULL default '',
