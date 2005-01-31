cd265fd87865b6af03d094d6a1d7857123c5c6a4
Matt McNaney <matt@tux.appstate.edu>
2005-01-31 15:52:37 +0000

    added new error codes

--- mod/boost/conf/config.php
+++ mod/boost/conf/config.php
@@ -1,12 +1,12 @@
 <?php
 
 /********** Error Codes ***************/
-define("BOOST_ERR_NOT_MODULE",       -1);
-define("BOOST_ERR_NO_INSTALLSQL",    -2);
-define("BOOST_NO_MODULES_SET",       -3);
-define("BOOST_FAILED_PRE94_INSTALL", -4);
-define("BOOST_FAILED_PRE94_UPGRADE", -5);
-
-
+define('BOOST_ERR_NOT_MODULE',       -1);
+define('BOOST_ERR_NO_INSTALLSQL',    -2);
+define('BOOST_NO_MODULES_SET',       -3);
+define('BOOST_FAILED_PRE94_INSTALL', -4);
+define('BOOST_FAILED_PRE94_UPGRADE', -5);
+define('BOOST_NO_REGISTER_FILE',     -6);
+define('BOOST_NO_REGISTER_FUNCTION', -7);
 
 ?>
