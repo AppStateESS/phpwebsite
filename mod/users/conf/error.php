9011fe26a9c199d699f6c9f706e5c88d6cef318c
Matt McNaney <matt@tux.appstate.edu>
2005-01-31 15:54:55 +0000

    changed double to single

--- mod/users/conf/error.php
+++ mod/users/conf/error.php
@@ -1,29 +1,31 @@
 <?php
 
-translate("users");
+translate('users');
 $errors = array(
-		USER_ERROR                => _("Unknown error."),
-		USER_ERR_DUP_USERNAME     => _("User with this name already exists."),
-		USER_ERR_DUP_EMAIL        => _("User with this email already exists."),
-		USER_ERR_DUP_GROUPNAME    => _("A group with this name already exists."),
-		USER_ERR_PERM_TABLE       => _("Permission table name already exists."),
-		USER_ERR_PERM_MISS        => _("Permission table not found."),
-		USER_ERR_PERM_FILE        => _("Module's permission file is missing."),
-		USER_ERR_ITEM_PERM_FILE   => _("Module's item permission file is missing."),
-		USER_ERR_BAD_USERNAME     => _("Username is improperly formatted."),
-		USER_ERR_PASSWORD_MATCH   => _("Passwords do not match."),
-		USER_ERR_PASSWORD_LENGTH  => sprintf(_("Password must be at least %s characters in length."), PASSWORD_LENGTH),
-		USER_ERR_PASSWORD_EASY    => _("Password is too easy to guess."),
-		USER_ERR_NO_MODULE        => _("Module does not exist."),
-		USER_ERR_BAD_VAR          => _("Invalid variable name."),
-		USER_ERR_USER_NOT_SAVED   => _("Unable to save user information."),
-		USER_ERR_BAD_GROUP_NAME   => _("Improperly formatted group name."),
-		USER_ERR_GROUP_DNE        => _("Group does not exist."),
-		USER_ERR_MISSING_GROUP    => _("User is missing their permission group"),
-		USER_ERR_FAIL_ON_SUBPERM  => _("Module tried to detect a non-existant permission."),
-		USER_ERR_NO_EMAIL         => _("User's email address is missing."),
-		USER_ERR_BAD_EMAIL        => _("User's email address is missing or malformed."),
-		USER_ERR_MISSING_AUTH     => _("Missing authorization script.")
+		USER_ERROR                => _('Unknown error.'),
+		USER_ERR_DUP_USERNAME     => _('User with this name already exists.'),
+		USER_ERR_DUP_EMAIL        => _('User with this email already exists.'),
+		USER_ERR_DUP_GROUPNAME    => _('A group with this name already exists.'),
+		USER_ERR_PERM_TABLE       => _('Permission table name already exists.'),
+		USER_ERR_PERM_MISS        => _('Permission table not found.'),
+		USER_ERR_PERM_FILE        => _('Module\'s permission file is missing.'),
+		USER_ERR_ITEM_PERM_FILE   => _('Module\'s item permission file is missing.'),
+		USER_ERR_BAD_USERNAME     => _('Username is improperly formatted.'),
+		USER_ERR_PASSWORD_MATCH   => _('Passwords do not match.'),
+		USER_ERR_PASSWORD_LENGTH  => sprintf(_('Password must be at least %s characters in length.'), PASSWORD_LENGTH),
+		USER_ERR_PASSWORD_EASY    => _('Password is too easy to guess.'),
+		USER_ERR_NO_MODULE        => _('Module does not exist.'),
+		USER_ERR_BAD_VAR          => _('Invalid variable name.'),
+		USER_ERR_USER_NOT_SAVED   => _('Unable to save user information.'),
+		USER_ERR_BAD_GROUP_NAME   => _('Improperly formatted group name.'),
+		USER_ERR_GROUP_DNE        => _('Group does not exist.'),
+		USER_ERR_MISSING_GROUP    => _('User is missing their permission group'),
+		USER_ERR_FAIL_ON_SUBPERM  => _('Module tried to detect a non-existant permission.'),
+		USER_ERR_NO_EMAIL         => _('User\'s email address is missing.'),
+		USER_ERR_BAD_EMAIL        => _('User\'s email address is missing or malformed.'),
+		USER_ERR_MISSING_AUTH     => _('Missing authorization script.'),
+		USER_ANONGROUP_DELETE     => _('The anonymous group may not be deleted.')
+		
 		);
 
 ?>
--- mod/users/conf/errorDefines.php
+++ mod/users/conf/errorDefines.php
@@ -1,34 +1,35 @@
 <?php
 /* Errors */
-define("USER_ERROR",               -1);
-define("USER_ERR_DUP_USERNAME",    -2);
-define("USER_ERR_DUP_GROUPNAME",   -3);
-define("USER_ERR_PERM_TABLE",      -4);
-define("USER_ERR_PERM_MISS",       -5);
-define("USER_ERR_PERM_FILE",       -6);
-define("USER_ERR_BAD_USERNAME",    -7);
-define("USER_ERR_PASSWORD_MATCH",  -8);
-define("USER_ERR_PASSWORD_LENGTH", -9);
-define("USER_ERR_PASSWORD_EASY",   -10);
-define("USER_ERR_USER_NOT_SAVED",  -11);
-define("USER_ERR_MISSING_GROUP",   -12);
-define("USER_ERR_DUP_EMAIL",       -13);
-define("USER_ERR_NO_EMAIL",        -14);
-define("USER_ERR_BAD_EMAIL",       -15);
-define("USER_ERR_ITEM_PERM_FILE",  -16);
+define('USER_ERROR',               -1);
+define('USER_ERR_DUP_USERNAME',    -2);
+define('USER_ERR_DUP_GROUPNAME',   -3);
+define('USER_ERR_PERM_TABLE',      -4);
+define('USER_ERR_PERM_MISS',       -5);
+define('USER_ERR_PERM_FILE',       -6);
+define('USER_ERR_BAD_USERNAME',    -7);
+define('USER_ERR_PASSWORD_MATCH',  -8);
+define('USER_ERR_PASSWORD_LENGTH', -9);
+define('USER_ERR_PASSWORD_EASY',   -10);
+define('USER_ERR_USER_NOT_SAVED',  -11);
+define('USER_ERR_MISSING_GROUP',   -12);
+define('USER_ERR_DUP_EMAIL',       -13);
+define('USER_ERR_NO_EMAIL',        -14);
+define('USER_ERR_BAD_EMAIL',       -15);
+define('USER_ERR_ITEM_PERM_FILE',  -16);
 
 /* User Variable Errors */
-define("USER_ERR_NO_MODULE",       -20);
-define("USER_ERR_BAD_VAR",         -21);
+define('USER_ERR_NO_MODULE',       -20);
+define('USER_ERR_BAD_VAR',         -21);
 
 /* Group errors */
 define('USER_ERR_BAD_GROUP_NAME',  -30);
 define('USER_ERR_GROUP_DNE',       -31);
+define('USER_ANONGROUP_DELETE',    -32);
 
 /* Permission errors */
-define("USER_ERR_FAIL_ON_SUBPERM", -40);
+define('USER_ERR_FAIL_ON_SUBPERM', -40);
 
 /* Authorization errors */
-define("USER_ERR_MISSING_AUTH",    -50);
+define('USER_ERR_MISSING_AUTH',    -50);
 
 ?>
