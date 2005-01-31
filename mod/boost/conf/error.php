17aff6c86cab22444486c77f9370f916f5476b6e
Matt McNaney <matt@tux.appstate.edu>
2005-01-31 15:52:48 +0000

    new error codes

--- mod/boost/conf/error.php
+++ mod/boost/conf/error.php
@@ -1,8 +1,10 @@
 <?php
-$errors[BOOST_ERR_NOT_MODULE]       = _("Object sent to boost was not of the PHPWS_Module class.");
-$errors[BOOST_ERR_NO_INSTALLSQL]    = _("Unable to locate SQL import file.");
-$errors[BOOST_NO_MODULES_SET]       = _("Modules have not been set.");
-$errors[BOOST_FAILED_PRE94_INSTALL] = _("Failed installation of module.");
-$errors[BOOST_FAILED_PRE94_UPGRADE] = _("Failed upgrade of module.");
+$errors[BOOST_ERR_NOT_MODULE]       = _('Object sent to boost was not of the PHPWS_Module class.');
+$errors[BOOST_ERR_NO_INSTALLSQL]    = _('Unable to locate SQL import file.');
+$errors[BOOST_NO_MODULES_SET]       = _('Modules have not been set.');
+$errors[BOOST_FAILED_PRE94_INSTALL] = _('Failed installation of module.');
+$errors[BOOST_FAILED_PRE94_UPGRADE] = _('Failed upgrade of module.');
+$errors[BOOST_NO_REGISTER_FILE]     = _('Module is missing register file.');
+$errors[BOOST_NO_REGISTER_FUNCTION] = _('Module is missing register function.');
 
 ?>
