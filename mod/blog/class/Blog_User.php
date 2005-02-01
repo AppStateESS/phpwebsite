880fa025f19dc361578cf703152a081c2768233a
Matt McNaney <matt@tux.appstate.edu>
2005-02-01 15:51:14 +0000

    Added per item viewing permission

--- mod/blog/class/Blog_User.php
+++ mod/blog/class/Blog_User.php
@@ -14,17 +14,26 @@ class Blog_User {
 
     $db = & new PHPWS_DB("blog_entries");
     $db->setLimit($limit);
-    $db->addOrder("date desc");
+    $db->addOrder('date desc');
     if (!Current_User::isLogged()) {
       $db->addWhere('restricted', '0');
     }
+
     $result = $db->getObjects("Blog");
 
     if (empty($result))
       return ("No blog entries found.");
     
-    foreach ($result as $blog)
-      $list[] = $blog->view(); 
+    foreach ($result as $blog) {
+      if ($blog->getRestricted() == 2 &&
+	  !Current_User::allow('blog', 'view_blog', $blog->getId(), 'entry')) {
+	continue;
+      }
+      $view = $blog->view();
+      if (!empty($view)) {
+	$list[] = $view;
+      }
+    }
 
     $content = implode("", $list);
     if (!Current_User::allow("blog")) {
