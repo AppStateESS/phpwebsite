5b636e91c230fe2c4dd9db7ac959317ad6b17f07
Matt McNaney <matt@tux.appstate.edu>
2005-03-09 19:32:09 +0000

    upgrade >> update

--- mod/boost/index.php
+++ mod/boost/index.php
@@ -2,29 +2,29 @@
 if (!Current_User::isDeity() || !isset($_REQUEST['action'])) return;
 
 $content = array();
-PHPWS_Core::initModClass("boost", "Form.php");
-PHPWS_Core::initModClass("controlpanel", "Panel.php");
+PHPWS_Core::initModClass('boost', 'Form.php');
+PHPWS_Core::initModClass('controlpanel', 'Panel.php');
 
-$boostPanel = & new PHPWS_Panel("boost");
+$boostPanel = & new PHPWS_Panel('boost');
 Boost_Form::setTabs($boostPanel);
 
 switch ($_REQUEST['action']){
- case "admin":
+ case 'admin':
    $content[] = Boost_Form::listModules(Boost_Form::boostTab($boostPanel));
    break;
 
- case "check":
-   PHPWS_Core::initModClass("boost", "Action.php");
+ case 'check':
+   PHPWS_Core::initModClass('boost', 'Action.php');
    $content[] = Boost_Action::checkupdate($_REQUEST['opmod']);
    break;
 
- case "aboutView":
-   PHPWS_Core::initModClass("boost", "Boost.php");
+ case 'aboutView':
+   PHPWS_Core::initModClass('boost', 'Boost.php');
    PHPWS_Boost::aboutView($_REQUEST['aboutmod']);
    break;
 
- case "install":
-   PHPWS_Core::initModClass("boost", "Action.php");
+ case 'install':
+   PHPWS_Core::initModClass('boost', 'Action.php');
    $result = Boost_Action::installModule($_REQUEST['opmod']);
    if (PEAR::isError($result)) {
      PHPWS_Error::log($result);
@@ -35,18 +35,18 @@ switch ($_REQUEST['action']){
    }
    break;
 
- case "uninstall":
-   PHPWS_Core::initModClass("boost", "Action.php");
+ case 'uninstall':
+   PHPWS_Core::initModClass('boost', 'Action.php');
    $content[] = Boost_Action::uninstallModule($_REQUEST['opmod']);
    break;
 
- case "upgrade":
-   PHPWS_Core::initModClass("boost", "Action.php");
-   $content[] = Boost_Action::upgradeModule($_REQUEST['opmod']);
+ case 'update':
+   PHPWS_Core::initModClass('boost', 'Action.php');
+   $content[] = Boost_Action::updateModule($_REQUEST['opmod']);
    break;
 }// End area switch
 
-$boostPanel->setContent(implode("", $content));
+$boostPanel->setContent(implode('', $content));
 $finalContent = $boostPanel->display();
 Layout::add(PHPWS_ControlPanel::display($finalContent));
 ?>
