275bf71a321825ec94dbf06e82c4372c3a0f22fa
Matt McNaney <matt@tux.appstate.edu>
2005-03-22 14:27:25 +0000

    double to single quotes

--- mod/users/boost/install.php
+++ mod/users/boost/install.php
@@ -1,15 +1,15 @@
 <?php
 
 function users_install(&$content, $branchInstall=FALSE){
-  PHPWS_Core::initModClass("users", "Users.php");
-  PHPWS_Core::initModClass("users", "Action.php");
-  include PHPWS_Core::getConfigFile("users", "config.php");
+  PHPWS_Core::initModClass('users', 'Users.php');
+  PHPWS_Core::initModClass('users', 'Action.php');
+  PHPWS_Core::configRequireOnce('users', 'config.php');
 
   $user = & new PHPWS_User;
-  $content[] = "<hr />";
+  $content[] = '<hr />';
 
   if ($branchInstall==FALSE){
-    if (isset($_POST['module']) && $_POST['module']=="users"){
+    if (isset($_POST['module']) && $_POST['module']=='users'){
       $result = User_Action::postUser($user);
 
       if (!is_array($result)){
@@ -21,17 +21,17 @@ function users_install(&$content, $branchInstall=FALSE){
 	if (PEAR::isError($result))
 	  return $result;
 
-	$content[] = _("User created successfully.");
-	$db = & new PHPWS_DB("users_auth_scripts");
-	$db->addValue("display_name", _("Local"));
-	$db->addValue("filename", "local.php");
+	$content[] = _('User created successfully.');
+	$db = & new PHPWS_DB('users_auth_scripts');
+	$db->addValue('display_name', _('Local'));
+	$db->addValue('filename', 'local.php');
 	$db->insert();
       } else {
 	$content[] = userForm($user, $result);
 	return FALSE;
       }
     } else {
-      $content[] = _("Please create a user to administrate the site.") . "<br />";
+      $content[] = _('Please create a user to administrate the site.') . '<br />';
       $content[] = userForm($user);
       return FALSE;
     }
@@ -42,34 +42,32 @@ function users_install(&$content, $branchInstall=FALSE){
 
 
 function userForm(&$user, $errors=NULL){
-  PHPWS_Core::initCoreClass("Form.php");
-  PHPWS_Core::initModClass("users", "Form.php");
+  PHPWS_Core::initCoreClass('Form.php');
+  PHPWS_Core::initModClass('users', 'Form.php');
 
-  translate("users");
+  translate('users');
   $form = & new PHPWS_Form;
 
-  $form->addHidden("step", 3);
-  $form->addHidden("module", "users");
-  $form->addText("username", $user->getUsername());
-  $form->addText("email", $user->getEmail());
-  $form->addPassword("password1");
-  $form->addPassword("password2");
+  $form->addHidden('step', 3);
+  $form->addHidden('module', 'users');
+  $form->addText('username', $user->getUsername());
+  $form->addText('email', $user->getEmail());
+  $form->addPassword('password1');
+  $form->addPassword('password2');
 
-  $form->setLabel("username", _("Username"));
-  $form->setLabel("password1", _("Password"));
-  $form->setLabel("email", _("Email"));
+  $form->setLabel('username', _('Username'));
+  $form->setLabel('password1', _('Password'));
+  $form->setLabel('email', _('Email'));
 
-  $form->addSubmit("submit", _("Add User"));
+  $form->addSubmit('submit', _('Add User'));
   
-  $form->mergeTemplate($tpl);
-
   $template = $form->getTemplate();
 
   if (!empty($errors))
     foreach ($errors as $tag=>$message)
       $template[$tag] = $message;
 
-  $result = PHPWS_Template::process($template, "users", "forms/userForm.tpl");
+  $result = PHPWS_Template::process($template, 'users', 'forms/userForm.tpl');
 
   $content[] = $result;
   return implode("\n", $content);
--- setup/preconfig.php
+++ setup/preconfig.php
@@ -1,18 +1,18 @@
 <?php
 
-if (isset($_SERVER['WINDIR']) || preg_match("/(microsoft|win32)/i", $_SERVER['SERVER_SOFTWARE'])){
-  ini_set("include_path", ".;.\\lib\\pear\\");
+if (isset($_SERVER['WINDIR']) || preg_match('/(microsoft|win32)/i', $_SERVER['SERVER_SOFTWARE'])){
+  ini_set('include_path', '.;.\\lib\\pear\\');
 }
 else {
-  ini_set("include_path", ".:./lib/pear/");
+  ini_set('include_path', '.:./lib/pear/');
 }
 
-define("PHPWS_SOURCE_DIR", "./");
-define("PHPWS_LOG_DIRECTORY", "./logs/");
-define("LOG_PERMISSION", 0644);
-define("LOG_TIME_FORMAT", "%X %x");
-define("PHPWS_LOG_ERRORS", TRUE);
-define("FORCE_MOD_CONFIG", TRUE);
-define("DEFAULT_LANGUAGE", "en");
-
+define('PHPWS_SOURCE_DIR', './');
+define('PHPWS_LOG_DIRECTORY', './logs/');
+define('LOG_PERMISSION', 0644);
+define('LOG_TIME_FORMAT', '%X %x');
+define('PHPWS_LOG_ERRORS', TRUE);
+define('FORCE_MOD_CONFIG', TRUE);
+define('DEFAULT_LANGUAGE', 'en');
+define('USE_CRUTCH_FILES', FALSE);
 ?>
