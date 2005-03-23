5f9a43bac174e67e17b3129557b82299549dd888
Matt McNaney <matt@tux.appstate.edu>
2005-03-23 18:08:59 +0000

    custom file was missing added readme

---
+++ javascript/editors/htmlarea/custom.js
@@ -0,0 +1,16 @@
+custom_toolbar = [
+		[ "formatblock", "space",
+		  "bold", "italic", "underline", "strikethrough", "separator",
+		  "subscript", "superscript", "separator",
+		  "copy", "cut", "paste", "space", "undo", "redo", "space" ],
+
+		[ "justifyleft", "justifycenter", "justifyright", "justifyfull", "separator",
+		  "lefttoright", "righttoleft", "separator",
+		  "orderedlist", "unorderedlist", "outdent", "indent", "separator",
+		  "forecolor", "separator",
+		  "inserthorizontalrule", "createlink", "insertimage", "inserttable", "htmlmode" ]
+	];
+
+// if you edit these values, check the body.js file values as well
+custom_height = "320px";
+custom_width = "485px";
---
+++ javascript/editors/htmlarea/readme.txt
@@ -0,0 +1,6 @@
+The files contained in this directory allow the HtmlArea wysiwyg tool
+to function within phpWebSite.
+
+We do not offer this script and it is currently discontinued.
+
+To customize the editor using the custom.js file. 
