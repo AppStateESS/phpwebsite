d9f8a1bb8a89892555c2c6172b85113e4e66f924
Matt McNaney <matt@tux.appstate.edu>
2005-03-24 17:30:38 +0000

    set use of the custom config file and set the width and height

--- javascript/editors/FCKeditor/head.js
+++ javascript/editors/FCKeditor/head.js
@@ -2,8 +2,9 @@
 <script type="text/javascript">
 function FCKinit()
 {
-    var oFCKeditor = new FCKeditor( '{NAME}' ) ;
+    var oFCKeditor = new FCKeditor( '{NAME}' , 500, 300, "phpws") ;
     oFCKeditor.BasePath = './javascript/editors/FCKeditor/';
+    oFCKeditor.Config["CustomConfigurationsPath"] = 'custom.js';
     oFCKeditor.ReplaceTextarea() ;
 }
 </script>
