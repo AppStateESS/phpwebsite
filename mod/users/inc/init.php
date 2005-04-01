309fd6613f2d41c60483795fba36cf41c26d162d
Matt McNaney <matt@tux.appstate.edu>
2005-04-01 15:27:59 +0000

    moved the tags out to their own file

--- mod/users/inc/init.php
+++ mod/users/inc/init.php
@@ -1,12 +1,9 @@
 <?php
 
-PHPWS_Core::configRequireOnce("users", "config.php", TRUE);
-PHPWS_Core::configRequireOnce("users", "errorDefines.php", TRUE);
+PHPWS_Core::configRequireOnce('users', 'config.php', TRUE);
+PHPWS_Core::configRequireOnce('users', 'errorDefines.php', TRUE);
+PHPWS_Core::configRequireOnce('users', 'tags.php');
 
-include_once $includeFile;
-
-PHPWS_Core::initModClass("users", "Users.php");
-PHPWS_Core::initModClass("users", "Current_User.php");
-PHPWS_Text::addTag('users', 'new_account');
-
+PHPWS_Core::initModClass('users', 'Users.php');
+PHPWS_Core::initModClass('users', 'Current_User.php');
 ?>
