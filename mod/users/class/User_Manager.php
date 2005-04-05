82075e7dad2985d11d6808c5988137796ad91209
Matt McNaney <matt@tux.appstate.edu>
2005-04-05 20:24:28 +0000

    double quotes changed delete user ability added

--- mod/users/class/User_Manager.php
+++ mod/users/class/User_Manager.php
@@ -4,16 +4,16 @@ class User_Manager extends PHPWS_User{
 
   function listActive(){
     if ($this->isActive())
-      return _("Yes");
+      return _('Yes');
     else
-      return _("No");
+      return _('No');
   }
 
   function listLastLogged(){
-    $logged = $this->getLastLogged("%c");
+    $logged = $this->getLastLogged('%c');
 
     if (empty($logged))
-      return _("Never");
+      return _('Never');
     else
       return $logged;
 
@@ -22,24 +22,35 @@ class User_Manager extends PHPWS_User{
   function listAction($user){
     $id = $user->id;
 
-    $linkVar['action'] = "admin";
+    $linkVar['action'] = 'admin';
     $linkVar['user_id'] = $id;
 
-    $linkVar['command'] = "editUser";
-    $links[] = PHPWS_Text::secureLink(_("Edit"), "users", $linkVar);
+    $jsvar['QUESTION'] = sprintf(_('Are you certain you want to delete the user &quot;%s&quot; permanently?'),
+				 $user->getUsername());
+    $jsvar['ADDRESS']  = 'index.php?module=users&amp;action=admin&amp;command=deleteUser&amp;user_id='
+      . $id . '&amp;authkey=' . Current_User::getAuthKey();
+    $jsvar['LINK']     = _('Delete');
+    
+    
+    $linkVar['command'] = 'editUser';
+    $links[] = PHPWS_Text::secureLink(_('Edit'), 'users', $linkVar);
 
-    $linkVar['command'] = "setUserPermissions";
-    $links[] = PHPWS_Text::secureLink(_("Permissions"), "users", $linkVar);
+    $linkVar['command'] = 'setUserPermissions';
+    $links[] = PHPWS_Text::secureLink(_('Permissions'), 'users', $linkVar);
 
-    if ($user->active){
-      $linkVar['command'] = "deactivateUser";
-      $links[] = PHPWS_Text::secureLink(_("Deactivate"), "users", $linkVar);
-    } else {
-      $linkVar['command'] = "activateUser";
-      $links[] = PHPWS_Text::secureLink(_("Activate"), "users", $linkVar);
+    if (!$user->isDeity() && ($user->id != Current_User::getId())) {
+      $links[] = Layout::getJavascript('confirm', $jsvar);
     }
 
-    return implode(" | ", $links);
+    if ($user->active){
+      $linkVar['command'] = 'deactivateUser';
+      $links[] = PHPWS_Text::secureLink(_('Deactivate'), 'users', $linkVar);
+    } else {
+      $linkVar['command'] = 'activateUser';
+      $links[] = PHPWS_Text::secureLink(_('Activate'), 'users', $linkVar);
+    }
+
+    return implode(' | ', $links);
   }
 
   function listEmail(){
