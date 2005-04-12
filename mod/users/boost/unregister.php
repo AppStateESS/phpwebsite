2cea1344d7846c3c1108684efa90c1c2c65deb4f
Matt McNaney <matt@tux.appstate.edu>
2005-04-12 16:52:19 +0000

    double quotes to single

--- mod/users/boost/unregister.php
+++ mod/users/boost/unregister.php
@@ -1,15 +1,15 @@
 <?php
 
 function users_unregister($module, &$content){
-  PHPWS_Core::initModClass("users", "Permission.php");
+  PHPWS_Core::initModClass('users', 'Permission.php');
 
-  $content[] = _("Removing permissions table.");
+  $content[] = _('Removing permissions table.');
   if (Users_Permission::removePermissions($module)){
-    $content[] = _("Permissions table removed successfully.");
+    $content[] = _('Permissions table removed successfully.');
     return TRUE;
   }
   else {
-    $content[] = _("Permissions table not removed successfully.");
+    $content[] = _('Permissions table not removed successfully.');
     return FALSE;
   }
 }
