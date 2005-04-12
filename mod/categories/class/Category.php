cf15ad38abe7a085d5cce860d87dad7fa52798ed
Matt McNaney <matt@tux.appstate.edu>
2005-04-12 16:50:16 +0000

    changed reroute to rewrite per changes in core

--- mod/categories/class/Category.php
+++ mod/categories/class/Category.php
@@ -164,7 +164,7 @@ class Category{
       $vars['ref_mod'] = $module;
       return PHPWS_Text::moduleLink($this->title, 'categories', $vars);
     } else {
-      return PHPWS_Text::rerouteLink($this->title, 'categories', 'view', $this->id);
+      return PHPWS_Text::rewriteLink($this->title, 'categories', 'view', $this->id);
     }
   }
 
