6b9c706c6fd45b0530e1ddfa170f31b570d91832
Matt McNaney <matt@tux.appstate.edu>
2005-04-25 17:26:31 +0000

    removed pear's bbcode parser for the one off of corz.org. Added special function call to parse a smilie pak file.

--- core/class/Text.php
+++ core/class/Text.php
@@ -2,6 +2,8 @@
 /**
  * Controls the text parsing and profanity controls for phpWebSite
  * Also contains extra HTML utilities
+ *
+ * See config/core/text_settings.php for configuration options
  * 
  * @version $Id$
  * @author  Matthew McNaney <matt at tux dot appstate dot edu>
@@ -11,13 +13,13 @@
  * @package Core
  */
 
-PHPWS_Core::configRequireOnce('core', 'profanity.php');
+PHPWS_Core::configRequireOnce('core', 'text_settings.php');
 
 class PHPWS_Text {
   var $use_profanity  = ALLOW_PROFANITY;
   var $use_breaker    = TRUE;
-  var $use_encoded    = FALSE;
   var $use_strip_tags = TRUE;
+  var $use_bbcode     = ALLOW_BB_CODE;
   var $_allowed_tags  = NULL;
 
   function PHPWS_Text($text=NULL, $encoded=FALSE)
@@ -26,19 +28,22 @@ class PHPWS_Text {
     $this->setText($text, $encoded);
   }
 
-  function setText($text, $encoded=FALSE)
+  function setText($text, $decode=TRUE)
   {
     if (empty($text) || !is_string($text)) {
       return;
     }
 
-    if ($encoded) {
+    if ($decode) {
       $this->text = html_entity_decode($text, ENT_QUOTES);
     } else {
       $this->text = $text;
     }
   }
 
+  function useBBcode($use = TRUE) {
+    $this->use_bbcode = $use;
+  }
 
   function useProfanity($use = TRUE)
   {
@@ -48,11 +53,6 @@ class PHPWS_Text {
   function useBreaker($use = TRUE)
   {
     $this->use_breaker = (bool)$use;
-  }
-
-  function useEncoded($use = TRUE)
-  {
-    $this->use_encoded = (bool)$use;
   }
 
   function useStripTags($use = TRUE)
@@ -95,8 +95,10 @@ class PHPWS_Text {
     }
     $text = $this->text;
 
-    $text = PHPWS_Text::parseBBCode($text);
-
+    if ($this->use_bbcode) {
+      $text = PHPWS_Text::bb2html($text, 'whatsthat');
+    }
+   
     if (!$this->use_profanity) {
       $text = PHPWS_Text::profanityFilter($text);
     }
@@ -121,23 +123,7 @@ class PHPWS_Text {
     return strip_tags($text, $allowed_tags);
   }
 
-  function parseBBCode($text)
-  {
-    require_once('HTML/BBCodeParser.php');
-    
-    // Set up BBCodeParser
-    $config  = parse_ini_file(PHPWS_SOURCE_DIR . '/config/core/BBCodeParser.ini', true);
-    $options = &PEAR::getStaticProperty('HTML_BBCodeParser', '_options');
-    $options = $config['HTML_BBCodeParser'];
-    unset($options);
-
-    // Parse BBCode
-    $parser = new HTML_BBCodeParser();
-    $parser->setText($text);
-    $parser->parse();
-    return $parser->getParsed();
-  }
-
+  
   /**
    * Removes profanity from a text string
    *
@@ -246,19 +232,10 @@ class PHPWS_Text {
   }// END FUNC breaker()
 
 
-  function parseInput($text){
-    return PHPWS_Text::prepare($text);
-  }
-
-   /**
-   * Prepares text for saving in the database.
-   *
-   * @author                       Matthew McNaney <matt at tux dot appstate dot edu>
-   * @param   string  text         Text to parse
-   */
-  function prepare($text)
-  {
-    $text = PHPWS_Text::encodeXHTML($text);
+  function parseInput($text, $encode=TRUE){
+    if ($encode) {
+      $text = PHPWS_Text::encodeXHTML($text);
+    }
     if (MAKE_ADDRESSES_RELATIVE) {
       PHPWS_Text::makeRelative($text);
     }
@@ -294,27 +271,17 @@ class PHPWS_Text {
    *
    * @author                       Matthew McNaney <matt at tux dot appstate dot edu>
    * @param   string  text         Text to parse
+   * @param   boolean decode       Whether entity_decoding should take place.
    * @return  string  text         Stripped text
    */
-  function parseOutput($text, $printTags = FALSE)
+  function parseOutput($text, $decode=TRUE)
   {
     $t = & new PHPWS_Text;
-    $t->setText($text, FALSE);
+    $t->setText($text, $decode);
 
     $text = $t->getPrint();
-    if ($printTags) {
-      return $t->encodeXHTML($text);
-    } else {
-      return $text;
-    }
+    return $text;
   }
-
-  function parseEncoded($text) {
-    $t = & new PHPWS_Text;
-    $t->setText($text, TRUE);
-    return $t->getPrint();
-  }
-
 
   /**
    * Checks the validity of text based on the type
@@ -539,6 +506,200 @@ class PHPWS_Text {
   {
     $GLOBALS['embedded_tags'][$module][] = $function_name;
   }
+
+  /**
+   * Gets smilie graphics for bbcode
+   *
+   * @author Matthew McNaney <matt at tux dot appstate dot edu>
+   */
+  function getSmilie($bbcode)
+  {
+    if (isset($GLOBALS['Smilie_Search'])) {
+      $search = &$GLOBALS['Smilie_Search']['code'];
+      $replace = &$GLOBALS['Smilie_Search']['img'];
+    } else {
+      $results = file_get_contents(PHPWS_HOME_DIR . './config/core/smiles.pak');
+      if (empty($results)) {
+	return $bbcode;
+      }
+      $smiles = explode("\n", $results);
+      foreach ($smiles as $row){
+	$icon = explode('=+:', $row);
+	
+	$search[] = '@' . preg_quote($icon[2]) . '@';
+	$replace[] = '<img src="' . PHPWS_HOME_DIR . '/images/core/smilies/'
+	  . $icon[0] . '" title="' . $icon[1]. '" />';
+      }
+      $GLOBALS['Smilie_Search']['code'] = $search;
+      $GLOBALS['Smilie_Search']['img'] = $replace;
+    }
+
+    $bbcode = preg_replace($search, $replace, $bbcode);
+    return $bbcode;
+  }
+
+  /**
+   * Parses bbcode
+   *
+   * This is a direct copy of corzblog's (http://corz.org/) blog code.
+   * Compared to Pear's bbcode parser, it is easier to use and edit.
+   * It has been edited slightly to work specifically with phpWebSite.
+   *
+   *
+   */
+  function bb2html($bb2html, $title) {
+    global  $insert_link, $effin_casinos, $spammer_preview;
+
+    /*	pre-formatted text (even bbcode inside [pre] text will remain untouched, as it should be)
+		there may be multiple <pre> blocks, so we grab them all and create an array	*/
+    $pre = array(); $i=0;
+    while ($pre_str = stristr($bb2html,'[pre]')) {
+      $pre_str = substr($pre_str,0,strpos($pre_str,'[/pre]')+6);
+      $bb2html = str_replace($pre_str, "***pre_string***$i", $bb2html);
+      $pre[$i] = str_replace("\r\n","\n",$pre_str);
+      $i++;
+    }
+
+	
+    // oh dem pesky casinos...
+    if($effin_casinos == true) {
+      if(stristr($bb2html, 'casino')) {
+	$bb2html = preg_replace("/\[url(.*)\](.*)\[\/url\]/i",
+				"[url=\"http://$insert_link\" title=\"hahahah\!\"]\$2[/url]", $bb2html);
+			
+	//buggers!
+	$bb2html = preg_replace("/<a (.*)>(.*)<\/a>/",
+				"[url=\"http://$insert_link\" title=\"hahahah\!\"]\$2[/url]", $bb2html);
+      }
+    }
+
+	
+    // now the bbcode proper..
+	
+    // grab any *real* square brackets first, store em
+    $bb2html = str_replace('[[', '**$@$**', $bb2html);
+    $bb2html = str_replace(']]', '**@^@**', $bb2html);
+	
+    // news headline block
+    $bb2html = str_replace('[news]', 
+			   '<table width="20%" border="0" align="right"><tr><td align="center"><span class="news"><b><big>', $bb2html);
+    $bb2html = str_replace('[/news]', '</big></b></span></td></tr></table>', $bb2html);
+	
+    // references - we need to create the whole string first, for the str_replace
+    $r1 = '<a href="#refs-'.$title.'" title="'.$title.'"><font class="ref"><sup>';
+    $bb2html = str_replace('[ref]', $r1 , $bb2html);
+    $r2 = '<p id="refs-'.$title.'"></p>
+<font class="ref"><b><u><a title="back to the text" href="javascript:history.go(-1)">references:</a></u><br><br>1: </b></font><font class="reftext">';
+    $bb2html = str_replace('[reftxt1]', $r2 , $bb2html);
+    $bb2html = str_replace('[reftxt2]', '<font class="ref"><b>2: </b></font><font class="reftext">', $bb2html);
+    $bb2html = str_replace('[reftxt3]', '<font class="ref"><b>3: </b></font><font class="reftext">', $bb2html);
+    $bb2html = str_replace('[reftxt4]', '<font class="ref"><b>4: </b></font><font class="reftext">', $bb2html);
+    $bb2html = str_replace('[reftxt5]', '<font class="ref"><b>5: </b></font><font class="reftext">', $bb2html);
+    $bb2html = str_replace('[/ref]', '</sup></font></a>', $bb2html);
+    $bb2html = str_replace('[/reftxt]', '</font>', $bb2html);
+	
+    // ordinary transformations..
+    // we rely on the browser producing \r\n (DOS) carriage returns, as per spec
+    $bb2html = str_replace("\r",'<br>', $bb2html);	// the \n remains, makes the raw html readable
+    $bb2html = str_replace('[b]', '<b>', $bb2html);
+    $bb2html = str_replace('[/b]', '</b>', $bb2html);
+    $bb2html = str_replace('[i]', '<i>', $bb2html);
+    $bb2html = str_replace('[/i]', '</i>', $bb2html);
+    $bb2html = str_replace('[u]', '<u>', $bb2html);
+    $bb2html = str_replace('[/u]', '</u>', $bb2html);
+    $bb2html = str_replace('[big]', '<big>', $bb2html);
+    $bb2html = str_replace('[/big]', '</big>', $bb2html);
+    $bb2html = str_replace('[sm]', '<small>', $bb2html);
+    $bb2html = str_replace('[/sm]', '</small>', $bb2html);
+	
+	
+    // tables (couldn't resist this, too handy)
+    $bb2html = str_replace('[t]', '<table width="100%" border=0 cellspacing=0 cellpadding=0>', $bb2html);	
+    $bb2html = str_replace('[bt]', '<table width="100%" border=1 cellspacing=0 cellpadding=3>', $bb2html);
+    $bb2html = str_replace('[st]', '<table width="100%" border=0 cellspacing=3 cellpadding=3>', $bb2html);	
+    $bb2html = str_replace('[/t]', '</table>', $bb2html);
+    $bb2html = str_replace('[c]', '<td valign=top>', $bb2html);	// cell data
+    $bb2html = str_replace('[/c]', '</td>', $bb2html);
+    $bb2html = str_replace('[r]', '<tr>', $bb2html);	// a row
+    $bb2html = str_replace('[/r]', '</tr>', $bb2html);
+	
+    // a simple list
+    $bb2html = str_replace('[*]', '<li>', $bb2html);
+    $bb2html = str_replace('[list]', '<ul>', $bb2html);
+    $bb2html = str_replace('[/list]', '</ul>', $bb2html);
+	
+    // smilies (just starting these, *ahem*) ..
+    
+    if (ALLOW_SMILIES) {
+      $bb2html = PHPWS_Text::getSmilie($bb2html);
+    }
+
+    // anchors and stuff..
+    $bb2html = str_replace('[img]', '<img border="0" src="', $bb2html);
+    $bb2html = str_replace('[/img]', '" alt="an image">', $bb2html);
+    $bb2html = str_replace('[url=', '<a target="_blank" href=', $bb2html);
+    $bb2html = str_replace('[/url]', '</a>', $bb2html);
+    // Added for phpwebsite
+    $bb2html = preg_replace('/\[quote=([\w\s\.]+)\]/', '<blockquote><b>\\1 ' . _('said') . ':</b><br />', $bb2html);
+
+    $bb2html = str_replace('[quote]', '<blockquote>', $bb2html);
+    $bb2html = str_replace('[/quote]', '</blockquote>', $bb2html);
+
+    // code
+    $bb2html = str_replace('[code]', '<div class
+="simcode">', $bb2html);
+    $bb2html = str_replace('[coderz]', '<div class="code">', $bb2html);
+    $bb2html = str_replace('[/code]', '</div>', $bb2html);
+    $bb2html = str_replace('[/coderz]', '</div>', $bb2html); // you can complete either way, it's all [/code]
+	
+    // divisions..
+    $bb2html = str_replace('[hr]', '<hr size=1 width="70%" align=center>', $bb2html);
+    $bb2html = str_replace('[block]', '<blockquote>', $bb2html);
+    $bb2html = str_replace('[/block]', '</blockquote>', $bb2html);
+	
+    // dropcaps. five flavours, small up to large.. [dc1]I[/dc] >> [dc5]W[/dc]
+    $bb2html = str_replace('[dc1]', '<span class="dropcap1">', $bb2html);
+    $bb2html = str_replace('[dc2]', '<span class="dropcap2">', $bb2html);
+    $bb2html = str_replace('[dc3]', '<span class="dropcap3">', $bb2html);
+    $bb2html = str_replace('[dc4]', '<span class="dropcap4">', $bb2html);
+    $bb2html = str_replace('[dc5]', '<span class="dropcap5">', $bb2html);
+    $bb2html = str_replace('[/dc]', '<dc></span>', $bb2html);
+	
+    // special characters (html entity encoding) ..
+    $bb2html = str_replace('[sp]', '&nbsp;', $bb2html);
+    $bb2html = str_replace('±', '&plusmn;', $bb2html);
+    $bb2html = str_replace('™', '&trade;', $bb2html);
+    $bb2html = str_replace('•', '&bull;', $bb2html);
+    $bb2html = str_replace('°', '&deg;', $bb2html);
+    $bb2html = str_replace('[<]', '&lt;', $bb2html);
+    $bb2html = str_replace('[>]', '&gt;', $bb2html);
+    $bb2html = str_replace('©', '&copy;', $bb2html);
+    $bb2html = str_replace('®', '&reg;', $bb2html);
+    $bb2html = str_replace('…', '&hellip;', $bb2html);
+	
+    // for URL's..
+    $bb2html = str_replace(']', ' >', $bb2html);	
+	
+    // get back those square brackets..
+    $bb2html = str_replace('**$@$**', '[', $bb2html);
+    $bb2html = str_replace('**@^@**', ']', $bb2html);
+	
+    // re-insert the preformatted text blocks..
+    $cp = count($pre)-1;
+    for($i=0;$i <= $cp;$i++) {
+      $bb2html = str_replace("***pre_string***$i", '<pre>'.substr($pre[$i],5,-6).'</pre>', $bb2html);
+    }
+	
+    // prevent some idiot running arbitary php commands on our web server
+    $bb2html = preg_replace("/<\?(.*)\?>/i", "<b>script-kiddie prank: \\1</b>", $bb2html);
+	
+    // slash me!
+    if (get_magic_quotes_gpc()) {
+      return stripslashes($bb2html);
+    } else {
+      return ($bb2html);
+    }
+  }/* end function bb2html($bb2html, $title) */
 
 }//END CLASS CLS_text
 
