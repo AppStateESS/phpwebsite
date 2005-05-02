68ec9111a0dc75bb76c1ffb0b84b46eb9d3f5f36
Matt McNaney <matt@tux.appstate.edu>
2005-05-02 20:45:44 +0000

    added admin option (none written yet)

--- mod/comments/index.php
+++ mod/comments/index.php
@@ -9,7 +9,7 @@ PHPWS_Core::initModClass('comments', 'Comments.php');
 if (isset($_REQUEST['user_action'])) {
   Comments::userAction($_REQUEST['user_action']);
 } elseif (isset($_REQUEST['admin_action'])&& Current_User::authorized('comments')) {
-  Comments::userAction($_REQUEST['admin_action']);
+  Comments::adminAction($_REQUEST['admin_action']);
 }
 
 ?>
