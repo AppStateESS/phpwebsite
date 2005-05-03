8480b32c1e7ccbbe9e71a563e246853df73b804d
Matt McNaney <matt@tux.appstate.edu>
2005-05-03 17:23:33 +0000

    added some sorting devices pulling source_values to allow the sorting form elements to work properly

--- mod/comments/class/Comment_Thread.php
+++ mod/comments/class/Comment_Thread.php
@@ -13,14 +13,15 @@ PHPWS_Core::initModClass('comments', 'Comment_Item.php');
 define('NO_COMMENTS_FOUND', 'none');
 
 class Comment_Thread {
-  var $id          = 0;
-  var $module      = NULL;
-  var $item_name   = NULL;
-  var $item_id     = NULL;
-  var $source_url = NULL;
-  var $_key        = NULL;
-  var $_comments   = NULL;
-  var $_error      = NULL;
+  var $id             = 0;
+  var $module         = NULL;
+  var $item_name      = NULL;
+  var $item_id        = NULL;
+  var $source_url     = NULL;
+  var $_source_values = NULL;
+  var $_key           = NULL;
+  var $_comments      = NULL;
+  var $_error         = NULL;
 
 
   function Comment_Thread($id=NULL)
@@ -42,6 +43,7 @@ class Comment_Thread {
       PHPWS_Error::log($result);
       $this->_error = $result->getMessage();
     }
+    $this->buildSourceValues();
   }
 
   function countComments($formatted=FALSE)
@@ -112,9 +114,26 @@ class Comment_Thread {
     return $this->id;
   }
 
-  function setSourceUrl($link)
+  function setSourceUrl($link=NULL)
   {
-    $this->source_url = $link;
+    if (isset($link)) {
+      $link = str_replace('&amp;', '&', $link);
+      $this->source_url = stristr($link, 'index.php?');
+    } else {
+      $this->source_url = $_SERVER['argv'][0];
+    }
+  }
+
+  function buildSourceValues()
+  {
+    $url = parse_url($this->source_url);
+    parse_str($url['path'], $output);
+    $this->_source_values = $output;
+  }
+
+  function getSourceValues()
+  {
+    return $this->_source_values;
   }
 
   function getSourceUrl($full=FALSE)
@@ -154,7 +173,7 @@ class Comment_Thread {
   {
     $vars['user_action']   = 'post_comment';
     $vars['thread_id']     = $this->id;
-    return PHPWS_Text::moduleLink(_('Post Comment'), 'comments', $vars);
+    return PHPWS_Text::moduleLink(_('Post New Comment'), 'comments', $vars);
   }
 
   function save()
@@ -163,24 +182,108 @@ class Comment_Thread {
     return $db->saveObject($this);
   }
 
+  function _getTimePeriod()
+  {
+    switch ($_GET['time_period']) {
+    case 'today':
+      return gmmktime(0,0,0);
+      break;
+
+    case 'yd':
+      return gmmktime(0,0,0, gmdate('m'), gmdate('d')-1);
+      break;
+
+    case 'week':
+      return gmmktime(0,0,0, gmdate('m'), gmdate('d')-7);
+      break;
+
+    case 'month':
+      return gmmktime(0,0,0, gmdate('m')-1);
+      break;
+    }
+
+  }
+
   function view()
   {
     PHPWS_Core::initCoreClass('DBPager.php');
 
-    $page_tags['NEW_POST_LINK'] = $this->postLink();
+    $time_period = array('all'    => _('All'),
+			 'today'  => _('Today'),
+			 'yd'     => _('Since yesterday'),
+			 'week'   => _('This week'),
+			 'month'  => _('This month')
+			 );
+
+    $order_list = array('old_all'  => _('Oldest first'),
+			'new_all'  => _('Newest first'),
+			'old_base' => _('Oldest first - Top'),
+			'new_base' => _('Newest first - Top')
+			);
+
     $pager = & new DBPager('comments_items', 'Comment_Item');
+    $form = & new PHPWS_Form;
+    $form->addSelect('time_period', $time_period);
+    $form->addSelect('order', $order_list);
+
+    // set where clauses
+    if (isset($_GET['time_period']) && $_GET['time_period'] != 'all') {
+      $form->setMatch('time_period', $_GET['time_period']);
+      $time_period = $this->_getTimePeriod();
+      $pager->addWhere('create_time', $time_period, '>=');
+    }
+
+    if (!empty($parent_id)) {
+      $pager->addWhere('parent', (int)$parent_id);
+    }
+    $pager->addWhere('thread_id', $this->id);
+
+    if (isset($_GET['order'])) {
+      switch ($_GET['order']) {
+      case 'new_all':
+	$pager->setOrder('create_time', 'desc');
+	break;
+
+      case 'old_all':
+	$pager->setOrder('create_time', 'asc');
+	break;
+
+      case 'new_base':
+	$pager->setOrder('create_time', 'desc');
+	break;
+
+      case 'old_base':
+	$pager->setOrder('create_time', 'asc');
+	break;
+      }
+      $form->setMatch('order', $_GET['order']);
+    }
+
+    $form->noAuthKey();
+    $form->addHidden($this->getSourceValues());
+    $form->addSubmit(_('Go'));
+    $form->setMethod('get');
+
+    $page_tags = $form->getTemplate();
+    $page_tags['NEW_POST_LINK'] = $this->postLink();
+
+
     $pager->setModule('comments');
     $pager->setTemplate('view.tpl');
     $pager->setLink($this->source_url);
     $pager->addPageTags($page_tags);
     $pager->addRowTags('getTpl');
-    if (!empty($parent_id)) {
-      $pager->addWhere('parent', (int)$parent_id);
-    }
-    $pager->addWhere('thread_id', $this->id);
+
+
+
+
     $pager->setLimitList(array(10, 20, 50));
     $pager->setEmptyMessage(_('No comments'));
     $content = $pager->get();
+    /*
+    echo gmmktime();
+    echo    PHPWS_DB::lastQuery();
+    */
     return $content;
   }
 
