f8aacd127d17036f44db43a44d8598ff27ae0194
Matt McNaney <matt@tux.appstate.edu>
2005-05-24 18:15:28 +0000

    added the users table

--- mod/comments/boost/install.sql
+++ mod/comments/boost/install.sql
@@ -3,9 +3,9 @@ CREATE TABLE comments_items (
   thread_id int NOT NULL default '0',
   parent int NOT NULL default '0',
   author_ip varchar(15) NOT NULL default '',
+  author_id int NOT NULL default '0',
   subject varchar(100) NOT NULL default '',
   entry text NOT NULL,
-  author_id int NOT NULL default '0',
   edit_author varchar(50) default NULL,
   create_time int NOT NULL default '0',
   edit_time int NOT NULL default '0',
@@ -23,3 +23,16 @@ CREATE TABLE comments_threads (
   last_poster varchar(40) default NULL,
   PRIMARY KEY  (id)
 );
+
+CREATE TABLE comments_users (
+  user_id int(11) NOT NULL default '0',
+  display_name varchar(50) NOT NULL default '',
+  signature varchar(255) default NULL,
+  comments_made int(11) NOT NULL default '0',
+  joined_date int(11) NOT NULL default '0',
+  picture varchar(255) default NULL,
+  contact_email varchar(255) default NULL,
+  website varchar(255) default NULL,
+  location varchar(128) default NULL,
+  locked smallint(6) NOT NULL default '0'
+) 
