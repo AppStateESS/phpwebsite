a6630ff7b3a7b6ebe54ea5501615a36ac61ce789
Matt McNaney <matt@tux.appstate.edu>
2005-06-08 17:34:27 +0000

    double to single quotes

--- mod/blog/class/Blog_User.php
+++ mod/blog/class/Blog_User.php
@@ -39,27 +39,27 @@ class Blog_User {
   }
 
   function show(){
-    $key = "front blog page";
+    $key = 'front blog page';
 
     if (!Current_User::isLogged()    &&
-	!Current_User::allow("blog") &&
+	!Current_User::allow('blog') &&
 	$content = PHPWS_Cache::get($key)) {
       return $content;
     }
 
     $limit = 5;
 
-    $db = & new PHPWS_DB("blog_entries");
+    $db = & new PHPWS_DB('blog_entries');
     $db->setLimit($limit);
     $db->addOrder('date desc');
     if (!Current_User::isLogged()) {
       $db->addWhere('restricted', '0');
     }
 
-    $result = $db->getObjects("Blog");
+    $result = $db->getObjects('Blog');
 
     if (empty($result))
-      return ("No blog entries found.");
+      return ('No blog entries found.');
     
     foreach ($result as $blog) {
       if ($blog->getRestricted() == 2 &&
@@ -72,8 +72,8 @@ class Blog_User {
       }
     }
 
-    $content = implode("", $list);
-    if (!Current_User::allow("blog")) {
+    $content = implode('', $list);
+    if (!Current_User::allow('blog')) {
       PHPWS_Cache::save($key, $content);
     }
 
