b636b9d9c1cae81f2ec4a7773f9cebb949adb54d
Matt McNaney <matt@tux.appstate.edu>
2005-06-10 18:57:50 +0000

    pretty close to final table structure here

--- mod/menu/boost/install.sql
+++ mod/menu/boost/install.sql
@@ -1,9 +1,7 @@
 CREATE TABLE menu_links (
   id int NOT NULL default '0',
-  module varchar(30) NOT NULL default '',
-  item_name varchar(30) NOT NULL default '',
-  item_id int NOT NULL default '0',
   menu_id int NOT NULL default '0',
+  title varchar(50) NOT NULL default '',
   url varchar(255) default NULL,
   parent int NOT NULL default '0',
   active smallint NOT NULL default '0',
@@ -15,9 +13,13 @@ CREATE TABLE menus (
   id int NOT NULL default '0',
   title varchar(30) NOT NULL default '',
   template varchar(50) NOT NULL default '',
-  module varchar(30) default NULL,
-  item_name varchar(30) default NULL,
-  item_id int NOT NULL default '0',
   pin_all smallint NOT NULL default '0',
   PRIMARY KEY  (id)
+);
+
+CREATE TABLE menu_assoc (
+  menu_id int NOT NULL default '0',
+  module varchar(40) NOT NULL default '',
+  item_name varchar(40) NOT NULL default '',
+  item_id int NOT NULL default '0'
 );
