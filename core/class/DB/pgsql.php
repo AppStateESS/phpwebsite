c7e530a477e8421f39cfec1ebafee5ee4acdb56d
Matt McNaney <matt@tux.appstate.edu>
2005-06-28 13:20:48 +0000

    tweaked to work better tabified

--- core/class/DB/pgsql.php
+++ core/class/DB/pgsql.php
@@ -2,83 +2,98 @@
 
 class PHPWS_SQL {
 
-  function export(&$info){
-    switch ($info['type']){
+    function export(&$info){
+        switch ($info['type']){
 
-    case "int8":
-    case "int4":
-      $setting = "INT";
-      $info['flags'] = preg_replace("/unique primary/", "PRIMARY KEY", $info['flags']);
-      break;
+        case 'int8':
+        case 'int4':
+        case 'int':
+            $setting = 'INT';
+            $info['flags'] = preg_replace('/unique primary/', 'PRIMARY KEY', $info['flags']);
+            break;
 
-    case "int2":
-      $setting = "SMALLINT";
-      break;
+        case 'int2':
+            $setting = 'SMALLINT';
+            break;
 
-    case "text":
-    case "blob":
-      $setting = "TEXT";
-      $info['flags'] = NULL;
-      break;
+        case 'text':
+        case 'blob':
+            $setting = 'TEXT';
+            if (stristr($info['flags'], 'not_null')) {
+                $info['flags'] = 'NOT NULL';
+            }
+            break;
 
-    case "bpchar":
-      $setting = "CHAR(255)";
+        case 'varchar':
+            $setting = 'VARCHAR(255)';
+            if (stristr($info['flags'], 'not_null')) {
+                $info['flags'] = 'NOT NULL';
+            }
+            break;
 
-      if (empty($info['flags']))
-	$info['flags'] = "NULL";
-      break;
+        case 'bpchar':
+            $setting = 'CHAR(255)';
+
+            if (empty($info['flags'])) {
+                $info['flags'] = 'NULL';
+            }
+            break;
     
-    case "date":
-      $setting = "DATE";
-      break;
+        case 'date':
+            $setting = 'DATE';
+            break;
     
-    case "real":
-      $setting = "FLOAT";
-      break;
+        case 'real':
+            $setting = 'FLOAT';
+            break;
     
-    case "timestamp":
-      $setting = "TIMESTAMP";
-      $info['flags'] = NULL;
-      break;
+        case 'timestamp':
+            $setting = 'TIMESTAMP';
+            $info['flags'] = NULL;
+            break;
 
-    default:
-      exit(_("Unknown column type:") . " " . $info['type']);
-      break;
-    }
-    return $setting;
-  }
-
-  function getLimit($limit){
-    $sql[] = "LIMIT";
-
-    if (isset($limit['offset'])) {
-      $sql[] = $limit['offset'];
-      $sql[] = "OFFSET";
-    }
-
-    $sql[] = $limit['total'];
-    
-    return implode(" ", $sql);
-  }
-
-  function readyImport(&$query){
-
-    $from = array('/datetime/i',
-		  '/double\((\d+),(\d+)\)/Uie'
-		  );
-    $to   = array('timestamp without time zone',
-		  "'numeric(' . (\\1 + \\2) . ', \\2)'"
-		  );
-    $query = preg_replace($from, $to, $query);
-
-    if (preg_match("/id int [\w\s]* primary key[\w\s]*,/iU", $query)){
-      $tableName = PHPWS_DB::extractTableName($query);
-
-      $query = preg_replace('/primary key/i', '', $query);
-      $query .= 'CREATE UNIQUE INDEX id ON ' . $tableName . "(id);";
+        default:
+            exit(_('Unknown column type:') . ' ' . $info['type']);
+            break;
+        }
+        return $setting;
     }
 
-  }
+    function getLimit($limit){
+        $sql[] = 'LIMIT';
+
+        if (isset($limit['offset'])) {
+            $sql[] = $limit['offset'];
+            $sql[] = 'OFFSET';
+        }
+
+        $sql[] = $limit['total'];
+    
+        return implode(' ', $sql);
+    }
+
+    function readyImport(&$query){
+
+        $from = array('/datetime/i',
+                      '/double\((\d+),(\d+)\)/Uie'
+                      );
+        $to   = array('timestamp without time zone',
+                      "'numeric(' . (\\1 + \\2) . ', \\2)'"
+                      );
+        $query = preg_replace($from, $to, $query);
+
+        if (preg_match('/id int [\w\s]* primary key[\w\s]*,/iU', $query)){
+            $tableName = PHPWS_DB::extractTableName($query);
+
+            $query = preg_replace('/primary key/i', '', $query);
+
+            $query = preg_replace('/\);/', ', PRIMARY KEY (id));', $query);
+            /*
+      $query .= sprintf('CREATE UNIQUE INDEX %s_%s ON %s (id);',
+                        $tableName, 'id', $tableName);
+            */
+        }
+    }
 
 }
 
