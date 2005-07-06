0e4938760d47c72c0aa23370d73b170218cce7f4
Matt McNaney <matt@tux.appstate.edu>
2005-07-06 17:31:42 +0000

    moving from the module directory

---
+++ core/class/Security.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Handles the logging and routing of security problems
+ *
+ * @author Matthew McNaney <matt at tux dot appstate dot edu>
+ * @version $Id$
+ */
+
+class Security {
+    function log($message){
+        translate('core');
+        if (class_exists('Current_User')) {
+            $username = Current_User::getUsername();
+        } else {
+            $username = _('Unknown user');
+        }
+
+        $ip = $_SERVER['REMOTE_ADDR'];
+
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            $via = sprintf(_('Coming from: %s'), $_SERVER['HTTP_REFERER']);
+        }
+        else {
+            $via = _('Unknown source');
+        }
+
+        $infraction = sprintf('%s@%s %s -- %s', $username, $ip, $via, $message);
+
+        PHPWS_Core::log($infraction, 'security.log', _('Warning'));
+    }
+}
+
+?>
