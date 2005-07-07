e84cb8eebec182a311db4b1ee3bfb1563d26b4d9
Matt McNaney <matt@tux.appstate.edu>
2005-07-07 17:44:58 +0000

    header comments and tabs added/fixed fixed display problems

--- mod/block/class/Block.php
+++ mod/block/class/Block.php
@@ -1,61 +1,72 @@
 <?php
+
+/**
+ * Command class for block
+ *
+ * @author Matthew McNaney <matt at tux dot appstate dot edu>
+ * @version $Id$
+ */
+
 PHPWS_Core::initModClass('block', 'Block_Item.php');
 
 class Block {
 
-  function show($key)
-  {
-    Block::showBlocks($key);
+    function show($key)
+    {
+        Block::showBlocks($key);
 
-    if (isset($_SESSION['Stored_Blocks'])) {
-      Block::viewStoredBlocks($key);
-    }
+        if (isset($_SESSION['Stored_Blocks'])) {
+            Block::viewStoredBlocks($key);
+        }
   
-  }
-
-  function viewStoredBlocks($key)
-  {
-    if (!isset($_SESSION['Stored_Blocks'])) {
-      return FALSE;
     }
 
-    $block_list = &$_SESSION['Stored_Blocks'];
-    if (empty($block_list)) {
-      return NULL;
+    function viewStoredBlocks($key)
+    {
+        if (!isset($_SESSION['Stored_Blocks'])) {
+            return FALSE;
+        }
+
+        $block_list = &$_SESSION['Stored_Blocks'];
+        if (empty($block_list)) {
+            return NULL;
+        }
+
+        foreach ($block_list as $block_id => $block) {
+            if (isset($GLOBALS['Current_Blocks'][$block_id])) {
+                continue;
+            }
+
+            $block->setPinKey($key);
+            $content[] = $block->view(TRUE);
+        }
+
+        if (empty($content)) {
+            return;
+        }
+
+        $complete = implode('', $content);
+        Layout::add($complete, 'block', 'Block_List');
     }
 
-    foreach ($block_list as $block_id => $block) {
-      if (isset($GLOBALS['Current_Blocks'][$block_id])) {
-	continue;
-      }
+    function showBlocks($key)
+    {
+        $key->setTable('block_pinned');
+        $key->setColumnName('block_id');
+        $result = $key->getMatches();
 
-      $content[] = $block->view($key, TRUE);
+        if (empty($result)) {
+            return;
+        }
+
+        foreach ($result as $block_id) {
+            $block = & new Block_Item($block_id);
+            $block->setPinKey($key);
+            Layout::add($block->view(), 'block', $block->getContentVar());
+            $GLOBALS['Current_Blocks'][$block_id] = TRUE;
+        }
+
     }
-
-    if (empty($content)) {
-      return;
-    }
-    $complete = implode('', $content);
-    Layout::add($complete, 'block', 'Block_List', FALSE);
-  }
-
-  function showBlocks($key)
-  {
-    $key->setTable('block_pinned');
-    $key->setColumnName('block_id');
-    $result = $key->getMatches();
-
-    if (empty($result)) {
-      return;
-    }
-
-    foreach ($result as $block_id) {
-      $block = & new Block_Item($block_id);
-      Layout::add($block->view($key), 'block', $block->getContentVar(), FALSE);
-      $GLOBALS['Current_Blocks'][$block_id] = TRUE;
-    }
-
-  }
 
 }
 
