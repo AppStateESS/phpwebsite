034278050f9a656c77e339da74e44dcf321f793c
Matt McNaney <matt@tux.appstate.edu>
2005-07-07 17:50:20 +0000

    added error for fubar box order

--- mod/layout/conf/error.php
+++ mod/layout/conf/error.php
@@ -1,11 +1,11 @@
 <?php
 $errors = array(
-		LAYOUT_SESSION_NOT_SET   => _("Layout session not set"),
-		LAYOUT_NO_CONTENT        => _("Layout did not receive any data for display"),
-		LAYOUT_NO_THEME          => _("Unable to receive theme information."),
-		LAYOUT_BAD_JS_DATA       => _("Data was not an array."),
-		LAYOUT_JS_FILE_NOT_FOUND => _("Javascript file was not found.")
-		
+		LAYOUT_SESSION_NOT_SET   => _('Layout session not set'),
+		LAYOUT_NO_CONTENT        => _('Layout did not receive any data for display'),
+		LAYOUT_NO_THEME          => _('Unable to receive theme information.'),
+		LAYOUT_BAD_JS_DATA       => _('Data was not an array.'),
+		LAYOUT_JS_FILE_NOT_FOUND => _('Javascript file was not found.'),
+                LAYOUT_BOX_ORDER_BROKEN  => _('Box order is out of sequence.')
 );
 
 
