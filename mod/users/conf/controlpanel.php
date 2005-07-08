e50c7dc6bfc4a07d27046315f7c85df9e5b6b43d
Matt McNaney <matt@tux.appstate.edu>
2005-07-08 16:53:24 +0000

    fixed quotes

--- mod/users/conf/controlpanel.php
+++ mod/users/conf/controlpanel.php
@@ -1,16 +1,16 @@
 <?php
 
-$tabs[] = array("title" => _("My Page"),
-		"label" => "my_page",
-		"link"  => "index.php?module=users&amp;action=user",
+$tabs[] = array('title' => _('My Page'),
+		'label' => 'my_page',
+		'link'  => 'index.php?module=users&amp;action=user',
 		);
 
-$link[] = array("label"       => _("User Administration"),
-		"restricted"  => TRUE,
-		"url"         => "index.php?module=users&amp;action=admin",
-		"description" => _("Lets you create and edit users and groups."),
-		"image"       => "users.png",
-		"tab"         => "admin"
+$link[] = array('label'       => _('User Administration'),
+		'restricted'  => TRUE,
+		'url'         => 'index.php?module=users&amp;action=admin',
+		'description' => _('Lets you create and edit users and groups.'),
+		'image'       => 'users.png',
+		'tab'         => 'admin'
 		);
 
 ?>
