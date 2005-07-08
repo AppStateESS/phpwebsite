e16ccc3a1788f7a06908251fb610a83150e31f6c
Matt McNaney <matt@tux.appstate.edu>
2005-07-08 13:29:51 +0000

    commented and tabbed

--- mod/filecabinet/conf/controlpanel.php
+++ mod/filecabinet/conf/controlpanel.php
@@ -1,10 +1,15 @@
 <?php
 
-$link[] = array("label"       => _("File Cabinet"),
-		 "restricted"  => TRUE,
-		 "url"         => "index.php?module=filecabinet&amp;action=main",
-		 "description" => _("Manages images and documents uploaded to your site."),
-		 "image"       => "cabinet.png",
-		 "tab"         => "admin"
-		 );
+/**
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @version $Id$
+ */
+
+$link[] = array('label'       => _('File Cabinet'),
+                'restricted'  => TRUE,
+                'url'         => 'index.php?module=filecabinet&amp;action=main',
+                'description' => _('Manages images and documents uploaded to your site.'),
+                'image'       => 'cabinet.png',
+                'tab'         => 'admin'
+                );
 ?>
