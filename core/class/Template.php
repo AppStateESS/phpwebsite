835f856fca30341fd25d919535a5bc7c6ca7e675
Matt McNaney <matt@tux.appstate.edu>
2005-07-20 19:50:35 +0000

    I think just clean up

--- core/class/Template.php
+++ core/class/Template.php
@@ -14,203 +14,217 @@ require_once 'config/core/template.php';
  */
 
 class PHPWS_Template extends HTML_Template_Sigma {
-  var $module           = NULL;
-  var $error            = NULL;
-  var $lastTemplatefile = NULL;
+    var $module           = NULL;
+    var $error            = NULL;
+    var $lastTemplatefile = NULL;
 
-  function PHPWS_Template($module=NULL, $file=NULL){
-    $this->HTML_Template_Sigma();
-    if (isset($module))
-      $this->setModule($module);
+    function PHPWS_Template($module=NULL, $file=NULL)
+    {
+        $this->HTML_Template_Sigma();
+        if (isset($module))
+            $this->setModule($module);
 
-    if (isset($file)){
-      $result = $this->setFile($file);
+        if (isset($file)){
+            $result = $this->setFile($file);
 
-      if (PEAR::isError($result)){
-	PHPWS_Error::log($result);
-	$this->error = $result;
-      }
+            if (PEAR::isError($result)){
+                PHPWS_Error::log($result);
+                $this->error = $result;
+            }
+        }
     }
-  }
 
-  function getTplDir($module){
-    if (!class_exists('Layout')) {
-      return PHPWS_SOURCE_DIR . "mod/$module/templates/";    
-    }
+    function getTplDir($module)
+    {
+        if (!class_exists('Layout')) {
+            return PHPWS_SOURCE_DIR . "mod/$module/templates/";    
+        }
     
-    $theme = Layout::getThemeDir();
-    return $theme . 'templates/' . $module . "/";
-  }
-
-  function setCache(){
-    if (!PHPWS_Template::allowSigmaCache() ||
-	!defined('CACHE_DIRECTORY')        ||
-	!defined('CACHE_LIFETIME')         ||
-	!is_writable(CACHE_DIRECTORY)
-	)
-      return;
-
-    $this->setCacheRoot(CACHE_DIRECTORY);
-  }
-
-  function allowSigmaCache(){
-    if (defined('ALLOW_SIGMA_CACHE'))
-      return ALLOW_SIGMA_CACHE;
-    else
-      return FALSE;
-  }
-
-  function getTemplateDirectory($module, $directory=NULL)
-  {
-    $theme_dir = PHPWS_Template::getTplDir($module) . $directory;
-    $local_dir = sprintf('./templates/%s/', $module) . $directory;
-    $module_dir = sprintf('./mod/%s/templates/', $module) . $directory;
-
-    if (FORCE_THEME_TEMPLATES && is_dir($theme_dir)) {
-      return $theme_dir;
-    } elseif (FORCE_MOD_TEMPLATES && is_dir($module_dir)) {
-      return $module_dir;
-    } elseif (is_dir($local_dir)) {
-      return $local_dir;
-    } else {
-      return NULL;
-    }
-  }
-
-  /**
-   * Lists the template files in a directory.
-   * Can be called statically. 
-   */
-  function listTemplates($module, $directory=NULL)
-  {
-    $tpl_dir = PHPWS_Template::getTemplateDirectory($module, $directory);
-
-    if (!$result = scandir($tpl_dir)) {
-      return NULL;
+        $theme = Layout::getThemeDir();
+        return $theme . 'templates/' . $module . "/";
     }
 
-    foreach ($result as $file) {
-      if (!preg_match('/(^\.)|(~$)/iU', $file) && preg_match('/\.tpl$/i', $file)) {
-	$file_list[$file] = $file;
-      }
+    function setCache()
+    {
+        if (!PHPWS_Template::allowSigmaCache() ||
+            !defined('CACHE_DIRECTORY')        ||
+            !defined('CACHE_LIFETIME')         ||
+            !is_writable(CACHE_DIRECTORY)
+            )
+            return;
+
+        $this->setCacheRoot(CACHE_DIRECTORY);
     }
 
-    return $file_list;
-  }
-
-  function setFile($file, $strict=FALSE){
-    $module = $this->getModule();
-    $this->setCache();
-    if ($strict == TRUE) {
-      $result = $this->loadTemplatefile($file);
-    } else {
-      $altFile = PHPWS_Template::getTplDir($module) . $file;
-
-      if (PEAR::isError($altFile)) {
-	return $altFile;
-      }
-
-      if (FORCE_THEME_TEMPLATES || is_file($altFile)) {
-	$result = $this->loadTemplatefile($altFile);
-      } elseif (FORCE_MOD_TEMPLATES) {
-	$file = PHPWS_SOURCE_DIR . "mod/$module/templates/$file";
-	$result = $this->loadTemplatefile($file);
-      } else {
-	$file = "templates/$module/$file";
-	$result = $this->loadTemplatefile($file);
-      }
+    function allowSigmaCache()
+    {
+        if (defined('ALLOW_SIGMA_CACHE'))
+            return ALLOW_SIGMA_CACHE;
+        else
+            return FALSE;
     }
 
-    if ($result) {
-      $this->lastTemplatefile = $file;
-      return $result;
-    }
-    else 
-      return $this->err[0];
-  }
+    function getTemplateDirectory($module, $directory=NULL)
+    {
+        $theme_dir  = PHPWS_Template::getTplDir($module) . $directory;
+        $local_dir  = sprintf('./templates/%s/', $module) . $directory;
+        $module_dir = sprintf('./mod/%s/templates/', $module) . $directory;
 
-  function setModule($module){
-    $this->module = $module;
-  }
-
-  function getModule(){
-    return $this->module;
-  }
-
-  function setData($data){
-    if (PEAR::isError($data)){
-      PHPWS_Error::log($data);
-      return NULL;
+        if (FORCE_THEME_TEMPLATES && is_dir($theme_dir)) {
+            return $theme_dir;
+        } elseif (FORCE_MOD_TEMPLATES && is_dir($module_dir)) {
+            return $module_dir;
+        } elseif (is_dir($local_dir)) {
+            return $local_dir;
+        } else {
+            return NULL;
+        }
     }
 
-    foreach($data as $tag=>$content)
-      $this->setVariable($tag, $content);
-  }
+    /**
+     * Lists the template files in a directory.
+     * Can be called statically. 
+     */
+    function listTemplates($module, $directory=NULL)
+    {
+        $tpl_dir = PHPWS_Template::getTemplateDirectory($module, $directory);
 
-  function getLastTplFile(){
-    return $this->getfile($this->lastTemplatefile);
-  }
+        if (!$result = scandir($tpl_dir)) {
+            return NULL;
+        }
 
-  function process($template, $module, $file){
-    if (!is_array($template)) {
-      return PHPWS_Error::log(PHPWS_VAR_TYPE, 'core', 
-		       'PHPWS_Template::process', 
-		       'template=' . gettype($template));
-      return NULL;
+        foreach ($result as $file) {
+            if (!preg_match('/(^\.)|(~$)/iU', $file) && preg_match('/\.tpl$/i', $file)) {
+                $file_list[$file] = $file;
+            }
+        }
+
+        return $file_list;
     }
-    if (PEAR::isError($template)){
-      PHPWS_Error::log($template);
-      return NULL;
+
+    function setFile($file, $strict=FALSE)
+    {
+        $module = $this->getModule();
+        $this->setCache();
+        if ($strict == TRUE) {
+            $result = $this->loadTemplatefile($file);
+        } else {
+            $altFile = PHPWS_Template::getTplDir($module) . $file;
+
+            if (PEAR::isError($altFile)) {
+                return $altFile;
+            }
+
+            if (FORCE_THEME_TEMPLATES || is_file($altFile)) {
+                $result = $this->loadTemplatefile($altFile);
+            } elseif (FORCE_MOD_TEMPLATES) {
+                $file = PHPWS_SOURCE_DIR . "mod/$module/templates/$file";
+                $result = $this->loadTemplatefile($file);
+            } else {
+                $file = "templates/$module/$file";
+                $result = $this->loadTemplatefile($file);
+            }
+        }
+
+        if ($result) {
+            $this->lastTemplatefile = $file;
+            return $result;
+        }
+        else 
+            return $this->err[0];
     }
+
+    function setModule($module)
+    {
+        $this->module = $module;
+    }
+
+    function getModule()
+    {
+        return $this->module;
+    }
+
+    function setData($data)
+    {
+        if (PEAR::isError($data)){
+            PHPWS_Error::log($data);
+            return NULL;
+        }
+
+        foreach($data as $tag=>$content)
+            $this->setVariable($tag, $content);
+    }
+
+    function getLastTplFile()
+    {
+        return $this->getfile($this->lastTemplatefile);
+    }
+
+    function process($template, $module, $file)
+    {
+        if (!is_array($template)) {
+            return PHPWS_Error::log(PHPWS_VAR_TYPE, 'core', 
+                                    'PHPWS_Template::process', 
+                                    'template=' . gettype($template));
+            return NULL;
+        }
+        if (PEAR::isError($template)){
+            PHPWS_Error::log($template);
+            return NULL;
+        }
       
-    $tpl = & new PHPWS_Template($module, $file);
+        $tpl = & new PHPWS_Template($module, $file);
 
-    if (PEAR::isError($tpl->error)){
-      return _('Template error.');
+        if (PEAR::isError($tpl->error)){
+            return _('Template error.');
+        }
+
+        foreach ($template as $key => $value) {
+            if (!is_array($value)) {
+                continue;
+            }
+
+            foreach ($value as $content) {
+                $tpl->setCurrentBlock($key);
+                $tpl->setData($content);
+                $tpl->parseCurrentBlock();
+            }
+            unset($template[$key]);
+        }
+
+
+        $tpl->setData($template);
+
+        $result = $tpl->get();
+
+        if (PEAR::isError($result)) {
+            return $result;
+        }
+
+        if (LABEL_TEMPLATES == TRUE){
+            $start = "\n<!-- START TPL: " . $tpl->lastTemplatefile . " -->\n";
+            $end = "\n<!-- END TPL: " . $tpl->lastTemplatefile . " -->\n";
+        }
+        else
+            $start = $end = NULL;
+
+        if (!isset($result) && RETURN_BLANK_TEMPLATES)
+            return $start . $tpl->getLastTplFile() . $end;
+        else
+            return $start . $result . $end;
     }
 
-    foreach ($template as $key => $value) {
-      if (!is_array($value)) {
-	continue;
-      }
-
-      foreach ($value as $content) {
-	$tpl->setCurrentBlock($key);
-	$tpl->setData($content);
-	$tpl->parseCurrentBlock();
-      }
+    function processTemplate($template, $module, $file, $defaultTpl=TRUE)
+    {
+        if ($defaultTpl)
+            return PHPWS_Template::process($template, $module, $file);
+        else {
+            $tpl = & new PHPWS_Template($module);
+            $tpl->setFile($file, TRUE);
+            $tpl->setData($template);
+            return $tpl->get();
+        }
     }
-
-    $tpl->setData($template);
-
-    $result = $tpl->get();
-
-    if (PEAR::isError($result))
-      return $result;
-
-    if (LABEL_TEMPLATES == TRUE){
-      $start = "\n<!-- START TPL: " . $tpl->lastTemplatefile . " -->\n";
-      $end = "\n<!-- END TPL: " . $tpl->lastTemplatefile . " -->\n";
-    }
-    else
-      $start = $end = NULL;
-
-    if (!isset($result) && RETURN_BLANK_TEMPLATES)
-      return $start . $tpl->getLastTplFile() . $end;
-    else
-      return $start . $result . $end;
-  }
-
-  function processTemplate($template, $module, $file, $defaultTpl=TRUE){
-    if ($defaultTpl)
-      return PHPWS_Template::process($template, $module, $file);
-    else {
-      $tpl = & new PHPWS_Template($module);
-      $tpl->setFile($file, TRUE);
-      $tpl->setData($template);
-      return $tpl->get();
-    }
-  }
 }
 
 ?>
