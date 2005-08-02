6aeacae8e88f3bad8a80cd03b0de166235f5e7db
Matt McNaney <matt@tux.appstate.edu>
2005-08-02 20:11:03 +0000

    beginning of image manager for choosing images

---
+++ mod/filecabinet/javascript/pick_image/body.js
@@ -0,0 +1,3 @@
+{TITLE_LABEL}<span id='image-title'> </span><br />
+{DESC_LABEL}<span id='image-desc'> </span><br />
+<span id='image-tag'> </span>
---
+++ mod/filecabinet/javascript/pick_image/head.js
@@ -0,0 +1,57 @@
+<script type="text/javascript">
+
+var requester = null;
+
+function showImage(image_id)
+{
+    if (requester != null && requester.readyState != 0 && requester.readyState != 4) {
+            requester.abort();
+    }
+    try	{
+        requester = new XMLHttpRequest();
+    }
+    catch (error) {
+        try {
+            requester = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch (error) {
+            requester = null;
+            return false;
+        }
+    }
+
+    requester.onreadystatechange = getImage;
+    requester.open("GET", "index.php?module=filecabinet&action=get_image_xml&id=" + image_id);
+    requester.send(null);
+}
+
+function getImage()
+{
+    if (requester.readyState == 4) {
+        if (requester.status == 200) {
+            show_image();
+        }
+        else {
+            alert('Unable to get file');
+        }
+    }
+    return true;
+}
+
+function show_image()
+{
+    image_src = requester.responseXML.getElementsByTagName('src')[0].firstChild.nodeValue;
+    image_width = requester.responseXML.getElementsByTagName('width')[0].firstChild.nodeValue;
+    image_height = requester.responseXML.getElementsByTagName('height')[0].firstChild.nodeValue;
+    image_title  =requester.responseXML.getElementsByTagName('title')[0].firstChild.nodeValue;
+    image_alt  =requester.responseXML.getElementsByTagName('alt')[0].firstChild.nodeValue;
+    image_desc  =requester.responseXML.getElementsByTagName('desc')[0].firstChild.nodeValue;
+    
+
+    image_tag = '<img src="' + image_src + '" width="' + image_width +'" height="' + image_height + '" title="' + image_title + '" alt="' + image_alt + '" />';
+    document.getElementById('image-tag').innerHTML = image_tag;
+}
+
+</script>
+
+    
