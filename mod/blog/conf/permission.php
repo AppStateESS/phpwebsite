848a49144c8315f5a26d3ac3a650c245ff974271
Matt McNaney <matt@tux.appstate.edu>
2005-08-03 13:02:46 +0000

    changed text slightly

--- mod/blog/conf/permission.php
+++ mod/blog/conf/permission.php
@@ -2,9 +2,9 @@
 
 $use_permissions = TRUE;
 
-$permissions['view_blog']   = _('View Blog');
-$permissions['edit_blog']   = _('Edit Blog');
-$permissions['delete_blog'] = _('Delete Blog');
+$permissions['view_blog']   = _('View blog');
+$permissions['edit_blog']   = _('Edit blogs');
+$permissions['delete_blog'] = _('Delete blog');
 
 $item_permissions = TRUE;
 ?>
