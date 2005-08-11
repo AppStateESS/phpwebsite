f282810bc01c49a0c64623eeeb2994addb443942
Matt McNaney <matt@tux.appstate.edu>
2005-08-11 13:41:32 +0000

    increased width added some code to work with images better

--- javascript/editors/FCKeditor/head.js
+++ javascript/editors/FCKeditor/head.js
@@ -1,10 +1,13 @@
 <script type="text/javascript" src="./javascript/editors/FCKeditor/fckeditor.js"></script>
 <script type="text/javascript">
+//<![CDATA[
+window.onload = FCKinit;
 function FCKinit()
 {
-    var oFCKeditor = new FCKeditor( '{NAME}' , 500, 300, "phpws") ;
+    var oFCKeditor = new FCKeditor( '{NAME}' , 600, 300, "phpws") ;
     oFCKeditor.BasePath = './javascript/editors/FCKeditor/';
-    oFCKeditor.Config["CustomConfigurationsPath"] = 'custom.js';
+    oFCKeditor.Config["CustomConfigurationsPath"] = 'custom.php?module={module}';
     oFCKeditor.ReplaceTextarea() ;
 }
+//]]>
 </script>
