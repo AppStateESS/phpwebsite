cc2a1e4c60911990ff65896bb11754ae20714998
Matt McNaney <matt@tux.appstate.edu>
2005-08-16 20:28:34 +0000

    window pops mid screen

--- javascript/open_window/head.js
+++ javascript/open_window/head.js
@@ -2,7 +2,14 @@
 //<![CDATA[
 
 function open_window(page, width, height) {
-  OpenWin = this.open(page, "CtrlWindow", "toolbar={toolbar},scrollbars={scrollbars},menubar={menubar},location={location},resizable={resizable}, width=" + width + ", height=" + height);
+   x = (640 - width)/2, y = (480 - height)/2;
+
+    if (screen) {
+        y = (screen.availHeight - height)/2;
+        x = (screen.availWidth - width)/2;
+    }
+
+  popup = window.open(page, 'CtrlWindow', 'toolbar={toolbar},top='+ y +',left='+ x +',screenY='+ y +',screenX='+ x +',scrollbars={scrollbars},menubar={menubar},location={location},resizable={resizable},width=' + width + ',height=' + height);
 }
 
 //]]>
