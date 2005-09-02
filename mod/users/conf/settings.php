3a20ccf1dff8afdbd3f67430c1a400d8e5419522
Matt McNaney <matt@tux.appstate.edu>
2005-09-02 19:57:26 +0000

    new settings file for users

---
+++ mod/users/conf/settings.php
@@ -0,0 +1,6 @@
+<?php
+$settings = array('default_authorization' => 1,
+                  'new_user_method' => 2,
+                  'user_menu' => 'top.tpl',
+                  'graphic_confirm' => 0);
+?>
