87e1845cc1157e5cb396d0b9117b3816d0aa60d4
Matt McNaney <matt@tux.appstate.edu>
2005-09-12 16:36:19 +0000

    changed include to file_get_contents

--- mod/profiler/profiler.php
+++ mod/profiler/profiler.php
@@ -6,8 +6,10 @@ function rand_profiler($profile_category, $template)
     $profile_category = preg_replace('/\W/', '', strip_tags($profile_category));
     $template         = preg_replace('/\W/', '', strip_tags($template));
 
-    include sprintf('%s/index.php?module=profiler&user_cmd=random_profile&type=%s&template=%s',
+    $directory =  sprintf('%s/index.php?module=profiler&user_cmd=random_profile&type=%s&template=%s',
                     SOURCE_ADDRESS, $profile_category, $template);
+
+    echo file_get_contents($directory);
 }
 
 ?>
