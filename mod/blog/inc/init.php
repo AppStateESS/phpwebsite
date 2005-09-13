c4fa1a90e49cea8d8d797d1c1b0af7891476046d
Matt McNaney <matt@tux.appstate.edu>
2005-09-13 16:13:07 +0000

    commented and tidied

--- mod/blog/inc/init.php
+++ mod/blog/inc/init.php
@@ -1,8 +1,16 @@
 <?php
 
-PHPWS_Core::configRequireOnce("blog", "config.php");
+/**
+ * Blog init file
+ * 
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @version $Id$
+ */
 
-PHPWS_Core::initModClass("blog", "Blog.php");
-PHPWS_Core::initModClass("blog", "Blog_User.php");
+PHPWS_Core::configRequireOnce('blog', 'config.php');
+PHPWS_Core::initModClass('blog', 'Blog.php');
+PHPWS_Core::initModClass('blog', 'Blog_User.php');
+
+
 
 ?>
