bfce8bb9f7077d83ebcbe3873969ca20af39b70e
Matt McNaney <matt@tux.appstate.edu>
2005-09-14 16:48:44 +0000

    added default for hitting index file as an admin

--- mod/webpage/index.php
+++ mod/webpage/index.php
@@ -9,6 +9,10 @@ if (isset($_REQUEST['wp_user'])) {
     PHPWS_Core::initModClass('webpage', 'User.php');
 } elseif(isset($_REQUEST['wp_admin'])) {
     PHPWS_Core::initModClass('webpage', 'Admin.php');
+    Webpage_Admin::main();
+} elseif (Current_User::allow('webpage')) {
+    PHPWS_Core::initModClass('webpage', 'Admin.php');
+    Webpage_Admin::main();
 } else {
     PHPWS_Core::errorPage('404');
 }
