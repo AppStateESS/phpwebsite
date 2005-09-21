a8eb1f9fc4a84ad59fc914fa256519528758a1f7
Matt McNaney <matt@tux.appstate.edu>
2005-09-21 20:29:21 +0000

    changed double to single quotes

--- mod/users/conf/permission.php
+++ mod/users/conf/permission.php
@@ -2,11 +2,11 @@
 
 $use_permissions = TRUE;
 
-$permissions["edit_users"]       = _("Edit Users");
-$permissions["delete_users"]     = _("Delete Users");
-$permissions["add_edit_groups"]  = _("Add / Edit Groups");
-$permissions["delete_groups"]    = _("Delete Groups");
-$permissions["edit_permissions"] = _("Edit Permissions");
-$permissions["settings"]        = _("Settings");
+$permissions['edit_users']       = _('Edit Users');
+$permissions['delete_users']     = _('Delete Users');
+$permissions['add_edit_groups']  = _('Add / Edit Groups');
+$permissions['delete_groups']    = _('Delete Groups');
+$permissions['edit_permissions'] = _('Edit Permissions');
+$permissions['settings']        = _('Settings');
 
 ?>
