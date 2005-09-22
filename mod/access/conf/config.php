7fd6fc1c8ba2109868497b94c703cb5590a24776
Matt McNaney <matt@tux.appstate.edu>
2005-09-22 20:30:49 +0000

    added some new errors

---
+++ mod/access/conf/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('SHORTCUT_BAD_KEYWORD',     1);
+define('SHORTCUT_WORD_IN_USE',     2);
+define('SHORTCUT_MISSING_KEYWORD', 3);
+define('SHORTCUT_MISSING_URL',     4);
+
+?>
---
+++ mod/access/conf/error.php
@@ -0,0 +1,12 @@
+<?php
+translate('access');
+$errors = array(SHORTCUT_BAD_KEYWORD     => _('Bad keyword. Use only alphanumeric characters and underlines.'),
+                SHORTCUT_WORD_IN_USE     => _('Keyword already in use. Choose another.'),
+                SHORTCUT_MISSING_KEYWORD => _('Missing keyword.'),
+                SHORTCUT_MISSING_URL     => _('Missing url.'),
+                );
+
+translate();
+
+
+?>
