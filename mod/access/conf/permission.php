314b115f3f0ac3a5155d0c00df0a07a9bf4095a7
Matt McNaney <matt@tux.appstate.edu>
2005-09-26 19:52:47 +0000

    changed permissions

--- mod/access/conf/permission.php
+++ mod/access/conf/permission.php
@@ -2,7 +2,8 @@
 
 $use_permissions = TRUE;
 
-$permissions['deny_allow'] = _('Set denys and allows');
-$permissions['rewrite']    = _('Turn rewrite on and off');
+$permissions['deny_allow']    = _('Set denys and allows');
+$permissions['file_update']   = _('Automatically update .htaccess file');
+$permissions['admin_options'] = _('Administrative options');
 
 ?>
