85cd694d0aee05caaa442138c0a9c6cc1b88a3f4
Matt McNaney <matt@tux.appstate.edu>
2005-10-06 17:09:38 +0000

    new stub files for tinymce editor

---
+++ javascript/editors/tinymce/body.js
@@ -0,0 +1,1 @@
+<textarea id="{NAME}" name="{NAME}" cols="50" rows="15">{VALUE}</textarea>
---
+++ javascript/editors/tinymce/head.js
@@ -0,0 +1,6 @@
+<script language="javascript" type="text/javascript" src="./javascript/editors/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
+<script language="javascript" type="text/javascript">
+tinyMCE.init({
+	mode : "textareas"
+});
+</script>
