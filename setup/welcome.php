902ba761fa20e83fc5c9f54c9e21cbfe214b8054
Matt McNaney <matt@tux.appstate.edu>
2005-10-13 20:25:02 +0000

    welcome message! hi!

---
+++ setup/welcome.php
@@ -0,0 +1,24 @@
+<?php
+
+$content[] = '<b>Welcome to the phpWebSite 1.0.0 (also called "Fallout") Beta installation</b><br />
+<p>1.0.0 is an upgrade to the phpWebSite version 0.x software.
+It has been redesigned and coded from the ground up.</p>
+
+<p>We have added MANY new features to phpWebSite to benefit users and developers alike.
+Please be aware, however, that the version you are installing now is incomplete.
+<ul>
+<li> It is not yet 100% compatible with the 0.x series of phpWebSite.</li>
+<li> We have not written the Branch module.</li>
+<li> Some functionality may be missing.</li>
+<li> Bugs may abound.</li>
+<li> Language translation has not been tested.</li>
+</ul>
+</p>
+
+<p>The good news is that the code seems stable enough to use in a production environment.
+Keep the above in mind, however, before you depend on its use.</p>';
+
+$content[] = '<p>If however you have questions about its functioning and API, please visit '
+. 'us at irc: freenode.net #phpwebsite </p>';
+
+?>
