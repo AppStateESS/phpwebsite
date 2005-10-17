dc6cd79a54f83c3050e41970c5ebfb7d206028b3
Matt McNaney <matt@tux.appstate.edu>
2005-10-17 20:56:18 +0000

    tidied with comments

--- mod/layout/boost/unregister.php
+++ mod/layout/boost/unregister.php
@@ -1,20 +1,26 @@
 <?php
+/**
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @version $Id$
+ */
 
 function layout_unregister($module, &$content){
-  PHPWS_Core::initModClass("layout", "Box.php");
-  $content[] = _("Removing old layout components.");
-  $db = & new PHPWS_DB("layout_box");
-  $db->addWhere("module", $module);
-  $moduleBoxes = $db->getObjects("Layout_Box");
+    PHPWS_Core::initModClass('layout', 'Box.php');
+    $content[] = _('Removing old layout components.');
+    $db = & new PHPWS_DB('layout_box');
+    $db->addWhere('module', $module);
+    $moduleBoxes = $db->getObjects('Layout_Box');
 
-  if (empty($moduleBoxes))
-    return;
+    if (empty($moduleBoxes))
+        return;
 
-  if (PEAR::isError($moduleBoxes))
-    return $moduleBoxes;
+    if (PEAR::isError($moduleBoxes)) {
+        return $moduleBoxes;
+    }
 
-  foreach ($moduleBoxes as $box)
-    $box->kill();
+    foreach ($moduleBoxes as $box) {
+        $box->kill();
+    }
 
 }
 
