c90cc6023c0b2c017b3a4c339d97eeb1e4579ea7
Matt McNaney <matt@tux.appstate.edu>
2005-10-18 18:23:44 +0000

    switched to key format

--- mod/block/boost/install.sql
+++ mod/block/boost/install.sql
@@ -6,8 +6,6 @@ PRIMARY KEY ( id )
 );
 
 CREATE TABLE block_pinned (
-block_id INT DEFAULT '0' NOT NULL ,
-module VARCHAR( 40 ) NOT NULL ,
-itemname VARCHAR( 40 ) NOT NULL ,
-item_id INT DEFAULT '0' NOT NULL
+block_id INT DEFAULT '0' NOT NULL,
+key_id INT DEFAULT '0' NOT NULL
 );
