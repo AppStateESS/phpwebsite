0dd33d083a74853c174f99ba6c94750bd754be6d
Matt McNaney <matt@tux.appstate.edu>
2005-10-26 15:02:12 +0000

    added default search wording

--- mod/search/conf/config.php
+++ mod/search/conf/config.php
@@ -1,8 +1,16 @@
 <?php
 
+translate('search');
 
   // search words shorter than this length
   // will be ignored.
 define('SEARCH_MIN_WORD_LENGTH', 3);
+
+
+/**
+ * if this is set, this text will appear in your search text box
+ * The text will disappear if the search box is clicked.
+ */
+define('SEARCH_DEFAULT', NULL);
 
 ?>
