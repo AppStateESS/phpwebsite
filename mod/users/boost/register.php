9e13d0c4a85c57a92dadee2cb25a99ec2a9ca4c1
Matt McNaney <matt@tux.appstate.edu>
2005-10-31 18:57:24 +0000

    comments and tidy

--- mod/users/boost/register.php
+++ mod/users/boost/register.php
@@ -1,31 +1,37 @@
 <?php
 
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
 function users_register($module, &$content){
-  PHPWS_Core::initModClass("users", "Permission.php");
-  PHPWS_Core::initModClass("users", "My_Page.php");
+    PHPWS_Core::initModClass('users', 'Permission.php');
+    PHPWS_Core::initModClass('users', 'My_Page.php');
 
-  $result = Users_Permission::createPermissions($module);
+    $result = Users_Permission::createPermissions($module);
   
-  if (is_null($result)){
-      $content[] = _("Permissions file not found.");
-      PHPWS_Boost::addLog("users", _("Permissions file not found."));
-  } elseif (PEAR::isError($result)) {
-      $content[] = _("Permissions table not created successfully.");
-      PHPWS_Error::log($result);
-      return FALSE;
-  } else
-    $content[] = _("Permissions table created successfully.");
+    if (is_null($result)){
+        $content[] = _('Permissions file not found.');
+        PHPWS_Boost::addLog('users', _('Permissions file not found.'));
+    } elseif (PEAR::isError($result)) {
+        $content[] = _('Permissions table not created successfully.');
+        PHPWS_Error::log($result);
+        return FALSE;
+    } else {
+          $content[] = _('Permissions table created successfully.');
+    }
 
-  $result = My_Page::registerMyPage($module);
+    $result = My_Page::registerMyPage($module);
 
-  if (PEAR::isError($result)){
-    PHPWS_Boost::addLog("users", _("A problem occurred when trying to register this module to My Page."));
-    $content[] = _("A problem occurred when trying to register this module to My Page.");
-    return FALSE;
-  } elseif ($result != FALSE)
-    $content[] = _("My Page registered to Users module.");
+    if (PEAR::isError($result)){
+        PHPWS_Boost::addLog('users', _('A problem occurred when trying to register this module to My Page.'));
+        $content[] = _('A problem occurred when trying to register this module to My Page.');
+        return FALSE;
+    } elseif ($result != FALSE)
+          $content[] = _('My Page registered to Users module.');
 
-  return TRUE;
+    return TRUE;
 }
 
 ?>
