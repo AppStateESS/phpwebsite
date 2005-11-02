b2cdc81a8e5c32596f3ec0e448c38704fedbd1a8
Matt McNaney <matt@tux.appstate.edu>
2005-11-02 18:49:57 +0000

    updated to use permissions for admin

--- mod/search/conf/boost.php
+++ mod/search/conf/boost.php
@@ -1,12 +1,12 @@
 <?php
 $proper_name  = 'Search';
-$version      = '0.0.1';
+$version      = '0.0.2';
 $register     = FALSE;
 $unregister   = TRUE;
 $import_sql   = TRUE;
 $image_dir    = FALSE;
 $file_dir     = FALSE;
-$version_http = 'http://localhost/updates/search.xml';
+$version_http = 'http://phpwebsite.appstate.edu/updates/search.xml';
 $about        = TRUE;
 $priority     = 50;
 $dependency   = FALSE;
