67b5e4efa91d8485bc2afe7c3b57f7c207556e62
Matt McNaney <matt@tux.appstate.edu>
2005-11-03 20:45:47 +0000

    rewritten to use the new functions in users to control item edit and view rights

--- mod/blog/class/Blog_Form.php
+++ mod/blog/class/Blog_Form.php
@@ -1,57 +1,53 @@
 <?php
+
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
+PHPWS_Core::initCoreClass('Form.php');
+
 class Blog_Form {
-  function edit(&$blog, $version_id=NULL)
-  {
-    $form = & new PHPWS_Form;
-    $form->addHidden('module', 'blog');
-    $form->addHidden('action', 'admin');
-    $form->addHidden('command', 'postEntry');
+    function edit(&$blog, $version_id=NULL)
+    {
+        $key = & new Key($blog->key_id);
 
-    if (isset($version_id)) {
-      $form->addHidden('version_id', $version_id);
-      if (Current_User::isUnrestricted('blog')) {
-	$form->addSubmit('approve_entry', _('Save Changes and Approve'));
-      }
+        $form = & new PHPWS_Form;
+        $form->addHidden('module', 'blog');
+        $form->addHidden('action', 'admin');
+        $form->addHidden('command', 'postEntry');
+
+        if (isset($version_id)) {
+            $form->addHidden('version_id', $version_id);
+            if (Current_User::isUnrestricted('blog')) {
+                $form->addSubmit('approve_entry', _('Save Changes and Approve'));
+            }
+        }
+
+        if (isset($blog->id) || isset($version_id)){
+            $form->addHidden('blog_id', $blog->id);
+            $form->addSubmit('submit', _('Update Entry'));
+        } else
+            $form->addSubmit('submit', _('Add Entry'));
+
+        $form->addTextArea('entry', $blog->getEntry());
+        $form->useEditor('entry');
+        $form->setRows('entry', '10');
+        $form->setWidth('entry', '80%');
+        $form->setLabel('entry', _('Entry'));
+
+        $form->addText('title', $blog->title);
+        $form->setSize('title', 40);
+        $form->setLabel('title', _('Title'));
+
+        if (Current_User::isUnrestricted('blog') && empty($version_id)) {
+            PHPWS_User::editPermissions($form, $key);
+            PHPWS_User::viewPermissions($form, $key);
+        }
+
+        $template = $form->getTemplate();
+
+        return PHPWS_Template::process($template, 'blog', 'edit.tpl');
     }
-
-    if (isset($blog->id) || isset($version_id)){
-      $form->addHidden('blog_id', $blog->id);
-      $form->addSubmit('submit', _('Update Entry'));
-    } else
-      $form->addSubmit('submit', _('Add Entry'));
-
-    $form->addTextArea('entry', $blog->getEntry());
-    $form->useEditor('entry');
-    $form->setRows('entry', '10');
-    $form->setWidth('entry', '80%');
-    $form->setLabel('entry', _('Entry'));
-
-    $form->addText('title', $blog->title);
-    $form->setSize('title', 40);
-    $form->setLabel('title', _('Title'));
-
-    if (Current_User::isUnrestricted('blog') && empty($version_id)) {
-      $viewable_opts[] = 0;
-      $viewable_opts[] = 1;
-      $viewable_opts[] = 2;
-      $viewable_label[] = _('Unrestricted');
-      $viewable_label[] = _('Only logged in users');
-      $viewable_label[] = _('Only certain groups');
-      
-      $form->addRadio('viewable', $viewable_opts);
-      $form->setLabel('viewable', $viewable_label);
-      $form->setMatch('viewable', $blog->getRestricted());
-      $form->addTplTag('VIEWABLE_MAIN_LABEL', _('Viewing Permissions'));
-    }
-
-    $template = $form->getTemplate();
-
-    if (Current_User::isUnrestricted('blog') && empty($version_id)){
-      $assign = PHPWS_User::assignPermissions('blog', $blog->getId());
-      $template = array_merge($assign, $template);
-    }
-
-    return PHPWS_Template::process($template, 'blog', 'edit.tpl');
-  }
 }
 ?>
