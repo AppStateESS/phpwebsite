39c4418e3c76850309370e0927da84d9617861a1
Matt McNaney <matt@tux.appstate.edu>
2005-11-03 20:47:49 +0000

    fixed tabs and added comments. probably did some stuff with view permissions as well

--- mod/blog/class/Blog_User.php
+++ mod/blog/class/Blog_User.php
@@ -1,94 +1,106 @@
 <?php
+  /**
+   * User functionality in Blog
+   *
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
 
 class Blog_User {
 
-  function main()
-  {
-    if (!isset($_REQUEST['blog_id']) && isset($_REQUEST['id'])) {
-      $blog = & new Blog((int)$_REQUEST['id']);
-    } elseif (isset($_REQUEST['blog_id'])) {
-      $blog = & new Blog((int)$_REQUEST['blog_id']);
-    } else {
-      $blog = & new Blog();
+    function main()
+    {
+        if (!isset($_REQUEST['blog_id']) && isset($_REQUEST['id'])) {
+            $blog = & new Blog((int)$_REQUEST['id']);
+        } elseif (isset($_REQUEST['blog_id'])) {
+            $blog = & new Blog((int)$_REQUEST['blog_id']);
+        } else {
+            $blog = & new Blog();
+        }
+
+        if (!isset($_REQUEST['action'])) {
+            $action = 'view_comments';
+        } else {
+            $action = $_REQUEST['action'];
+        }
+
+        switch ($action) {
+        case 'view_comments':
+            $content = $blog->view(TRUE, FALSE);
+            break;
+
+        case 'make_comment':
+            $content = Blog_User::makeComment($blog);
+            break;
+
+        case 'save_comment':
+            $content = Blog_User::postComment($blog);
+            break;
+
+        default:
+            PHPWS_Core::errorPage(404);
+            break;
+        }
+
+        Layout::add($content);
     }
 
-    if (!isset($_REQUEST['action'])) {
-        $action = 'view_comments';
-    } else {
-        $action = $_REQUEST['action'];
+
+    function makeComment(&$blog)
+    {
+        PHPWS_Core::initModClass('comments', 'Comments.php');
+
+        $thread = $blog->makeThread();
+        $thread->form();
     }
 
-    switch ($action) {
-    case 'view_comments':
-      $content = $blog->view(TRUE, FALSE);
-      break;
+    function show(){
+        $key = 'front blog page';
 
-    case 'make_comment':
-      $content = Blog_User::makeComment($blog);
-      break;
+        if (!Current_User::isLogged()    &&
+            !Current_User::allow('blog') &&
+            $content = PHPWS_Cache::get($key)) {
+            return $content;
+        }
 
-    case 'save_comment':
-      $content = Blog_User::postComment($blog);
-      break;
+        $limit = 5;
 
-    default:
-        PHPWS_Core::errorPage(404);
-        break;
-    }
+        $db = & new PHPWS_DB('blog_entries');
+        $db->setLimit($limit);
+        $db->addOrder('date desc');
 
-    Layout::add($content);
-  }
+        if (!Current_User::isLogged()) {
+            $db->addWhere('key_id', 'phpws_key.id');
+            $db->addWhere('phpws_key.restricted', 0);
+        }
 
+        $result = $db->getObjects('Blog');
 
-  function makeComment(&$blog)
-  {
-    PHPWS_Core::initModClass('comments', 'Comments.php');
-
-    $thread = $blog->makeThread();
-    $thread->form();
-  }
-
-  function show(){
-    $key = 'front blog page';
-
-    if (!Current_User::isLogged()    &&
-	!Current_User::allow('blog') &&
-	$content = PHPWS_Cache::get($key)) {
-      return $content;
-    }
-
-    $limit = 5;
-
-    $db = & new PHPWS_DB('blog_entries');
-    $db->setLimit($limit);
-    $db->addOrder('date desc');
-    if (!Current_User::isLogged()) {
-      $db->addWhere('restricted', '0');
-    }
-
-    $result = $db->getObjects('Blog');
-
-    if (empty($result))
-      return ('No blog entries found.');
+        if (empty($result)) {
+            return ('No blog entries found.');
+        }
     
-    foreach ($result as $blog) {
-      if ($blog->getRestricted() == 2 &&
-	  !Current_User::allow('blog', 'view_blog', $blog->getId(), 'entry')) {
-	continue;
-      }
-      $view = $blog->view();
-      if (!empty($view)) {
-	$list[] = $view;
-      }
-    }
+        foreach ($result as $blog) {
+            echo 'have it check for group before';
+            /*
+            if ($blog->getRestricted() == 2 &&
+                !Current_User::allow('blog', 'view_blog', $blog->getId(), 'entry')) {
+                continue;
+            }
+            */
+            $view = $blog->view();
+            if (!empty($view)) {
+                $list[] = $view;
+            }
+        }
 
-    $content = implode('', $list);
-    if (!Current_User::allow('blog')) {
-      PHPWS_Cache::save($key, $content);
-    }
+        $content = implode('', $list);
+        if (!Current_User::allow('blog')) {
+            PHPWS_Cache::save($key, $content);
+        }
 
-    return $content;
-  }
+        return $content;
+    }
 
 }
 
