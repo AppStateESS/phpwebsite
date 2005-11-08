50f4d006b54b149d8a0620bf25198a9dd57f8e3a
Matt McNaney <matt@tux.appstate.edu>
2005-11-08 15:23:57 +0000

    tabbed properly, commented

--- mod/blog/boost/update.php
+++ mod/blog/boost/update.php
@@ -1,24 +1,29 @@
 <?php
 
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
 function blog_update(&$content, $currentVersion)
 {
-  switch ($currentVersion) {
-  case version_compare($currentVersion, '0.0.3', '<'):
-    $result = blog_update_003();
-    if (PEAR::isError($result)) {
-      return $result;
+    switch ($currentVersion) {
+    case version_compare($currentVersion, '0.0.3', '<'):
+        $result = blog_update_003();
+        if (PEAR::isError($result)) {
+            return $result;
+        }
+        $content[] = '+ added the author column';
+        break;
     }
-    $content[] = '+ added the author column';
-    break;
-  }
-  return TRUE;
+    return TRUE;
 }
 
 
 function blog_update_003()
 {
-  $filename = PHPWS_SOURCE_DIR . 'mod/blog/boost/update_0_0_3.sql';
-  $db = & new PHPWS_DB;
-  return $db->importFile($filename);
+    $filename = PHPWS_SOURCE_DIR . 'mod/blog/boost/update_0_0_3.sql';
+    $db = & new PHPWS_DB;
+    return $db->importFile($filename);
 }
 ?>
