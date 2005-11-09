f775e6c67607f60529f5c6de1cad83f00ce5f5f7
Matt McNaney <matt@tux.appstate.edu>
2005-11-09 17:59:02 +0000

    tabs corrected comment added

--- mod/controlpanel/index.php
+++ mod/controlpanel/index.php
@@ -1,26 +1,32 @@
 <?php
+
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
 if (!defined('PHPWS_SOURCE_DIR')) {
     include '../../config/core/404.html';
     exit();
-}
+ }
 
 if (isset($_GET['cp_image_toggle'])){
-  PHPWS_ControlPanel_Tab::toggleImage($_GET['tab']);
-}
+    PHPWS_ControlPanel_Tab::toggleImage($_GET['tab']);
+ }
      
 if (isset($_GET['cp_desc_toggle'])){
-  PHPWS_ControlPanel_Tab::toggleDesc($_GET['tab']);
-}
+    PHPWS_ControlPanel_Tab::toggleDesc($_GET['tab']);
+ }
 
 if (isset($_REQUEST['action'])){
-  PHPWS_Core::initModClass("controlpanel", "Action.php");
+    PHPWS_Core::initModClass('controlpanel', 'Action.php');
 
-  if ($_REQUEST['action'] == "admin" && Current_User::allow("controlpanel")) {
-    CP_Action::adminAction();
-  }
-} elseif ($_SESSION['User']->isLogged()){
-  Layout::add(PHPWS_ControlPanel::display());
-}
+    if ($_REQUEST['action'] == 'admin' && Current_User::allow('controlpanel')) {
+        CP_Action::adminAction();
+    }
+ } elseif ($_SESSION['User']->isLogged()){
+     Layout::add(PHPWS_ControlPanel::display());
+ }
 
 
 ?>
