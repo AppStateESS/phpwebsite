93cbde1acc9b2e8615ff50a57f527b750d87b787
Matt McNaney <matt@tux.appstate.edu>
2005-11-09 17:57:42 +0000

    added isDummy call

--- mod/access/inc/close.php
+++ mod/access/inc/close.php
@@ -2,7 +2,7 @@
 
 if (isset($_SESSION['Access_Shortcut_Enabled'])) {
     $key = Key::getCurrent();
-    if (!empty($key) && !$key->isHomeKey()) {
+    if (!empty($key) && !$key->isDummy()) {
         PHPWS_Core::initModClass('access', 'Access.php');
         Access::shortcut();
     }
