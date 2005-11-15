0c5b92281359d99fc6c89fcd4acdb94c536e5c3f
Matt McNaney <matt@tux.appstate.edu>
2005-11-15 20:19:32 +0000

    fixed tabs and added comments

--- mod/users/boost/unregister.php
+++ mod/users/boost/unregister.php
@@ -1,17 +1,21 @@
 <?php
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
 
 function users_unregister($module, &$content){
-  PHPWS_Core::initModClass('users', 'Permission.php');
+    PHPWS_Core::initModClass('users', 'Permission.php');
 
-  $content[] = _('Removing permissions table.');
-  if (Users_Permission::removePermissions($module)){
-    $content[] = _('Permissions table removed successfully.');
-    return TRUE;
-  }
-  else {
-    $content[] = _('Permissions table not removed successfully.');
-    return FALSE;
-  }
+    $content[] = _('Removing permissions table.');
+    if (Users_Permission::removePermissions($module)){
+        $content[] = _('Permissions table removed successfully.');
+        return TRUE;
+    }
+    else {
+        $content[] = _('Permissions table not removed successfully.');
+        return FALSE;
+    }
 }
 
 
