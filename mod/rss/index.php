0ea0ad3c5f127147fab16c46ca27c619bcbbe1a4
Matt McNaney <matt@tux.appstate.edu>
2005-11-16 20:01:53 +0000

    added user feed commands

--- mod/rss/index.php
+++ mod/rss/index.php
@@ -8,6 +8,9 @@ if (!defined('PHPWS_SOURCE_DIR')) {
 if ( ( isset($_REQUEST['command']) || isset($_REQUEST['tab']) ) && Current_User::allow('rss')) {
     PHPWS_Core::initModClass('rss', 'Admin.php');
     RSS_Admin::main();
+ } elseif ($_REQUEST['mod_title']) {
+     PHPWS_Core::initModClass('rss', 'RSS.php');
+     RSS::viewChannel($_REQUEST['mod_title']);
  }
 
 
