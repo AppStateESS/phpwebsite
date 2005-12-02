9e0a2301a3385aae13f8371b4cab6ade4fdea2b0
Matt McNaney <matt@tux.appstate.edu>
2005-12-02 17:05:25 +0000

    added offiste link icon call and added alts to images

--- mod/menu/conf/config.php
+++ mod/menu/conf/config.php
@@ -1,10 +1,16 @@
 <?php
 
-define('MENU_CURRENT_LINK_STYLE', 'current-link');
-
 translate('menu');
 
-define('MENU_LINK_ADD', sprintf('<img src="./images/mod/menu/gtk-add.png" title="%s" />', _('Add Link')));
+  // Limits the number of characters a menu link can be
+  // 0 means no limit
+define('MENU_TITLE_LIMIT', 20);
+
+define('MENU_CURRENT_LINK_STYLE', 'current-link');
+
+define('MENU_LINK_ADD', sprintf('<img src="./images/mod/menu/gtk-add.png" title="%s" alt="%s" />', _('Add Link'), _('Add Link')));
+
+define('MENU_LINK_ADD_OFFSITE', sprintf('<img src="./images/mod/menu/offsite.png" title="%s" alt="%s" />', _('Add Offsite Link'), _('Add Offsite Link')));
 
 define('MENU_LINK_EDIT', sprintf('<img src="./images/mod/menu/gnome-stock-edit.png" title="%s" />', _('Edit Link Title')));
 
