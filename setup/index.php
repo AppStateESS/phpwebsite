b88baf3c0420ba92e4e6b962bf6286a3537e86ee
Matt McNaney <matt@tux.appstate.edu>
2005-12-08 14:00:35 +0000

    tabified

--- setup/index.php
+++ setup/index.php
@@ -1,15 +1,20 @@
 <?php
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
 chdir('../');
 if (!is_file('lib/pear/DB.php')) {
-  exit(_('Unable to locate your pear library files.'));
-}
+    exit(_('Unable to locate your pear library files.'));
+ }
 
 if (isset($_REQUEST['step']) && $_REQUEST['step'] > 1) {
-  require_once './config/core/config.php';
-}
-else {
-  require_once './setup/preconfig.php';
-}
+    require_once './config/core/config.php';
+ }
+ else {
+     require_once './setup/preconfig.php';
+ }
 
 require_once './inc/Functions.php';
 require_once './core/class/Init.php';
@@ -29,44 +34,44 @@ $setup = & new Setup;
 $title = 'phpWebSite 1.0.0 - ';
 
 if (!$setup->checkSession($content) || !isset($_REQUEST['step'])) {
-  $step = 0;
-} else {
-  $step = $_REQUEST['step'];
-}
+    $step = 0;
+ } else {
+    $step = $_REQUEST['step'];
+ }
 
 if (!$setup->checkDirectories($content)){
-  $title .= _('Directory Permissions');
-  exit(Setup::show($content, $title));
-}
+    $title .= _('Directory Permissions');
+    exit(Setup::show($content, $title));
+ }
 
 switch ($step){
  case '0':
-   $title .=  'Beta Setup';
-   $setup->welcome($content);
-   break;
+     $title .=  'Beta Setup';
+     $setup->welcome($content);
+     break;
 
  case '1':
-   $title .= _('Create Config File');
-   $setup->createConfig($content);
-   break;
+     $title .= _('Create Config File');
+     $setup->createConfig($content);
+     break;
 
  case '1a':
-   $title .= _('Create Database');
-   $setup->createDatabase($content);
-   break;
+     $title .= _('Create Database');
+     $setup->createDatabase($content);
+     break;
 
  case '2':
-   $title .= _('Create Core');
-   $result = $setup->createCore($content);
-   break;
+     $title .= _('Create Core');
+     $result = $setup->createCore($content);
+     break;
 
  case '3':
-   $title .= _('Install Modules');
-   $result = $setup->installModules($content);
-   if ($result)
-     $setup->finish($content);
-   break;
-}
+     $title .= _('Install Modules');
+     $result = $setup->installModules($content);
+     if ($result)
+         $setup->finish($content);
+     break;
+ }
 
 echo Setup::show($content, $title);
 ?>
