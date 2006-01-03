17f18709ce0d2364bc0d58f7b21c316a220b8494
Matt McNaney <matt@tux.appstate.edu>
2006-01-03 19:28:12 +0000

    trying to limit some class calls

--- mod/controlpanel/inc/init.php
+++ mod/controlpanel/inc/init.php
@@ -1,7 +1,10 @@
 <?php
 
-PHPWS_Core::initModClass("controlpanel", "ControlPanel.php");
-PHPWS_Core::initModClass("controlpanel", "Tab.php");
-PHPWS_Core::initModClass("controlpanel", "Link.php");
+PHPWS_Core::initModClass('controlpanel', 'ControlPanel.php');
+
+if (isset($_REQUEST['module']) && $_REQUEST['module'] == 'controlpanel') {
+    PHPWS_Core::initModClass('controlpanel', 'Tab.php');
+    PHPWS_Core::initModClass('controlpanel', 'Link.php');
+ }
 
 ?>
