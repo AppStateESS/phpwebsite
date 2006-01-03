0713c5e6f6d303cdef9f8811d130d51b25379114
Matt McNaney <matt@tux.appstate.edu>
2006-01-03 19:27:16 +0000

    tidy, comments, extra info

--- javascript/open_window/default.php
+++ javascript/open_window/default.php
@@ -1,13 +1,18 @@
 <?php
 
-$default['toolbar'] = 'no';
-$default['menubar'] = 'no';
-$default['location'] = 'no';
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
+$default['toolbar']    = 'no';
+$default['menubar']    = 'no';
+$default['location']   = 'no';
 $default['scrollbars'] = 'yes';
-$default['resizable'] = 'yes';
-$default['width'] = '400';
-$default['height'] = '300';
-$default['titlebar'] = 'no';
+$default['resizable']  = 'yes';
+$default['width']      = '400';
+$default['height']     = '300';
+$default['titlebar']   = 'no';
 $default['link_title'] = '';
 
 ?>
