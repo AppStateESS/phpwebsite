21a5fd619f2185c9eed0bde47c6dee2ef345664b
Matt McNaney <matt@tux.appstate.edu>
2006-01-10 18:31:49 +0000

    changed alert so if a label was not sent, it would force an alert. sending a label returns a link.

--- javascript/alert/default.php
+++ javascript/alert/default.php
@@ -4,4 +4,8 @@ if (!substr("\'", $data['content'])) {
     $data['content'] = str_replace("'", "\'", $data['content']);
 }
 
+if (empty($data['label'])) {
+    $bodyfile = $base . 'javascript/alert/body2.js';
+ }
+
 ?>
