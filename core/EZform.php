a4347b3fcc6b908f8fc750cbd5771d7529aba665
Matt McNaney <matt@tux.appstate.edu>
2006-01-13 21:21:10 +0000

    crutch file

--- core/EZform.php
+++ core/EZform.php
@@ -2,4 +2,12 @@
 
 /* Crutch file */
 
+PHPWS_Core::initCoreClass('Form.php');
+
+class EZform extends PHPWS_Form {
+    function EZform($form_name) {
+        parent::PHPWS_Form($form_name);
+    }
+}
+
 ?>
