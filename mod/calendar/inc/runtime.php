1560ae7dd59d18bde75d33e61a1cb11318d23d5b
Matt McNaney <matt@tux.appstate.edu>
2006-02-06 16:02:15 +0000

    added caching to the mini calendar

--- mod/calendar/inc/runtime.php
+++ mod/calendar/inc/runtime.php
@@ -4,10 +4,24 @@
    * @version $Id$
    */
 
-$Calendar = & new PHPWS_Calendar;
-// This needs to load the default public calendar
-// or maybe the personal one?
-$Calendar->loadView();
-Layout::add($Calendar->view->month_grid('mini'), 'calendar', 'minimonth');
+$cal_key = sprintf('cal_%s_%s', date('m'), date('Y'));
+$lil_calendar = PHPWS_Cache::get($cal_key);
+
+if (empty($lil_calendar)) {
+    $Calendar = & new PHPWS_Calendar;
+    // This needs to load the default public calendar
+    // or maybe the personal one?
+    $Calendar->loadView();
+    $lil_calendar = $Calendar->view->month_grid('mini');
+
+    PHPWS_Cache::save($cal_key, $lil_calendar);
+
+ } else {
+    if (PHPWS_Settings::get('calendar', 'use_calendar_style')) {
+        Layout::addStyle('calendar');
+    }
+ }
+
+Layout::add($lil_calendar, 'calendar', 'minimonth');
 
 ?>
