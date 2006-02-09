e7b6b0b3ba2ec13e5cd1f27d3b38be71081cba58
Matt McNaney <matt@tux.appstate.edu>
2006-02-09 20:47:16 +0000

    search is now registered to the key. it will remove search entries when the key is deleted

---
+++ mod/search/inc/key.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * unregisters deleted keys from search
+ *
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @version $Id$
+ */
+
+
+function search_unregister(&$key)
+{
+    if (empty($key) || empty($key->id)) {
+        return FALSE;
+    }
+
+    $db = & new PHPWS_DB('search');
+    $db->addWhere('key_id', (int)$key->id);
+    return $db->delete();
+}
+
+?>
