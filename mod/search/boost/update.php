1c3ce4ef371dffa0ee344a5509a50b266850e5fc
Matt McNaney <matt@tux.appstate.edu>
2006-02-09 20:46:52 +0000

    registers itself to the key mod

--- mod/search/boost/update.php
+++ mod/search/boost/update.php
@@ -1,4 +1,10 @@
 <?php
+  /**
+   * update file for search
+   *
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
 
 function search_update(&$content, $currentVersion)
 {
@@ -7,12 +13,28 @@ function search_update(&$content, $currentVersion)
         $content[] = _('Add permissions to search.');
         search_update_002($content);
         return true;
+        break;
+
+    case version_compare($currentVersion, '0.0.3', '<'):
+        $content[] = _('Register Search to Key.');
+        search_update_003($content);
+        return true;
+
     }
 }
 
 function search_update_002(&$content)
 {
     PHPWS_Boost::registerMyModule('search', 'users', $content);
+}
+
+function search_update_003(&$content)
+{
+    $result = Key::registerModule('search');
+    if (PEAR::isError($result)) {
+        PHPWS_Error::log($result);
+        $content[] = _('A problem occurred during the update.');
+    }
 }
 
 ?>
