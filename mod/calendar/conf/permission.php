264563f7d46823fce85370de094da061ed424a56
Matt McNaney <matt@tux.appstate.edu>
2006-02-14 19:32:51 +0000

    not certain these will be the final permissions yet

--- mod/calendar/conf/permission.php
+++ mod/calendar/conf/permission.php
@@ -1,17 +1,27 @@
 <?php
+  /**
+   * Permissions file for users
+   *
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
 
 $use_permissions = TRUE;
 $item_permissions = TRUE;
 
-$permissions['settings']             = _('Change module settings');
+$permissions['settings'] = _('Change module settings');
 
-// Can create other calendars
-// personal calendars handled by settings
-$permissions['create_schedule']      = _('Create calendars');
+/**
+ * Can create other calendars
+ * personal calendars handled by settings
+ * Users with basic calendar rights can make a
+ * private calendar (if settings allow)
+ */
+$permissions['edit_public'] = _('Create/Edit public schedules');
+
+
 // can edit schedules assigned to them as well as create
 // and edit events on that calendar
-$permissions['edit_schedule']        = _('Edit calendars');
-
-$permissions['delete_event']         = _('Delete event');
+$permissions['edit_other']        = _('Edit other schedules');
 
 ?>
