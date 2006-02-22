31e29ed134d85476e127b95905b4b55596d2b984
Matt McNaney <matt@tux.appstate.edu>
2006-02-22 21:06:19 +0000

    login script for the conversion

---
+++ mod/users/scripts/convert.php
@@ -0,0 +1,50 @@
+<?php
+
+  /**
+   * Conversion login script
+   *
+   * In phpwebsite 0.x, the password was simply a md5 hash.
+   * phpwebsite 1.x salts the password with the user name.
+   * To prevent everyone from having to re-enter their password
+   * after upgrading, this login script handles the conversion.
+   *
+   * After conversion, all the usernames and passwords are copied
+   * to a users_conversion table. The FIRST time the user logs in
+   * this script salts their password and copies it to the main
+   * authorization table. It then removes their login from the
+   * conversion table. Once the conversion table is empty, this file
+   * and the conversion table may be deleted.
+   *
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
+function authorize(&$user, $password)
+{
+    $db = & new PHPWS_DB('users_conversion');
+    if (preg_match('/[^' . ALLOWED_USERNAME_CHARACTERS . ']/', $user->username)) {
+        return FALSE;
+    }
+
+    $db->addWhere('username', strtolower($user->username));
+    $db->addWhere('password', md5($password));
+    $result = $db->select('one');
+    if (PEAR::isError($result) || !$result) {
+        return $result;
+    }
+
+    $db2 = & new PHPWS_DB('users');
+    $db2->addWhere('username', strtolower($user->username));
+    $result = $db2->loadObject($user);
+    $user->setPassword($password);
+    $user->authorize = LOCAL_AUTHORIZATION;
+
+    if (PEAR::isError($result)) {
+        return $result;
+    }
+    $db->delete();
+    return TRUE;
+}
+
+
+?>
