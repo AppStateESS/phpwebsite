3682618ecbc78115f3ee5996df36d6d7c679047e
Matt McNaney <matt@tux.appstate.edu>
2006-02-23 15:31:58 +0000

    removing

--- mod/calendar/boost/uninstall.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS calendar_events;
-DROP TABLE IF EXISTS calendar_events_seq;
-DROP TABLE IF EXISTS calendar_schedule;
-DROP TABLE IF EXISTS calendar_schedule_seq;
-DROP TABLE IF EXISTS calendar_notice;
-DROP TABLE IF EXISTS calendar_schedule_to_event;
