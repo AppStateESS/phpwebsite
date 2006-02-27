e4da5650bc5a0a61ff01d55dbeb766e4624a358e
Matt McNaney <matt@tux.appstate.edu>
2006-02-27 20:48:17 +0000

    close refresh will no allow a 'location' variable to have it load a different page when the window closes

--- javascript/close_refresh/head.js
+++ javascript/close_refresh/head.js
@@ -2,12 +2,18 @@
 //<![CDATA[
 var timeout = {timeout};
 var refresh = {refresh};
+var loc_new = '{location}';
 
 setTimeout('closeWindow()', timeout * 1000);
      
  function closeWindow() {
      if (refresh) {
-         window.opener.location.href = window.opener.location.href;
+         if (loc_new) {
+             alert(loc_new);
+             window.opener.location.href = loc_new;
+         } else {
+             window.opener.location.href = window.opener.location.href;
+         }
      
          if (window.opener.progressWindow) {
              window.opener.progressWindow.close();
