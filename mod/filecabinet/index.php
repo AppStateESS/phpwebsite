85009adde9017b38698be1f602254bd99fff0638
Matt McNaney <matt@tux.appstate.edu>
2006-02-27 20:55:22 +0000

    commented add some code for the download function

--- mod/filecabinet/index.php
+++ mod/filecabinet/index.php
@@ -1,11 +1,19 @@
 <?php
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id
+   */
+
 if (!defined('PHPWS_SOURCE_DIR')) {
     include '../../config/core/404.html';
     exit();
 }
 
-PHPWS_Core::initModClass("filecabinet", "Cabinet_Action.php");
-
-Cabinet_Action::admin();
+PHPWS_Core::initModClass('filecabinet', 'Cabinet_Action.php');
+if (!isset($_REQUEST['tab']) && !isset($_REQUEST['action']) && isset($_REQUEST['id'])) {
+    Cabinet_Action::download($_REQUEST['id']);
+ } else {
+    Cabinet_Action::admin();
+ }
 
 ?>
