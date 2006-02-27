bf9f26bf09471f22932435a4e5cd1b311e448ddb
Matt McNaney <matt@tux.appstate.edu>
2006-02-27 20:49:12 +0000

    documents and images now created by filecabinet

---
+++ mod/filecabinet/boost/install.sql
@@ -0,0 +1,28 @@
+CREATE TABLE documents (
+  id int NOT NULL default '0',
+  key_id int NOT NULL default '0',
+  file_name varchar(50) NOT NULL default '',
+  file_directory varchar(150) NOT NULL default '',
+  file_type varchar(30) NOT NULL default '',
+  title varchar(255) NOT NULL default '',
+  description text NOT NULL,
+  size int NOT NULL default '0',
+  downloaded int NOT NULL default '0',
+  PRIMARY KEY  (id)
+);
+
+CREATE TABLE images (
+  id int NOT NULL default '0',
+  key_id int NOT NULL default '0',
+  file_name varchar(50) NOT NULL default '',
+  file_directory varchar(150) NOT NULL default '',
+  file_type varchar(30) NOT NULL default '',
+  title varchar(255) NOT NULL default '',
+  description text,
+  size int NOT NULL default '0',
+  width smallint NOT NULL default '0',
+  height smallint NOT NULL default '0',
+  alt varchar(255) NOT NULL default '',
+  thumbnail_source int NOT NULL default '0',
+  PRIMARY KEY  (id)
+);
