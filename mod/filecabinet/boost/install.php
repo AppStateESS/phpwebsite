ae37941c7889547b5ff55ba8b71516eeeb754baa
Matt McNaney <matt@tux.appstate.edu>
2006-02-27 20:48:52 +0000

    removed some directory creation

--- mod/filecabinet/boost/install.php
+++ mod/filecabinet/boost/install.php
@@ -1,7 +1,5 @@
 <?php
 
-@mkdir(PHPWS_SOURCE_DIR . "images/filecabinet", "0700");
-@mkdir(PHPWS_SOURCE_DIR . "images/filecabinet/documents", "0700");
-@mkdir(PHPWS_SOURCE_DIR . "images/filecabinet/temp", "0700");
+@mkdir(PHPWS_SOURCE_DIR . 'images/filecabinet', '0700');
 
 ?>
