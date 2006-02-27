7567ca82a39493f2dfe907ba70d17f1c1b95dedb
Matt McNaney <matt@tux.appstate.edu>
2006-02-27 20:54:40 +0000

    added more permissions and upped version

--- mod/filecabinet/conf/boost.php
+++ mod/filecabinet/conf/boost.php
@@ -1,13 +1,17 @@
 <?php
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
 
-$proper_name  = "File Cabinet";
-$version      = "0.0.1";
+$proper_name  = 'File Cabinet';
+$version      = '0.1.0';
 $register     = FALSE;
 $unregister   = FALSE;
 $import_sql   = TRUE;
 $image_dir    = TRUE;
 $file_dir     = TRUE;
-$version_http = "http://phpwebsite.appstate.edu/test/update.ini";
+$version_http = 'http://phpwebsite.appstate.edu/test/update.ini';
 $about        = FALSE;
 $priority     = 50;
 
--- mod/filecabinet/conf/permission.php
+++ mod/filecabinet/conf/permission.php
@@ -8,8 +8,9 @@
 $use_permissions = TRUE;
 $item_permissions = TRUE;
 
-$permissions['edit']       = _('Edit files');
-$permissions['delete']     = _('Delete files');
-
+$permissions['edit_document']       = _('Edit files');
+$permissions['delete_document']     = _('Delete files');
+$permissions['edit_image']          = _('Edit images');
+$permissions['delete_image']        = _('Delete images');
 
 ?>
