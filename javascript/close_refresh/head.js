a938e042869d1a3211c59e8a860fa4017815b8f8
Matt McNaney <matt@tux.appstate.edu>
2006-03-07 17:37:15 +0000

    removed alert

--- javascript/close_refresh/head.js
+++ javascript/close_refresh/head.js
@@ -9,7 +9,6 @@ setTimeout('closeWindow()', timeout * 1000);
  function closeWindow() {
      if (refresh) {
          if (loc_new) {
-             alert(loc_new);
              window.opener.location.href = loc_new;
          } else {
              window.opener.location.href = window.opener.location.href;
