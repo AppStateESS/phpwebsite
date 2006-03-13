d5272c9308e41bde49c99a0abafa87638623321c
Matt McNaney <matt@tux.appstate.edu>
2006-03-13 19:05:03 +0000

    added module proper names to the mini admin box

--- mod/miniadmin/class/MiniAdmin.php
+++ mod/miniadmin/class/MiniAdmin.php
@@ -1,10 +1,19 @@
 <?php
 
+  /**
+   * Simple class to add a module's administrator commands to a box
+   *
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
 class MiniAdmin {
     function add($module, $links)
     {
         if (!is_array($links)) {
-            $links = array($links);
+            $hold = $links;
+            unset($links);
+            $links[] = $hold;
         }
 
         $GLOBALS['MiniAdmin'][$module] = $links;
@@ -13,18 +22,29 @@ class MiniAdmin {
 
     function get()
     {
+        $modlist = PHPWS_Core::getModuleNames();
+
         if (!isset($GLOBALS['MiniAdmin'])) {
             return NULL;
         }
 
+        $oTpl = & new PHPWS_Template('miniadmin');
+        $oTpl->setFile('mini_admin.tpl');
+
         $tpl['MINIADMIN_TITLE'] = _('MiniAdmin');
         foreach ($GLOBALS['MiniAdmin'] as $module => $links) {
             foreach ($links as $link) {
-                $tpl['links'][] = array('ADMIN_LINK' => $link);
+                $oTpl->setCurrentBlock('links');
+                $oTpl->setData(array('ADMIN_LINK' => $link));
+                $oTpl->parseCurrentBlock();
             }
+            $oTpl->setCurrentBlock('module');
+            $oTpl->setData(array('MODULE' => $modlist[$module]));
+            $oTpl->parseCurrentBlock();
         }
+        $oTpl->setData($tpl);
+        $content = $oTpl->get();
 
-        $content = PHPWS_Template::process($tpl, 'miniadmin', 'mini_admin.tpl');
         Layout::add($content, 'users', 'mini_admin');
     }
 }
--- mod/miniadmin/templates/mini_admin.tpl
+++ mod/miniadmin/templates/mini_admin.tpl
@@ -1,10 +1,13 @@
-<div class="box">
-  <div class="box-title"><h1>{MINIADMIN_TITLE}</h1></div>
-  <div class="box-content">
-  <ul class="no-bullet">
-    <!-- BEGIN links -->
-    <li>{ADMIN_LINK}</li>
-    <!-- END links -->
-  </ul>
+<div id="miniadmin">
+  <div class="box">
+    <div class="box-title"><h1>{MINIADMIN_TITLE}</h1></div>
+    <div class="box-content">
+    <!-- BEGIN module -->
+      <h2>{MODULE}</h2>
+      <ul class="no-bullet">
+        <!-- BEGIN links --><li>{ADMIN_LINK}</li><!-- END links -->
+      </ul>
+    <!-- END module -->
+    </div>
   </div>
 </div>
