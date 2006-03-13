c9d602fe690589552fef35b9fe2c60b740798653
Matt McNaney <matt@tux.appstate.edu>
2006-03-13 19:04:29 +0000

    Layout now sends user to error page if bad command sent to it

--- mod/layout/index.php
+++ mod/layout/index.php
@@ -1,11 +1,18 @@
 <?php
+
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
 if (!defined('PHPWS_SOURCE_DIR')) {
     include '../../config/core/404.html';
     exit();
 }
 
-if ($_REQUEST['module'] != 'layout' || !isset($_REQUEST['action']))
-  exit();
+if ($_REQUEST['module'] != 'layout' || !isset($_REQUEST['action'])) {
+     PHPWS_Core::errorPage('404');
+ }
 
 PHPWS_Core::initModClass('layout', 'LayoutAdmin.php');
 
@@ -13,6 +20,9 @@ switch ($_REQUEST['action']){
  case 'admin':
    Layout_Admin::admin();
    break;
+
+ default:
+     PHPWS_Core::errorPage('404');
 } // END action switch
 
 
