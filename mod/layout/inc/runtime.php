9d0e33a2b341356fac10f982ebe4769750d38eb6
Matt McNaney <matt@tux.appstate.edu>
2006-03-13 19:04:40 +0000

    miniadmin link added if box move turned on

--- mod/layout/inc/runtime.php
+++ mod/layout/inc/runtime.php
@@ -5,4 +5,7 @@
    */
 
 Layout::checkSettings();
+if (Layout::isMoveBox() && Current_User::allow('layout')) {
+    MiniAdmin::add('layout', sprintf('<a href="index.php?module=layout&action=admin&command=turn_off_box_move">%s</a>', _('Turn off box move')));
+ }
 ?>
