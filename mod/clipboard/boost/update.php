ced60202e707257ed42e7e0ff3f8bd7b9ccdd305
Matt McNaney <matt@tux.appstate.edu>
2006-03-13 18:27:56 +0000

    dumping clipboard icon in update

---
+++ mod/clipboard/boost/update.php
@@ -0,0 +1,21 @@
+<?php
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
+function clipboard_update(&$content, $currentVersion)
+{
+    switch ($currentVersion) {
+    case version_compare($currentVersion, '0.0.2', '<'):
+        $db = & new PHPWS_DB('controlpanel_link');
+        $db->addWhere('itemname', 'clipboard');
+        $db->delete();
+        $content[] = 'Removing Clipboard\'s Control Panel link.';
+        return TRUE;
+        break;
+    }
+
+}
+
+?>
--- mod/clipboard/conf/boost.php
+++ mod/clipboard/conf/boost.php
@@ -1,13 +1,19 @@
 <?php
 
-$proper_name  = "Clipboard";
-$version      = "0.0.1";
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
+
+$proper_name  = 'Clipboard';
+$version      = '0.0.2';
 $register     = FALSE;
 $unregister   = FALSE;
 $import_sql   = FALSE;
 $image_dir    = FALSE;
 $file_dir     = FALSE;
-$version_http = "http://phpwebsite.appstate.edu/test/update.ini";
+$version_http = 'http://phpwebsite.appstate.edu/test/update.ini';
 $about        = FALSE;
 $priority     = 50;
 
