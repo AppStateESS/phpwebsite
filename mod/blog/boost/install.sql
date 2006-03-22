da7a02eceaa0dd2e7e5d1d48a243340cf915df60
Matt McNaney <matt@tux.appstate.edu>
2006-03-22 20:33:49 +0000

    install sql missing some column changes

--- mod/blog/boost/install.sql
+++ mod/blog/boost/install.sql
@@ -4,6 +4,7 @@ key_id INT NOT NULL,
 title VARCHAR( 40 ) NOT NULL ,
 entry TEXT NOT NULL ,
 author varchar(50) NOT NULL default '',
-date INT NOT NULL ,
+create_date INT NOT NULL ,
+allow_comments SMALLINT NOT NULL default'0',
 PRIMARY KEY ( id )
 );
