eea2ecf83b640313b6389a0dd188c60f76f16ae6
Matt McNaney <matt@tux.appstate.edu>
2006-03-23 21:50:18 +0000

    comment header added

--- mod/users/boost/update.php
+++ mod/users/boost/update.php
@@ -1,29 +1,35 @@
 <?php
 
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
+
 function users_update(&$content, $currentVersion)
 {
 
-  switch ($currentVersion) {
+    switch ($currentVersion) {
 
-  case version_compare($currentVersion, '2.0.2', '<'):
-    $result = users_update_202($content);
-    if (PEAR::isError($result)) {
-      return $result;
+    case version_compare($currentVersion, '2.0.2', '<'):
+        $result = users_update_202($content);
+        if (PEAR::isError($result)) {
+            return $result;
+        }
+        $content[] = '+ added ability to pick a default user menu.';
+        $content[] = '+ added graphi confirmation option';
+        $content[] = '- dropped default_group column';
+        break;
     }
-    $content[] = '+ added ability to pick a default user menu.';
-    $content[] = '+ added graphi confirmation option';
-    $content[] = '- dropped default_group column';
-    break;
-  }
 
-  return TRUE;
+    return TRUE;
 }
 
 function users_update_202(&$content)
 {
-  $filename = PHPWS_SOURCE_DIR . 'mod/users/boost/update_2_0_2.sql';
-  $db = & new PHPWS_DB;
-  return $db->importFile($filename);
+    $filename = PHPWS_SOURCE_DIR . 'mod/users/boost/update_2_0_2.sql';
+    $db = & new PHPWS_DB;
+    return $db->importFile($filename);
 }
 
 ?>
