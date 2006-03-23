bf4821fdbb93e64aa2c1ec2b57cd7e862d0201f8
Matt McNaney <matt@tux.appstate.edu>
2006-03-23 18:10:57 +0000

    comments demographics file

---
+++ mod/comments/conf/demographics.php
@@ -0,0 +1,17 @@
+<?php
+
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
+  // New fields for comments
+$fields['avatar']['limit']    = 255; // small graphic of user
+$fields['signature']['limit'] = 255; // graphic or saying from user
+
+
+// default fields used
+$default[] = 'contact_email';
+$default[] = 'website';
+
+?>
