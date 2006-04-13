38f88c399e605f0146b83ffda3fce93e79c463da
Matt McNaney <matt@tux.appstate.edu>
2006-04-13 17:33:29 +0000

    tabified and commented

--- mod/layout/boost/install.php
+++ mod/layout/boost/install.php
@@ -1,60 +1,71 @@
 <?php
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
 
 function layout_install(&$content, $branchInstall=FALSE)
 {
-  $page_title = NULL;
+    $page_title = NULL;
 
-  if (isset($_POST['process_layout'])) {
-    if (empty($_POST['page_title'])) {
-      $error  = _('Please enter a page title.');
+    if (isset($_POST['process_layout'])) {
+        if (empty($_POST['page_title'])) {
+            $error  = _('Please enter a page title.');
+        } else {
+            $page_title = strip_tags($_POST['page_title']);
+            $default_theme = $_POST['theme'];
+        }
+
+        if (!isset($error)) {
+            $db = & new PHPWS_DB('layout_config');
+            $db->addValue('default_theme', trim($default_theme));
+            $db->addValue('page_title', $page_title);
+            $db->update();
+            $content[] = _('Layout settings updated.');
+            return TRUE;
+        } else {
+            $tpl['ERROR'] = $error;
+        }
     } else {
-      $page_title = strip_tags($_POST['page_title']);
-      $default_theme = $_POST['theme'];
+        $page_title = strip_tags($_POST['page_title']);
+        $default_theme = $_POST['theme'];
     }
 
-    if (!isset($error)) {
-      $db = & new PHPWS_DB('layout_config');
-      $db->addValue('default_theme', trim($default_theme));
-      $db->addValue('page_title', $page_title);
-      $db->update();
-      $content[] = _('Layout settings updated.');
-      return TRUE;
+    PHPWS_Core::initCoreClass('File.php');
+    $theme_dir = PHPWS_SOURCE_DIR . 'themes/';
+    $available_themes = PHPWS_File::readDirectory($theme_dir, TRUE);
+
+    $form = & new PHPWS_Form;
+    if (isset($_REQUEST['module'])) {
+        $form->addHidden('module', $_REQUEST['module']);
+        if ($_REQUEST['module'] == 'branch') {
+            $form->addHidden('command', $_REQUEST['command']);
+            $form->addHidden('branch_id', $_REQUEST['branch_id']);
+        }
     } else {
-      $tpl['ERROR'] = $error;
+        $form->addHidden('step', 3);
     }
-  } else {
-    $page_title = 'My phpWebSite';
-    $default_theme = 'zen';
-  }
+    $form->addHidden('process_layout', 1);
 
-  PHPWS_Core::initCoreClass('File.php');
-  $theme_dir = PHPWS_SOURCE_DIR . 'themes/';
-  $available_themes = PHPWS_File::readDirectory($theme_dir, TRUE);
+    if (empty($available_themes)) {
+        $content[] = _('No themes installed.');
+        $content[] = _('Expect an error theme when finished.');
+    } else {
+        $form->addSelect('theme', $available_themes);
+        $form->reindexValue('theme');
+        $form->setLabel('theme', _('Pick a theme'));
+        $form->setMatch('theme', $default_theme);
+    }
 
-  $form = & new PHPWS_Form;
-  $form->addHidden('step', 3);
-  $form->addHidden('module', 'layout');
-  $form->addHidden('process_layout', 1);
-
-  if (empty($available_themes)) {
-    $content[] = _('No themes installed.');
-    $content[] = _('Expect an error theme when finished.');
-  } else {
-    $form->addSelect('theme', $available_themes);
-    $form->reindexValue('theme');
-    $form->setLabel('theme', _('Pick a theme'));
-    $form->setMatch('theme', $default_theme);
-  }
-
-  $form->addText('page_title', $page_title);
-  $form->setLabel('page_title', _('Page Title'));
-  $form->setTitle('page_title', _('Page Title: Name of your web site'));
-  $form->addSubmit(_('Done'));
+    $form->addText('page_title', $page_title);
+    $form->setLabel('page_title', _('Page Title'));
+    $form->setTitle('page_title', _('Page Title: Name of your web site'));
+    $form->addSubmit(_('Done'));
   
-  $template = $form->getTemplate();
-  $content[] = PHPWS_Template::process($template, 'layout', 'setup.tpl');
+    $template = $form->getTemplate();
+    $content[] = PHPWS_Template::process($template, 'layout', 'setup.tpl');
 
-  return FALSE;
+    return FALSE;
 
 }
 
