aa5d2efc92ac410f8ce655a08f5abeb638ff910f
Matt McNaney <matt@tux.appstate.edu>
2006-04-26 18:18:12 +0000

    fixed incompatibilities

--- mod/search/boost/install.sql
+++ mod/search/boost/install.sql
@@ -1,8 +1,9 @@
 CREATE TABLE search (
-  key_id int(11) NOT NULL default '0',
-  keywords text NOT NULL,
-  INDEX key_id (key_id)
+  key_id int NOT NULL default '0',
+  keywords text NOT NULL
 );
+
+CREATE INDEX search_index on search (key_id);
 
 CREATE TABLE search_stats (
   keyword varchar(50) NOT NULL default '',
@@ -10,10 +11,11 @@ CREATE TABLE search_stats (
   query_failure int NOT NULL default '0',
   mixed_query int NOT NULL default '0',
   total_query int NOT NULL default '0',
-  highest_result smallint(6) NOT NULL default '0',
+  highest_result smallint NOT NULL default '0',
   last_called int NOT NULL default '0',
   multiple_word int NOT NULL default '0',
   exact_success int NOT NULL default '0',
-  ignored smallint NOT NULL default '0',
-  INDEX keyword (keyword)
+  ignored smallint NOT NULL default '0'
 );
+
+CREATE INDEX search_stats_index on search_stats (keyword);
