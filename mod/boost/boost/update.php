ff8900a64c0ab17388d149244d558b7e7d26a680
Matt McNaney <matt@tux.appstate.edu>
2006-05-01 17:36:27 +0000

    removed breaks added update to 1.8.0, new error file

--- mod/boost/boost/update.php
+++ mod/boost/boost/update.php
@@ -11,14 +11,12 @@ function boost_update(&$content, $currentVersion)
     case version_compare($currentVersion, '1.6.1', '<'):
         $content[] = '+ Boost wasn\'t updating modules if there wasn\'t an update.php file.';
         $content[] = '+ Dependencies now checked during update.';
-        break;
 
     case version_compare($currentVersion, '1.7.0', '<'):
         $content[] = '+ Adding dependency table for core.';
         $filename = PHPWS_SOURCE_DIR . 'mod/boost/boost/update_1_7_0.sql';
         $db = & new PHPWS_DB;
         return $db->importFile($filename);
-        break;
 
     case version_compare($currentVersion, '1.7.1', '<'):
         $content[] = 'Add converted table.';
@@ -26,7 +24,19 @@ function boost_update(&$content, $currentVersion)
         $filename = PHPWS_SOURCE_DIR . 'mod/boost/boost/update_1_7_1.sql';
         $db = & new PHPWS_DB;
         return $db->importFile($filename);
-        break;
+
+    case version_compare($currentVersion, '1.8.0', '<'):
+        $content[] = 'Added error messages.';
+        $result = PHPWS_Boost::updateFiles(array('conf/error.php'), 'boost');
+        if (!$result) {
+            $content[] = _('Failed to update local files.');
+            return FALSE;
+        } elseif (PEAR::isError($result)) {
+            PHPWS_Error::log($result);
+            $content[] = _('Failed to update local files. Check your log.');
+        } else {
+            $content[] = _('Files updated successfully.');
+        }
     }
 
     return TRUE;
