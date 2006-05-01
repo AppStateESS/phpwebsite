80720dd16e48570ef4c1ed4d1db9e1fa79c3f2ea
Matt McNaney <matt@tux.appstate.edu>
2006-05-01 17:36:08 +0000

    commented upped version

--- mod/boost/boost/boost.php
+++ mod/boost/boost/boost.php
@@ -1,6 +1,11 @@
 <?php
+  /**
+   * @version $Id$
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   */
+
 $proper_name = 'Boost';
-$version = '1.7.1';
+$version = '1.8.0';
 $register = TRUE;
 $import_sql = FALSE;
 $image_dir = FALSE;
