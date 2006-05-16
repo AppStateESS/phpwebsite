60a4d236ccd15e65f559e7a8d11058eee2d1f14f
Matt McNaney <matt@tux.appstate.edu>
2006-05-16 19:37:02 +0000

    upped the version. no upgrade path

--- mod/profiler/boost/boost.php
+++ mod/profiler/boost/boost.php
@@ -1,6 +1,12 @@
 <?php
+
+  /**
+   * @version $Id$
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   */
+
 $proper_name  = 'Profiler';
-$version      = '0.0.1';
+$version      = '0.1.0';
 $register     = FALSE; // You could also leave this blank
 $unregister   = FALSE; // Ditto
 $import_sql   = TRUE;
