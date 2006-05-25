7b91b3b44709c9cd1a62f8a823468142e05c8182
Matt McNaney <matt@tux.appstate.edu>
2006-05-25 20:23:15 +0000

    moved defines to config file

---
+++ mod/filecabinet/conf/config.php
@@ -0,0 +1,23 @@
+<?php
+
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
+define('MAX_TN_IMAGE_WIDTH', 80);
+define('MAX_TN_IMAGE_HEIGHT', 80);
+
+define('FC_VIEW_MARGIN_WIDTH', 20);
+define('FC_VIEW_MARGIN_HEIGHT', 100);
+
+define('FC_UPLOAD_WIDTH', 450);
+define('FC_UPLOAD_HEIGHT', 450);
+
+define('FC_MANAGER_WIDTH', 640);
+define('FC_MANAGER_HEIGHT', 480);
+
+define('FC_NONE_IMAGE_SRC', 'images/mod/filecabinet/none.png');
+
+
+?>
