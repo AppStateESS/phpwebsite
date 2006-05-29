5cd25a7d04ec2f4fdd058abf35e9316c1e471f6d
Matt McNaney <matt@tux.appstate.edu>
2006-05-29 19:33:52 +0000

    install was missing two columns

--- mod/blog/boost/install.sql
+++ mod/blog/boost/install.sql
@@ -2,10 +2,12 @@ CREATE TABLE blog_entries (
 id INT NOT NULL,
 key_id INT NOT NULL,
 title VARCHAR( 40 ) NOT NULL ,
-entry TEXT NOT NULL ,
+entry TEXT NOT NULL,
+author_id INT NOT NULL default '0',
 author varchar(50) NOT NULL default '',
 create_date INT NOT NULL ,
-allow_comments SMALLINT NOT NULL default'0',
+allow_comments SMALLINT NOT NULL default '0',
+approved INT NOT NULL default '0',
 PRIMARY KEY ( id )
 );
 
