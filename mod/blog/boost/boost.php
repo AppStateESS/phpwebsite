d31d2b6665e9f114fe2d773b4421cb50847db958
Matt McNaney <matt@tux.appstate.edu>
2006-05-30 18:36:18 +0000

    removed notes

--- mod/blog/boost/boost.php
+++ mod/blog/boost/boost.php
@@ -6,8 +6,8 @@
 
 $proper_name  = 'Blog';
 $version      = '0.2.0';
-$register     = FALSE; // You could also leave this blank
-$unregister   = FALSE; // Ditto
+$register     = FALSE;
+$unregister   = FALSE;
 $import_sql   = TRUE;
 $image_dir    = TRUE;
 $file_dir     = FALSE;
