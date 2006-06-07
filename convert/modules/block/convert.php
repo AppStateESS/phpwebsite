7ad972c4448c07d9f6f0bfbbd666687331a0cd87
Matt McNaney <matt@tux.appstate.edu>
2006-06-07 12:54:23 +0000

    block conversion written

---
+++ convert/modules/block/convert.php
@@ -0,0 +1,54 @@
+<?php
+
+  /**
+   * Block conversion file
+   *
+   * Converts old blocks to new module
+   *
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
+PHPWS_Core::initModClass('block', 'Block_Item.php');
+
+function convert()
+{
+    if (Convert::isConverted('block')) {
+        return _('Block is already converted.');
+    }
+
+    $mod_list = PHPWS_Core::installModList();
+    
+    if (!in_array('block', $mod_list)) {
+        return _('Block is not installed.');
+    }
+
+    $db = Convert::getSourceDB('mod_blockmaker_data');
+    $all_blocks = $db->select();
+    $db->disconnect();
+
+    if (empty($all_blocks)) {
+        return _('No blocks found.');
+    } elseif (PEAR::isError($all_blocks)) {
+        PHPWS_Error::log($all_blocks);
+        return _('An error occurred while accessing your mod_blockmaker_data table.');
+    }
+   
+
+    foreach ($all_blocks as $old_block) {
+        $new_block = & new Block_Item;
+        $new_block->setTitle($old_block['block_title']);
+        $new_block->setContent($old_block['block_content'] . $old_block['block_footer']);
+        $result = $new_block->save();
+        if (PEAR::isError($result)) {
+            PHPWS_Error::log($all_blocks);
+            return _('An error occurred while converting your old blocks.');
+        }
+    }
+    Convert::addConvert('block');
+    return _('Blocks converted.');
+
+}
+
+
+?>
