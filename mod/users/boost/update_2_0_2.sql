67170b026b97ea59735ebd203fa0573499e1387f
Matt McNaney <matt@tux.appstate.edu>
2006-06-23 17:55:23 +0000

    old version, removing

--- mod/users/boost/update_2_0_2.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE users_config ADD user_menu VARCHAR( 40 ) NOT NULL;
-ALTER TABLE users_config ADD graphic_confirm SMALLINT NOT NULL DEFAULT 0;
-ALTER TABLE users_config DROP COLUMN default_group;
-UPDATE users_config set user_menu='Default.tpl';
