cb32b5ee439ea715454f75585c1192aebd9d7392
Matt McNaney <matt@tux.appstate.edu>
2006-06-23 17:55:32 +0000

    updated version

--- mod/users/boost/boost.php
+++ mod/users/boost/boost.php
@@ -5,7 +5,7 @@
    */
 
 $proper_name  = 'Users';
-$version      = '2.0.4';
+$version      = '2.0.5';
 $register     = TRUE;
 $unregister   = TRUE;
 $import_sql   = TRUE;
--- mod/users/boost/update.php
+++ mod/users/boost/update.php
@@ -11,30 +11,6 @@ function users_update(&$content, $currentVersion)
 
     switch ($currentVersion) {
 
-    case version_compare($currentVersion, '2.0.2', '<'):
-        $result = users_update_202($content);
-        if (PEAR::isError($result)) {
-            return $result;
-        }
-        $content[] = '+ added ability to pick a default user menu.';
-        $content[] = '+ added graphic confirmation option';
-        $content[] = '- dropped default_group column';
-
-    case version_compare($currentVersion, '2.0.3', '<'):
-        $tpl_dir = 'templates/forms/';
-        $files[] = $tpl_dir . 'groupForm.tpl';
-        $files[] = $tpl_dir . 'memberForm.tpl';
-        $files[] = $tpl_dir . 'permission_pop.tpl';
-        $files[] = $tpl_dir . 'permissions.tpl';
-        $files[] = $tpl_dir . 'userForm.tpl';
-        $files[] = 'templates/main.tpl';
-        if (!PHPWS_Boost::updateFiles($files, 'users')) {
-            $content[] = 'Failed to update template files.';
-            return FALSE;
-        }
-
-        $content[] = '+ Added extra administrative links for managing groups and users.';
-
     case version_compare($currentVersion, '2.0.4', '<'):
         if (!PHPWS_Boost::updateFiles(array('conf/config.php'), 'users')) {
             $content[] = 'Failed to update config.php file.';
@@ -50,15 +26,18 @@ function users_update(&$content, $currentVersion)
             return $result;
         }
         $content[] = '+ Created user signup authorization table.';
+
+    case version_compare($currentVersion, '2.0.5', '<'):
+        $files[] = 'templates/forms/permission_menu.tpl';
+        $content[] = '+ Added ability to change deity status.';
+        $content[] = '+ fixed h1 tag in template';
+
+        if (!PHPWS_Boost::updateFiles($files, 'users')) {
+            $content[] = 'Failed copying template file locally.';
+        }
     }
 
     return TRUE;
-}
-
-function users_update_202(&$content)
-{
-    $filename = PHPWS_SOURCE_DIR . 'mod/users/boost/update_2_0_2.sql';
-    return $db->importFile($filename);
 }
 
 ?>
