51f8f5910ecfa7553ff89da25ff7f7ee4e1eba54
Matt McNaney <matt@tux.appstate.edu>
2006-07-07 16:20:27 +0000

    commented

--- javascript/prompt/default.php
+++ javascript/prompt/default.php
@@ -1,7 +1,13 @@
 <?php
 
+  /**
+   * @version $Id$
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   */
+
 $default['question']      = _('Change to');
 $default['answer']        = '';
 $default['value_name']    = 'prompt';
 $default['link']          = 'Prompt!';
+
 ?>
