baf7e5c524d10053e595cd346d81070453035da6
Matt McNaney <matt@tux.appstate.edu>
2006-08-11 13:26:35 +0000

    commented and tidied

--- mod/users/inc/parse.php
+++ mod/users/inc/parse.php
@@ -1,18 +1,24 @@
 <?php
 
+  /**
+   * @author Matthew McNaney
+   * @version $Id$
+   */
+
 function new_account($item)
 {
-  if (!PHPWS_User::getUserSetting('new_user_method') > 0) {
-    return _('New user signup is currently disabled.');
-  }
-  $signup_vars = array('action'  => 'user',
-		       'command' => 'signup_user');
-  if (!empty($item[1])) {
-    $link = strip_tags($item[1]);
-  } else {
-    $link = USER_SIGNUP_QUESTION;
-  }
-  return PHPWS_Text::moduleLink($link, 'users', $signup_vars);
+    if (!PHPWS_User::getUserSetting('new_user_method') > 0) {
+        return _('New user signup is currently disabled.');
+    }
+    $signup_vars = array('action'  => 'user',
+                         'command' => 'signup_user');
+    if (!empty($item[1])) {
+        $link = strip_tags($item[1]);
+    } else {
+        $link = USER_SIGNUP_QUESTION;
+    }
+
+    return PHPWS_Text::moduleLink($link, 'users', $signup_vars);
 }
 
 ?>
