dcfdd2bbb27daad31237947d5255402d31777e91
Matt McNaney <matt@tux.appstate.edu>
2006-08-18 20:41:32 +0000

    commented

--- mod/blog/conf/config.php
+++ mod/blog/conf/config.php
@@ -1,5 +1,10 @@
 <?php
 
+  /**
+   * @version $Id$
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   */
+
 define('BLOG_VIEW_DATE_FORMAT', '%T, %A, %B %e');
 
 define('BLOG_CACHE_KEY', 'front_blog_page');
