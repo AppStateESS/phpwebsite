4c6767a3e73f1f560a3bb49f4e45d87f4f38d71a
Matt McNaney <matt@tux.appstate.edu>
2006-08-18 20:41:06 +0000

    added missing columns

--- mod/blog/boost/install.sql
+++ mod/blog/boost/install.sql
@@ -2,12 +2,14 @@ CREATE TABLE blog_entries (
 id INT NOT NULL,
 key_id INT NOT NULL,
 title VARCHAR( 60 ) NOT NULL ,
+summary TEXT NULL,
 entry TEXT NOT NULL,
 author_id INT NOT NULL default '0',
 author varchar(50) NOT NULL default '',
 create_date INT NOT NULL ,
 allow_comments SMALLINT NOT NULL default '0',
 approved INT NOT NULL default '0',
+allow_anon SMALLINT NOT NULL default '0',
 PRIMARY KEY ( id )
 );
 
