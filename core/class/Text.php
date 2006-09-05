97dfff1ce3d22279d66d09c2b7b4898de14998f7
Matt McNaney <matt@tux.appstate.edu>
2006-09-05 12:45:00 +0000

    Changed the bbcode decoder to use str_ireplace, thanks single

--- core/class/Text.php
+++ core/class/Text.php
@@ -39,7 +39,7 @@ class PHPWS_Text {
         }
 
         if ($decode) {
-            if ((int)substr(phpversion(),0,1) > 4) {
+            if (version_compare(phpversion(), '5.0.0', '>=')) {
                 $this->text = html_entity_decode($text, ENT_QUOTES, 'UTF-8');
             } else {
                 $this->text = $this->html_entity_decode($text, ENT_QUOTES, 'ISO-8859-1');
@@ -670,8 +670,8 @@ class PHPWS_Text {
          there may be multiple <pre> blocks, so we grab them all and create an array     */
         $pre = array(); $i=0;
         while ($pre_str = stristr($bb2html,'[pre]')) {
-            $pre_str = substr($pre_str,0,strpos($pre_str,'[/pre]')+6);
-            $bb2html = str_replace($pre_str, "***pre_string***$i", $bb2html);
+            $pre_str = substr($pre_str,0,stripos($pre_str,'[/pre]')+6);
+            $bb2html = str_ireplace($pre_str, "***pre_string***$i", $bb2html);
             $pre[$i] = str_replace("\r\n","\n",$pre_str);
             $i++;
         }
@@ -683,52 +683,52 @@ class PHPWS_Text {
         $bb2html = str_replace(']]', '**@^@**', $bb2html);
         
         // news headline block
-        $bb2html = str_replace('[news]', 
+        $bb2html = str_ireplace('[news]', 
                                '<table width="20%" border="0" align="right"><tr><td align="center"><span class="news">', $bb2html);
-        $bb2html = str_replace('[/news]', '</span></td></tr></table>', $bb2html);
+        $bb2html = str_ireplace('[/news]', '</span></td></tr></table>', $bb2html);
         
         // references - we need to create the whole string first, for the str_replace
         $r1 = '<a href="#refs-'.$title.'" title="'.$title.'"><font class="ref"><sup>';
-        $bb2html = str_replace('[ref]', $r1 , $bb2html);
+        $bb2html = str_ireplace('[ref]', $r1 , $bb2html);
         $r2 = '<p id="refs-'.$title.'"></p>
 <font class="ref"><b><u><a title="back to the text" href="javascript:history.go(-1)">references:</a></u><br /><br />1: </b></font><font class="reftext">';
-        $bb2html = str_replace('[reftxt1]', $r2 , $bb2html);
-        $bb2html = str_replace('[reftxt2]', '<font class="ref"><b>2: </b></font><font class="reftext">', $bb2html);
-        $bb2html = str_replace('[reftxt3]', '<font class="ref"><b>3: </b></font><font class="reftext">', $bb2html);
-        $bb2html = str_replace('[reftxt4]', '<font class="ref"><b>4: </b></font><font class="reftext">', $bb2html);
-        $bb2html = str_replace('[reftxt5]', '<font class="ref"><b>5: </b></font><font class="reftext">', $bb2html);
-        $bb2html = str_replace('[/ref]', '</sup></font></a>', $bb2html);
-        $bb2html = str_replace('[/reftxt]', '</font>', $bb2html);
+        $bb2html = str_ireplace('[reftxt1]', $r2 , $bb2html);
+        $bb2html = str_ireplace('[reftxt2]', '<font class="ref"><b>2: </b></font><font class="reftext">', $bb2html);
+        $bb2html = str_ireplace('[reftxt3]', '<font class="ref"><b>3: </b></font><font class="reftext">', $bb2html);
+        $bb2html = str_ireplace('[reftxt4]', '<font class="ref"><b>4: </b></font><font class="reftext">', $bb2html);
+        $bb2html = str_ireplace('[reftxt5]', '<font class="ref"><b>5: </b></font><font class="reftext">', $bb2html);
+        $bb2html = str_ireplace('[/ref]', '</sup></font></a>', $bb2html);
+        $bb2html = str_ireplace('[/reftxt]', '</font>', $bb2html);
         
         // ordinary transformations..
         // we rely on the browser producing \r\n (DOS) carriage returns, as per spec
-        //    $bb2html = str_replace("\r",'<br />', $bb2html);  // the \n remains, makes the raw html readable
-        $bb2html = str_replace('[b]', '<b>', $bb2html);
-        $bb2html = str_replace('[/b]', '</b>', $bb2html);
-        $bb2html = str_replace('[i]', '<i>', $bb2html);
-        $bb2html = str_replace('[/i]', '</i>', $bb2html);
-        $bb2html = str_replace('[u]', '<u>', $bb2html);
-        $bb2html = str_replace('[/u]', '</u>', $bb2html);
-        $bb2html = str_replace('[big]', '<span class="bigger">', $bb2html);
-        $bb2html = str_replace('[/big]', '</span>', $bb2html);
-        $bb2html = str_replace('[sm]', '<span class="smaller">', $bb2html);
-        $bb2html = str_replace('[/sm]', '</span>', $bb2html);
+        //    $bb2html = str_ireplace("\r",'<br />', $bb2html);  // the \n remains, makes the raw html readable
+        $bb2html = str_ireplace('[b]', '<b>', $bb2html);
+        $bb2html = str_ireplace('[/b]', '</b>', $bb2html);
+        $bb2html = str_ireplace('[i]', '<i>', $bb2html);
+        $bb2html = str_ireplace('[/i]', '</i>', $bb2html);
+        $bb2html = str_ireplace('[u]', '<u>', $bb2html);
+        $bb2html = str_ireplace('[/u]', '</u>', $bb2html);
+        $bb2html = str_ireplace('[big]', '<span class="bigger">', $bb2html);
+        $bb2html = str_ireplace('[/big]', '</span>', $bb2html);
+        $bb2html = str_ireplace('[sm]', '<span class="smaller">', $bb2html);
+        $bb2html = str_ireplace('[/sm]', '</span>', $bb2html);
         
         
         // tables (couldn't resist this, too handy)
-        $bb2html = str_replace('[t]', '<table width="100%" border="0" cellspacing="0" cellpadding="0">', $bb2html);     
-        $bb2html = str_replace('[bt]', '<table width="100%" border="1" cellspacing="0" cellpadding="3">', $bb2html);
-        $bb2html = str_replace('[st]', '<table width="100%" border="0" cellspacing="3" cellpadding="3">', $bb2html);    
-        $bb2html = str_replace('[/t]', '</table>', $bb2html);
-        $bb2html = str_replace('[c]', '<td valign=top>', $bb2html);     // cell data
-        $bb2html = str_replace('[/c]', '</td>', $bb2html);
-        $bb2html = str_replace('[r]', '<tr>', $bb2html);        // a row
-        $bb2html = str_replace('[/r]', '</tr>', $bb2html);
+        $bb2html = str_ireplace('[t]', '<table width="100%" border="0" cellspacing="0" cellpadding="0">', $bb2html);     
+        $bb2html = str_ireplace('[bt]', '<table width="100%" border="1" cellspacing="0" cellpadding="3">', $bb2html);
+        $bb2html = str_ireplace('[st]', '<table width="100%" border="0" cellspacing="3" cellpadding="3">', $bb2html);    
+        $bb2html = str_ireplace('[/t]', '</table>', $bb2html);
+        $bb2html = str_ireplace('[c]', '<td valign=top>', $bb2html);     // cell data
+        $bb2html = str_ireplace('[/c]', '</td>', $bb2html);
+        $bb2html = str_ireplace('[r]', '<tr>', $bb2html);        // a row
+        $bb2html = str_ireplace('[/r]', '</tr>', $bb2html);
         
         // a simple list
         $bb2html = str_replace('[*]', '<li>', $bb2html);
-        $bb2html = str_replace('[list]', '<ul>', $bb2html);
-        $bb2html = str_replace('[/list]', '</ul>', $bb2html);
+        $bb2html = str_ireplace('[list]', '<ul>', $bb2html);
+        $bb2html = str_ireplace('[/list]', '</ul>', $bb2html);
         
         if (ALLOW_BB_SMILIES) {
             $bb2html = PHPWS_Text::getSmilie($bb2html);
@@ -736,11 +736,11 @@ class PHPWS_Text {
 
         // anchors and stuff..
         if (ALLOW_BB_IMAGES) {
-            $bb2html = str_replace('[img]', '<img border="0" src="', $bb2html);
-            $bb2html = str_replace('[/img]', '" alt="an image" />', $bb2html);
+            $bb2html = str_ireplace('[img]', '<img border="0" src="', $bb2html);
+            $bb2html = str_ireplace('[/img]', '" alt="an image" />', $bb2html);
         } else {
-            $bb2html = str_replace('[img]', '[' . _('No Images') . ']', $bb2html);
-            $bb2html = str_replace('[/img]', '[/' . _('No Images') . ']', $bb2html);
+            $bb2html = str_ireplace('[img]', '[' . _('No Images') . ']', $bb2html);
+            $bb2html = str_ireplace('[/img]', '[/' . _('No Images') . ']', $bb2html);
         }
 
 
@@ -751,28 +751,28 @@ class PHPWS_Text {
                                     array('<fieldset class="quote">&#013;<legend>' . _('\1 wrote') . ':</legend>&#013;\2', '<fieldset>&#013;', '</fieldset>&#013;'), $bb2html);
         } elseif (BBCODE_QUOTE_TYPE == 'blockquote') {
             $bb2html = preg_replace('/\[quote="(.*)"\]/Ui', '<blockquote class="quote"><h3>' . _('\1 wrote') . ':</h3>', $bb2html);
-            $bb2html = str_replace('[quote]', '<blockquote class="quote">', $bb2html);
-            $bb2html = str_replace('[/quote]', '</blockquote>', $bb2html);
+            $bb2html = str_ireplace('[quote]', '<blockquote class="quote">', $bb2html);
+            $bb2html = str_ireplace('[/quote]', '</blockquote>', $bb2html);
         }
  
         // code
         $bb2html = preg_replace('/\[code\](.*)\[\/code\]/Uies', "'<code>' . stripslashes(htmlentities('\\1')) . '</code>'", $bb2html);
 
         // divisions..
-        $bb2html = str_replace('[hr]', '<hr />', $bb2html);
-        $bb2html = str_replace('[block]', '<blockquote>', $bb2html);
-        $bb2html = str_replace('[/block]', '</blockquote>', $bb2html);
+        $bb2html = str_ireplace('[hr]', '<hr />', $bb2html);
+        $bb2html = str_ireplace('[block]', '<blockquote>', $bb2html);
+        $bb2html = str_ireplace('[/block]', '</blockquote>', $bb2html);
         
         // dropcaps. five flavours, small up to large.. [dc1]I[/dc] >> [dc5]W[/dc]
-        $bb2html = str_replace('[dc1]', '<span class="dropcap1">', $bb2html);
-        $bb2html = str_replace('[dc2]', '<span class="dropcap2">', $bb2html);
-        $bb2html = str_replace('[dc3]', '<span class="dropcap3">', $bb2html);
-        $bb2html = str_replace('[dc4]', '<span class="dropcap4">', $bb2html);
-        $bb2html = str_replace('[dc5]', '<span class="dropcap5">', $bb2html);
-        $bb2html = str_replace('[/dc]', '<dc></span>', $bb2html);
+        $bb2html = str_ireplace('[dc1]', '<span class="dropcap1">', $bb2html);
+        $bb2html = str_ireplace('[dc2]', '<span class="dropcap2">', $bb2html);
+        $bb2html = str_ireplace('[dc3]', '<span class="dropcap3">', $bb2html);
+        $bb2html = str_ireplace('[dc4]', '<span class="dropcap4">', $bb2html);
+        $bb2html = str_ireplace('[dc5]', '<span class="dropcap5">', $bb2html);
+        $bb2html = str_ireplace('[/dc]', '<dc></span>', $bb2html);
         
         // special characters (html entity encoding) ..
-        $bb2html = str_replace('[sp]', '&nbsp;', $bb2html);
+        $bb2html = str_ireplace('[sp]', '&nbsp;', $bb2html);
         $bb2html = str_replace('[<]', '&lt;', $bb2html);
         $bb2html = str_replace('[>]', '&gt;', $bb2html);
 
