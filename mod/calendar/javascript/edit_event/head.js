a1d402e201d93e5b0c9782af82857029fd67d31e
Matt McNaney <matt@tux.appstate.edu>
2006-09-14 13:45:41 +0000

    to prevent me deleting and rewriting this for the third time

---
+++ mod/calendar/javascript/edit_event/head.js
@@ -0,0 +1,27 @@
+<script type="text/javascript">
+//<![CDATA[
+
+    function changeTab(tab) {
+        repeat_pick = document.getElementById('repeat-pick');
+        event_pick  = document.getElementById('event-pick');
+
+        repeat_tab = document.getElementById('repeat-tab');
+        event_tab  = document.getElementById('event-tab');
+
+        if (tab) {
+            // repeat tab
+            repeat_pick.className = 'active';
+            event_pick.className = 'inactive'; 
+            repeat_tab.style.display = 'block';
+            event_tab.style.display = 'none'; 
+        } else {
+            // event tab
+            repeat_pick.className = 'inactive';
+            event_pick.className = 'active'; 
+            repeat_tab.style.display = 'none';
+            event_tab.style.display = 'block'; 
+        }
+    }
+
+//]]>
+</script>
