c68fbd81948911c86d63febf76e29191721dbbd5
Matt McNaney <matt@tux.appstate.edu>
2006-09-21 17:32:29 +0000

    related was missing key registration

--- mod/related/boost/update.php
+++ mod/related/boost/update.php
@@ -10,6 +10,10 @@ function related_update(&$content, $version)
     switch ($version) {
     case version_compare($version, '0.1.0', '<'):
         $content[] = 'Made links XHTML compatible.';
+
+    case version_compare($version, '0.1.1', '<'):
+        Key::registerModule('related');
+        $content[] = 'Register module to key.';
     }
 
     return TRUE;
