b0d708950a0d7432710250db322f0136e955efa2
Matt McNaney <matt@tux.appstate.edu>
2006-09-21 14:53:13 +0000

    - restored cache on mini calendar printout

--- mod/calendar/inc/runtime.php
+++ mod/calendar/inc/runtime.php
@@ -10,13 +10,13 @@ $mini_cal_display = PHPWS_Settings::get('calendar', 'display_mini');
 if ($mini_cal_display == MINI_CAL_SHOW_ALWAYS ||
     ($mini_cal_display == MINI_CAL_SHOW_FRONT && PHPWS_Core::atHome())) {
     $cal_key = sprintf('cal_%s_%s', date('m'), date('Y'));
-    //    $lil_calendar = PHPWS_Cache::get($cal_key);
+    $lil_calendar = PHPWS_Cache::get($cal_key);
 
     if (empty($lil_calendar)) {
         $Calendar = & new PHPWS_Calendar;
         $Calendar->loadUser();
         $lil_calendar = $Calendar->user->mini_month();
-        //  PHPWS_Cache::save($cal_key, $lil_calendar);
+        PHPWS_Cache::save($cal_key, $lil_calendar);
         unset($Calendar);
     } else {
         if (PHPWS_Settings::get('calendar', 'use_calendar_style')) {
