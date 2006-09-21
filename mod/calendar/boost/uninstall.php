314299b0ff602e3eb0ca3c17e6be5fe85b8556e1
Matt McNaney <matt@tux.appstate.edu>
2006-09-21 17:29:41 +0000

    rewrote uninstallation

--- mod/calendar/boost/uninstall.php
+++ mod/calendar/boost/uninstall.php
@@ -6,9 +6,43 @@
 
 function calendar_uninstall(&$content)
 {
+    PHPWS_Core::initModClass('calendar', 'Schedule.php');
     // Need functions to remove old event tables
-    PHPWS_DB::dropTable('calendar_schedule');
-    $content[] = 'Calendar tables removed.';
+    $db = & new PHPWS_DB('calendar_schedule');
+    $schedules = $db->getObjects('Calendar_Schedule');
+
+    if (PEAR::isError($schedules)) {
+        return $schedules;
+    } elseif (empty($schedules)) {
+        PHPWS_Core::dropTable('calendar_schedule');
+        return true;
+    }
+
+    $error = false;
+
+    foreach ($schedules as $sch) {
+        $result = $sch->delete();
+        if (PEAR::isError($result)) {
+            PHPWS_Error::log($result);
+            $error = true;
+        }
+    }
+
+    $result = PHPWS_DB::dropTable('calendar_schedule');
+    if (PEAR::isError($result)) {
+        return $result;
+    }
+
+    $result = PHPWS_DB::dropTable('calendar_notice');
+    if (PEAR::isError($result)) {
+        return $result;
+    }
+
+    if (!$error) {
+        $content[] = 'Calendar tables removed.';
+    } else {
+        $content[] = 'Some errors occurred when uninstalling Calendar.';
+    }
     return true;
 }
 
