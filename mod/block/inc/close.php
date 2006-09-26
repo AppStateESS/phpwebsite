316dfa05d978a2331b55d8b7f7aa1650347ee770
Matt McNaney <matt@tux.appstate.edu>
2006-09-26 13:09:07 +0000

    changed to display only if javascript is enabled

--- mod/block/inc/close.php
+++ mod/block/inc/close.php
@@ -9,13 +9,12 @@ Block::show();
 
 if (Current_User::allow('block')) {
     $key = Key::getCurrent();
-    if (Key::checkKey($key)) {
+    if (Key::checkKey($key) && javascriptEnabled()) {
         $val['address'] = sprintf('index.php?module=block&action=js_block_edit&key_id=%s&authkey=%s',
                                   $key->id, Current_User::getAuthkey());
         $val['label'] = _('Add block here');
         $val['width'] = 640;
         $val['height'] = 480;
-
         MiniAdmin::add('block', javascript('open_window', $val));
     }
  }
