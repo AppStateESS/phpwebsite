19cdac4dd298158d08a6fa90b467b4e9c85af91d
Matt McNaney <matt@tux.appstate.edu>
2006-09-29 17:09:56 +0000

    Categories now creates its sequence table Added automatic mode and graph to categories Added calendar and documents category conversion Fixed bug in categories causing module name to be missing from sort screen

--- convert/modules/categories/convert.php
+++ convert/modules/categories/convert.php
@@ -52,12 +52,12 @@ function convertCategories()
     $content[] = sprintf('%s&#37; done<br>', $batch->percentDone());
 
     $batch->completeBatch();
-
     
     if (!$batch->isFinished()) {
         $content[] =  $batch->continueLink();
     } else {
         $batch->clear();
+        createSeqTable();
         Convert::addConvert('categories');
         $content[] =  _('Finished converting categories!');
         $content[] = '<a href="index.php?command=convert&amp;package=categories">' . _('Continue to convert category elements.') . '</a>';
@@ -68,46 +68,62 @@ function convertCategories()
 
 function convertItems()
 {
-    $db = Convert::getSourceDB('mod_fatcat_elements');
+    if (!isset($_REQUEST['mode'])) {
+        $content[] = _('You may convert two different ways.');
+        $content[] = sprintf('<a href="%s">%s</a>', 'index.php?command=convert&package=categories&mode=manual',
+                             _('Manual mode requires you to click through the conversion process.'));
+        $content[] = sprintf('<a href="%s">%s</a>', 'index.php?command=convert&package=categories&mode=auto',
+                             _('Automatic mode converts the data without your interaction.'));
 
-    /**
-     * Going to ignore calendar for now. When calendar is finished
-     * I'll put it back in.
-     */
-    $db->addWhere('module_title', 'calendar', '!=');
+        $content[] = ' ';
+        $content[] = _('If you encounter problems, you should use manual mode.');
+        $content[] = _('Conversion will begin as soon as you make your choice.');
 
-    $batch = & new Batches('convert_category_items');
-    $total_items = $db->count();
-
-    $batch->setTotalItems($total_items);
-    $batch->setBatchSet(30);
-
-    if (isset($_REQUEST['reset_batch'])) {
-        $batch->clear();
-    }
-
-    if (!$batch->load()) {
-        $content[] = 'Batch previously run.';
+        return implode('<br />', $content);
     } else {
-        runCatItemBatch($db, $batch);
-    }
+        if ($_REQUEST['mode'] == 'auto') {
+            $show_wait = TRUE;
+        } else {
+            $show_wait = FALSE;
+        }
 
-    $content[] = sprintf('%s&#37; done<br>', $batch->percentDone());
+        $db = Convert::getSourceDB('mod_fatcat_elements');
 
-    $batch->completeBatch();
+        $batch = & new Batches('convert_category_items');
+        $total_items = $db->count();
 
+        $batch->setTotalItems($total_items);
+        $batch->setBatchSet(30);
+
+        if (isset($_REQUEST['reset_batch'])) {
+            $batch->clear();
+        }
+
+        if (!$batch->load()) {
+            $content[] = 'Batch previously run.';
+        } else {
+            runCatItemBatch($db, $batch);
+        }
+
+        $percent = $batch->percentDone();
+        $content[] = Convert::getGraph($percent, $show_wait);
+        $batch->completeBatch();
     
-    if (!$batch->isFinished()) {
-        $content[] =  $batch->continueLink();
-    } else {
-        $batch->clear();
-        Convert::addConvert('categories');
-        $content[] =  _('Finished converting category items!');
-        $content[] = '<a href="index.php">' . _('Return to the main page.') . '</a>';
-    }
+        if (!$batch->isFinished()) {
+            if ($_REQUEST['mode'] == 'manual') {
+                $content[] =  $batch->continueLink();                
+            } else {
+                Convert::forward($batch->getAddress());
+            }
+        } else {
+            $batch->clear();
+            Convert::addConvert('categories_items');
+            $content[] =  _('Finished converting category items!');
+            $content[] = '<a href="index.php">' . _('Return to the main page.') . '</a>';
+        }
     
-    return implode('<br />', $content);
-
+        return implode('<br />', $content);
+    }
 }
 
 function runCatBatch(&$db, &$batch)
@@ -160,7 +176,8 @@ function runCatItemBatch(&$db, &$batch)
                 break;
 
             case 'calendar':
-
+                $module = 'calendar';
+                $item_name = 'event';
                 break;
 
             case 'photoalbum':
@@ -173,6 +190,8 @@ function runCatItemBatch(&$db, &$batch)
                 break;
 
             case 'documents':
+                $module = 'filecabinet';
+                $item_name = 'document';
                 break;
 
             case 'phatform':
@@ -195,6 +214,7 @@ function runCatItemBatch(&$db, &$batch)
             } else {
                 $item_db->addValue('key_id', $key_id);
                 $item_db->addValue('cat_id', $item['cat_id']);
+                $item_db->addValue('module', $module);
                 $item_db->insert();
                 $item_db->reset();
             }
@@ -202,5 +222,14 @@ function runCatItemBatch(&$db, &$batch)
     }
 
 }
+
+function createSeqTable()
+{
+    $db = new PHPWS_DB('categories');
+    return $db->updateSequenceTable();
+}
+
+
+
 
 ?>
