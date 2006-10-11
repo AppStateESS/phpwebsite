1d80fed55ff93bfad24372d7d85934088968fa7a
Matt McNaney <matt@tux.appstate.edu>
2006-10-11 19:04:31 +0000

    Fixed default setting (Bug #1573480).

--- mod/version/inc/settings.php
+++ mod/version/inc/settings.php
@@ -1,7 +1,12 @@
 <?php
 
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
   // saved_version : the number of version saved until the oldest is deleted
   //                 <= 0 don't delete versions
-$settings = array('saved_versions' => 8);
+$settings = array('saved_versions' => 10);
 
 ?>
