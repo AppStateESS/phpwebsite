0df996d9d0dceb10bfa93b2d6d9164cecdf5c058
Matt McNaney <matt@tux.appstate.edu>
2006-10-31 20:28:03 +0000

    changed default settings

--- mod/blog/inc/settings.php
+++ mod/blog/inc/settings.php
@@ -6,8 +6,8 @@
    */
 
 $settings['allow_comments'] = 1;
-$settings['blog_limit'] = 10;
-$settings['past_entries'] = 10;
-$settings['home_page_display'] = 0;
+$settings['blog_limit'] = 5;
+$settings['past_entries'] = 5;
+$settings['home_page_display'] = 1;
 
 ?>
