52e37444c200551411809f4ae0ae2de7fb5f69d6
Matt McNaney <matt@tux.appstate.edu>
2006-11-02 18:54:07 +0000

    commented

--- mod/users/inc/settings.php
+++ mod/users/inc/settings.php
@@ -1,4 +1,8 @@
 <?php
+  /**
+   * @version $Id$
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   */
 $settings = array('default_authorization' => 1,
                   'new_user_method' => 2,
                   'user_menu' => 'top.tpl',
