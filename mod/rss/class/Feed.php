20633c4496726c361e0a89c502c138c29f76a7b1
Matt McNaney <matt@tux.appstate.edu>
2006-11-03 21:35:50 +0000

    commented

--- mod/rss/class/Feed.php
+++ mod/rss/class/Feed.php
@@ -1,5 +1,10 @@
 <?php
 
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
 PHPWS_Core::initModClass('rss', 'XMLParser.php');
 
 PHPWS_Core::requireConfig('rss');
