5f3d43870a4d346befdb47d28c0631f6302b7405
Matt McNaney <matt@tux.appstate.edu>
2006-11-06 21:41:16 +0000

    changed comment slightly

--- mod/notes/conf/config.php
+++ mod/notes/conf/config.php
@@ -9,8 +9,11 @@
 define('NOTE_CHECK_INTERVAL', 5);
 
 
-// Set to allow the searching of users when sending notes
-// turning it off means the user must enter the username exactly
+/**
+ * Set to allow the searching of users when sending notes
+ * turning it off means the user must enter the username (actually searches
+ * on the display name) exactly
+ **/
 define('NOTE_ALLOW_USERNAME_SEARCH', true);
 
 ?>
