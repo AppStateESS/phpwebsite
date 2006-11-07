3d9132c7cd072513d294118ab2bd6a3895fb75ff
Matt McNaney <matt@tux.appstate.edu>
2006-11-07 20:30:34 +0000

    upped

--- mod/comments/boost/boost.php
+++ mod/comments/boost/boost.php
@@ -4,7 +4,7 @@
    * @version $Id$
    */
 $proper_name  = 'Comments';
-$version      = '0.2.9';
+$version      = '0.3.0';
 $unregister   = TRUE;
 $import_sql   = TRUE;
 $image_dir    = TRUE;
--- mod/comments/boost/update.php
+++ mod/comments/boost/update.php
@@ -37,59 +37,14 @@ function comments_update(&$content, $currentVersion)
 
     case version_compare($currentVersion, '0.2.9', '<'):
         $content[] = 'Fix - Relative time listing.';
+
+    case version_compare($currentVersion, '0.3.0', '<'):
+        $content[] = 'Small changes to sync comments to current core.';
     }
             
     return TRUE;
 }
 
-
-function comments_update_002(&$content)
-{
-    PHPWS_Core::initModClass('controlpanel', 'ControlPanel.php');
-    if (!@mkdir('images/mod/comments')) {
-        $content[] = 'Unable to create image directory.';
-        return FALSE;
-    }
-    return PHPWS_ControlPanel::registerModule('comments', $content);
-}
-
-function comments_update_003(&$content) {
-    $content[] = 'Update control panel link.';
-    $db = & new PHPWS_DB('controlpanel_link');
-    $db->addWhere('itemname', 'comments');
-    $db->addValue('url', 'index.php?module=comments&admin_action=admin_menu');
-    return $db->update();
-}
-
-function comments_update_010(&$content)
-{
-    $db = & new PHPWS_DB('comments_users');
-    
-    $result = $db->dropTableColumn('signature');
-    if (PEAR::isError($result)) {
-        PHPWS_Error::log($result);
-    }
-    $result = $db->dropTableColumn('picture');
-    if (PEAR::isError($result)) {
-        PHPWS_Error::log($result);
-    }
-
-    $result = $db->dropTableColumn('contact_email');
-    if (PEAR::isError($result)) {
-        PHPWS_Error::log($result);
-    }
-
-    $result = $db->dropTableColumn('website');
-    if (PEAR::isError($result)) {
-        PHPWS_Error::log($result);
-    }
-
-    $result = $db->dropTableColumn('location');
-    if (PEAR::isError($result)) {
-        PHPWS_Error::log($result);
-    }
-    return TRUE;
-}
 
 function comments_update_020(&$content) {
     $db = & new PHPWS_DB('comments_threads');
