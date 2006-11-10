d04b38014084fb0831ca4ca7818bc1b56f107d8e
Matt McNaney <matt@tux.appstate.edu>
2006-11-10 18:11:59 +0000

    upped

--- mod/calendar/boost/boost.php
+++ mod/calendar/boost/boost.php
@@ -6,7 +6,7 @@
    */
 
 $proper_name  = 'Calendar';
-$version      = '1.1.0';
+$version      = '1.2.0';
 $version_http = 'http://phpwebsite.appstate.edu/downloads/modules/calendar/check.xml';
 $image_dir    = true;
 $import_sql   = true;
--- mod/calendar/boost/update.php
+++ mod/calendar/boost/update.php
@@ -20,6 +20,19 @@ function calendar_update(&$content, $version)
         }
         $content[] = 'New - event displays as Busy to the public if set as such.';
         $content[] = 'New - Settings tab returns with a few basic settings.';
+
+    case version_compare($version, '1.2.0', '<'):
+        $files = array('templates/admin/forms/settings.tpl');
+        if (PHPWS_Boost::updateFiles($files, 'calendar')) {
+            $content[] = 'Template files updated.';
+        } else {
+            $content[] = 'Failed to copy template files.';
+        }
+        
+        $content[] = '- Opened up private calendar key posting to allow permission settings.';
+        $content[] = '- Added admin option to change the default calendar view.';
+        $content[] = '- Month link on mini calendar now opens the default view.';
+        $content[] = '- Public calendars that are restricted are now properly hidden.';
     }
 
     return true;
