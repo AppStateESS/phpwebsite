730478d8709f042525876f219d3fdefa87749c4b
Matt McNaney <matt@tux.appstate.edu>
2006-11-10 15:05:11 +0000

    update file for version 220

---
+++ mod/layout/boost/update_220.sql
@@ -0,0 +1,8 @@
+CREATE TABLE layout_metatags (
+key_id INT NOT NULL ,
+page_title VARCHAR( 255 ) NOT NULL ,
+meta_description TEXT NULL ,
+meta_keywords TEXT NULL ,
+meta_robots CHAR( 2 ) NOT NULL DEFAULT '11',
+PRIMARY KEY ( key_id )
+);
