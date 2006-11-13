8b7af3580a9a0428bb4547ccadb22e8ede3ecdbc
Matt McNaney <matt@tux.appstate.edu>
2006-11-13 21:34:53 +0000

    New class to help with CAPTCHA

---
+++ core/class/Captcha.php
@@ -0,0 +1,57 @@
+<?php
+  /**
+   * Small class that assists in loading CAPTCHA routines
+   * 
+   * @version $Id$
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   */
+
+if (!defined('CAPTCHA_NAME')) {
+    define('CAPTCHA_NAME', '');
+ }
+
+class Captcha {
+
+    function get()
+    {
+        if (!Captcha::isGD()) {
+            return null;
+        }
+
+        $dirname = 'captcha/' . CAPTCHA_NAME . '/';
+
+        if (is_dir('javascript/' . $dirname)) {
+            return javascript($dirname);
+        } else {
+            return null;
+        }
+    }
+
+    function verify($answer)
+    {
+        if (!Captcha::isGD()) {
+            return true;
+        }
+
+        $file = 'javascript/captcha/' . CAPTCHA_NAME . '/verify.php';
+
+        if (!is_file($file)) {
+            return true;
+        }
+
+        include $file;
+        
+        if (!function_exists('verify')) {
+            return true;
+        }
+
+        return verify($answer);
+    }
+
+    function isGD()
+    {
+        return extension_loaded('gd');
+    }
+
+}
+?>
