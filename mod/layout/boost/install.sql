db5c1541ab9437bdc0e6341abf474b64e7733a35
Matt McNaney <matt@tux.appstate.edu>
2006-11-17 14:54:10 +0000

    added missing table to install.sql added index to metatags table

--- mod/layout/boost/install.sql
+++ mod/layout/boost/install.sql
@@ -33,3 +33,14 @@ style varchar( 40 ) NOT NULL
 );
 
 CREATE INDEX key_id ON layout_styles(key_id);
+
+CREATE TABLE layout_metatags (
+key_id INT NOT NULL ,
+page_title VARCHAR( 255 ) NOT NULL ,
+meta_description TEXT NULL ,
+meta_keywords TEXT NULL ,
+meta_robots CHAR( 2 ) NOT NULL DEFAULT '11',
+PRIMARY KEY ( key_id )
+);
+
+CREATE INDEX key_id ON layout_metatags(key_id);
