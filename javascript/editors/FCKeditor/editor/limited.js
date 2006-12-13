c3ae951c5f87e9da835d0e6351e3352d53054c73
Matt McNaney <matt@tux.appstate.edu>
2006-12-13 19:23:40 +0000

    limited configuration file

---
+++ javascript/editors/FCKeditor/editor/limited.js
@@ -0,0 +1,13 @@
+// This is the "limited" version. It may be used for untrusted users
+
+// Form line removed because this is not the place for it
+// Removed 'FontFormat','FontName','FontSize', 'TextColor', and 'BGColor'
+// because they lead to garish markup.
+
+
+FCKConfig.ToolbarSets["phpws"] = [
+	['Bold','Italic','Underline','StrikeThrough']
+] ;
+
+
+FCKConfig.ProcessHTMLEntities	= false ;
