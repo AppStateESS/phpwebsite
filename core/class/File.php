c37ab8087c5e4914908087a5dfe7c0b465e928c0
Matt McNaney <matt@tux.appstate.edu>
2007-01-09 16:58:43 +0000

    Fixed bug preventing files in directories from being removed.

--- core/class/File.php
+++ core/class/File.php
@@ -429,6 +429,10 @@ class PHPWS_File {
 
     function rmdir($dir) 
     {
+        if (!preg_match('/\/$/', $dir)) {
+            $dir .= '/';
+        }
+
         if(is_dir($dir)) {
             $handle = opendir($dir);
             while($file = readdir($handle)) {
@@ -445,6 +449,7 @@ class PHPWS_File {
                 }
             }
             closedir($handle);
+
             $result = @rmdir($dir);
             if (!$result) {
                 PHPWS_Error::log(PHPWS_DIR_DELETE_DENIED, 'core', 'PHPWS_File::rmdir', $dir);
