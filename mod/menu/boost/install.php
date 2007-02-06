5b57b3fac7678313b7ea144b1db0d6bcf1c63f6e
Matt McNaney <matt@tux.appstate.edu>
2007-02-06 14:55:09 +0000

    default menu is created on install

---
+++ mod/menu/boost/install.php
@@ -0,0 +1,26 @@
+<?php
+
+  /**
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   * @version $Id$
+   */
+
+function menu_install(&$content)
+{
+    PHPWS_Core::initModClass('menu', 'Menu_Item.php');
+    $menu = new Menu_Item;
+    $menu->title = _('Main menu');
+    $menu->template = 'basic.tpl';
+    $menu->pin_all = 1;
+    $result = $menu->save();
+    if (PEAR::isError($result)) {
+        PHPWS_Error::log($result);
+        return false;
+    } else {
+        $content[] = _('Default menu created successfully.');
+        return true;
+    }
+
+}
+
+?>
