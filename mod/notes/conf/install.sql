e62b4b7a5b30f890c0a03f6306e83cb13a5b0cc9
Matt McNaney <matt@tux.appstate.edu>
2007-02-19 19:51:08 +0000

    not needed

--- mod/notes/conf/install.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE notes (
-  id int NOT NULL default 0,
-  user_id int NOT NULL default 0,
-  check_key varchar(32) NOT NULL default '',
-  title varchar(60) NOT NULL default '',
-  content text,
-  PRIMARY KEY  (id)
-);
