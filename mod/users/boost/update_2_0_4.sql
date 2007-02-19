afade9810648940f5ec2ea946e4cceb0cd7d0093
Matt McNaney <matt@tux.appstate.edu>
2007-02-19 16:25:15 +0000

    not used anymore

--- mod/users/boost/update_2_0_4.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users_signup (
-  authkey char(32) NOT NULL default '',
-  user_id int NOT NULL default '0',
-  deadline int NOT NULL default '0'
-);
-
-CREATE INDEX userssignup_idx on users_signup (authkey);
