040914e84d6d40021ae48694906a16ba321bb618
Matt McNaney <matt@tux.appstate.edu>
2007-02-21 19:02:21 +0000

    added comments and changed english file name

--- core/conf/i18n/de_DE.php
+++ core/conf/i18n/de_DE.php
@@ -1,12 +1,17 @@
 <?php
 
-define("PHPWS_DATE_ORDER",        "dmy");
-define("PHPWS_TIME_FORMAT",       "h:i");
-define("PREFERRED_MONTH_DISPLAY", "%B");
-define("PREFERRED_DAY_DISPLAY",   "%d");
-define("PREFERRED_YEAR_DISPLAY",  "%Y");
-define("PHPWS_DATE_FORMAT",       "d/m/Y");
-define("PHPWS_DAY_START",         0); // 0 = Sunday, 1 = Monday
-define("PHPWS_TIME_DIFF",         0);
+  /**
+   * @version $Id$
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   */
+
+define('PHPWS_DATE_ORDER',        'dmy');
+define('PHPWS_TIME_FORMAT',       'h:i');
+define('PREFERRED_MONTH_DISPLAY', '%B');
+define('PREFERRED_DAY_DISPLAY',   '%d');
+define('PREFERRED_YEAR_DISPLAY',  '%Y');
+define('PHPWS_DATE_FORMAT',       'd/m/Y');
+define('PHPWS_DAY_START',         0); // 0 = Sunday, 1 = Monday
+define('PHPWS_TIME_DIFF',         0);
 
 ?>
