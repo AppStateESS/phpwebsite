64b4db55ebb044b26f2cdcdfb4e83cae3e8ea19a
Matt McNaney <matt@tux.appstate.edu>
2007-02-22 20:26:57 +0000

    + Put file directory option back into boost.php file.

--- mod/access/boost/boost.php
+++ mod/access/boost/boost.php
@@ -9,6 +9,7 @@ $version      = '0.1.2';
 $import_sql   = TRUE;
 $version_http = 'http://phpwebsite.appstate.edu/downloads/modules/access/check.xml';
 $about        = TRUE;
+$file_dir     = TRUE; // required for backup access files
 $priority     = 50;
 $dependency   = FALSE;
 ?>
