20d02e4fa80a4737a4433c12b4e820c42ef0f21d
Matt McNaney <matt@tux.appstate.edu>
2007-02-26 19:18:55 +0000

    not used

--- mod/boost/boost/update_1_7_0.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE dependencies (
-  source_mod varchar(40) NOT NULL default '',
-  depended_on varchar(40) NOT NULL default '',
-  version varchar(20) NOT NULL default ''
-);
--- mod/boost/boost/update_1_7_1.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE phpws_key_register (
-  module varchar(40) NOT NULL default ''
-);
-
-CREATE TABLE converted (
-  convert_name varchar(30) NOT NULL default ''
-);
