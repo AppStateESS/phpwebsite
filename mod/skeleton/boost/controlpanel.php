502184621fbd09fab111b8cd83b0bd8bdb3524fc
Matt McNaney <matt@tux.appstate.edu>
2007-03-05 17:01:26 +0000

    changed control panel icon

--- mod/skeleton/boost/controlpanel.php
+++ mod/skeleton/boost/controlpanel.php
@@ -1,9 +1,14 @@
 <?php
+  /**
+   * @version $Id$
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   */
+
 $link[] = array('label'       => _('Skeleton'),
 		'restricted'  => TRUE,
 		'url'         => 'index.php?module=skeleton',
 		'description' => _('A framework for creating modules.'),
-		'image'       => 'skeleton.jpg',
+		'image'       => 'skeleton.png',
 		'tab'         => 'content'
 		);
 
