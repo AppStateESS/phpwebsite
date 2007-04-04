2f0fde70d764be6ea52e936d93ec25b375cdbdbc
Matt McNaney <matt@tux.appstate.edu>
2007-04-04 13:11:57 +0000

    missing js files

---
+++ mod/filecabinet/javascript/folder_contents/head.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+
+     function show_images(image_html, folder_id)
+{
+    var button = document.getElementById('image-button');
+
+    if (!image_html) {
+        images.innerHTML = '{error_message}';
+        button.style.visibility = 'hidden';
+    } else {
+        button.style.visibility = 'visible';
+        button_update = button.innerHTML.replace(/folder_id=\d*&/gi, 'folder_id='+folder_id+'&');
+        button.innerHTML = button_update;
+        images = document.getElementById('images');
+        images.innerHTML = image_html;
+    }
+}
+
+</script>
---
+++ mod/filecabinet/javascript/refresh_manager/head.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+//<![CDATA[
+new_loc = window.opener.location.href.replace(/current=\d+/gi, 'current={image_id}');
+window.opener.location.href = new_loc;
+window.close();
+//]]>
+</script>
