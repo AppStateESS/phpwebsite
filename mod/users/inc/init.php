273c211e963f1d176cf2d4d7ec4d219505c59b36
Matt McNaney <matt@tux.appstate.edu>
2007-04-11 20:32:38 +0000

    + Removed redundant class calls in init.php

--- mod/users/inc/init.php
+++ mod/users/inc/init.php
@@ -5,11 +5,8 @@
    * @version $Id$
    */
 
-
 PHPWS_Core::configRequireOnce('users', 'config.php', TRUE);
 require_once PHPWS_SOURCE_DIR . 'mod/users/inc/errorDefines.php';
 PHPWS_Core::configRequireOnce('users', 'tags.php');
-
-PHPWS_Core::initModClass('users', 'Users.php');
 PHPWS_Core::initModClass('users', 'Current_User.php');
 ?>
