26fb313a6c927211c96105f50b7483bd4df01a02
Matt McNaney <matt@tux.appstate.edu>
2007-04-11 13:15:55 +0000

    commented

--- mod/branch/inc/init.php
+++ mod/branch/inc/init.php
@@ -1,5 +1,8 @@
 <?php
-
+  /**
+   * @version $Id$
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   */
 if (isset($_REQUEST['module']) && $_REQUEST['module'] == 'branch') {
     PHPWS_Core::initModClass('boost', 'Boost.php');
  }
