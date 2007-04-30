67516797ca8c3fb3db228af0e77e39465305ac0f
Matt McNaney <matt@tux.appstate.edu>
2007-04-30 13:35:40 +0000

    fixed default language define

--- setup/preconfig.php
+++ setup/preconfig.php
@@ -1,5 +1,10 @@
 <?php
 
+  /**
+   * @version $Id$
+   * @author Matthew McNaney <mcnaney at gmail dot com>
+   */
+
 if (isset($_SERVER['WINDIR']) || preg_match('/(microsoft|win32)/i', $_SERVER['SERVER_SOFTWARE'])){
   ini_set('include_path', '.;.\\lib\\pear\\');
 }
@@ -14,6 +19,6 @@ define('LOG_PERMISSION', 0644);
 define('LOG_TIME_FORMAT', '%X %x');
 define('PHPWS_LOG_ERRORS', TRUE);
 define('FORCE_MOD_CONFIG', TRUE);
-define('DEFAULT_LANGUAGE', 'en');
+define('DEFAULT_LANGUAGE', 'en_US'); //use same format (e.g. de_DE, es_ES, etc.)
 define('USE_CRUTCH_FILES', FALSE);
 ?>
