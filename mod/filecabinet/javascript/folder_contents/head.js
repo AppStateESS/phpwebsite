d55af1acfaddc6d0b2cb5b5a1a445c2891a0a3a0
Matt McNaney <matt@tux.appstate.edu>
2007-05-02 15:38:13 +0000

    Had to change variable name to prevent IE error

--- mod/filecabinet/javascript/folder_contents/head.js
+++ mod/filecabinet/javascript/folder_contents/head.js
@@ -1,6 +1,6 @@
 <script type="text/javascript">
 
-     function show_images(image_html, folder_id)
+function show_images(image_html, folder_id)
 {
     var button = document.getElementById('image-button');
 
@@ -11,8 +11,8 @@
         button.style.visibility = 'visible';
         button_update = button.innerHTML.replace(/folder_id=\d*&/gi, 'folder_id='+folder_id+'&');
         button.innerHTML = button_update;
-        images = document.getElementById('images');
-        images.innerHTML = image_html;
+        image_list = document.getElementById('images');
+        image_list.innerHTML = image_html;
     }
 }
 
