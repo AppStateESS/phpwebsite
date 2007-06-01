1819659c6bf8dc01a1d0b3cc2f5fed6a0f84d1e6
Matt McNaney <matt@tux.appstate.edu>
2007-06-01 16:31:41 +0000

    fixed from last update

--- mod/boost/class/Boost.php
+++ mod/boost/class/Boost.php
@@ -1171,23 +1171,6 @@ class PHPWS_Boost {
         return $dir_mods;
     }
 
-    function oldModList()
-    {
-        if (!isset($GLOBALS['Boost_Old_Mods'])) {
-            return null;
-        }
-
-        $old_mods = & $GLOBALS['Boost_Old_Mods'];
-
-        $content[] = dgettext('boost', 'The following modules will not function in phpWebSite 1.x.');
-        foreach ($old_mods as $mod) {
-            include sprintf('%smod/%s/conf/boost.php', PHPWS_SOURCE_DIR, $mod);
-            $content[] = $mod_pname;
-        }
-
-        return implode('<br />', $content);
-    }
-
 }
 
 ?>
