638fd21d1d66f8ffc979eb60d4bfc49b2e8d8b11
Matt McNaney <matt@tux.appstate.edu>
2007-06-06 13:14:38 +0000

    suppressing error

--- mod/users/inc/runtime.php
+++ mod/users/inc/runtime.php
@@ -9,7 +9,7 @@ if (!class_exists('PHPWS_User')) {
     exit();
  }
 
-if (@$_REQUEST['module'] == 'users' && $_REQUEST['action'] == 'reset') {
+if (@$_REQUEST['module'] == 'users' && @$_REQUEST['action'] == 'reset') {
     $_SESSION['User'] = new PHPWS_User;
  } else if (!isset($_SESSION['User'])) {
     $_SESSION['User'] = new PHPWS_User;
