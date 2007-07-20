c99762bf2d6e0ab5113f107354445943975989ad
Matt McNaney <matt@tux.appstate.edu>
2007-07-20 15:40:41 +0000

    committing fix to clear link

--- mod/filecabinet/javascript/clear_image/head.js
+++ mod/filecabinet/javascript/clear_image/head.js
@@ -2,13 +2,13 @@
 //<![CDATA[
 
     function clear_image(itemname) {
-        image = document.getElementById(itemname + '-current-image').childNodes[0].firstChild;
+        image = document.getElementById('image-manager-' + itemname);
         hidden = document.getElementById(itemname + '_hidden_value');
 
         if (image) {
             image.src = '{src}';
-            image.width = 100;
-            image.height = 100;
+            image.width = {width};
+            image.height = {height};
             image.title = '{title}';
             image.alt   = '{alt}';
         }
@@ -16,7 +16,6 @@
         if (hidden) {
             hidden.setAttribute('value', '0');
         }
-
     }
 
 //]]>
