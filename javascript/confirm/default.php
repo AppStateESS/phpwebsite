363ef11e871d7c6fa6a079ff4c95568c044c9748
Matt McNaney <matt@tux.appstate.edu>
2007-10-30 15:10:02 +0000

    Changed to copy lowercase variable names over.

---
+++ javascript/confirm/default.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($data['question'])) {
+    $data['QUESTION'] = $data['question'];
+}
+
+if (isset($data['link'])) {
+    $data['LINK'] = $data['link'];
+}
+
+if (isset($data['address'])) {
+    $data['ADDRESS'] = $data['address'];
+}
+
+?>
