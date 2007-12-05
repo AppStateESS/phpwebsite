75fcae00bfcbe374c85b5f6a5c3521887773419d
Matt McNaney <matt@tux.appstate.edu>
2007-12-05 13:47:54 +0000

    ready for update 1.4.3

--- mod/menu/boost/update.php
+++ mod/menu/boost/update.php
@@ -118,6 +118,15 @@ Please download 1.2.1.</pre>';
                         
         $content[] = '</pre>';
 
+    case version_compare($currentVersion, '1.4.3', '<'):
+        $content[] = '<pre>';
+        $files = array('templates/admin/settings.tpl');
+        menuUpdateFiles($files, $content);
+        if (!PHPWS_Boost::inBranch()) {
+            $content[] = file_get_contents(PHPWS_SOURCE_DIR . 'mod/menu/boost/changes/1_4_3.txt');
+        }
+        $content[] = '</pre>';
+
     }
     return true;
 }
