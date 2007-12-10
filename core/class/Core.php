1693a08b4377aef21f20ea019113dfdf3af02314
Matt McNaney <matt@tux.appstate.edu>
2007-12-10 18:50:16 +0000

    Core - small change to getHomeHttp to try and fix IIS error.

--- core/class/Core.php
+++ core/class/Core.php
@@ -696,10 +696,11 @@ class PHPWS_Core {
         $address[] = $_SERVER['HTTP_HOST'];
 
         if ($with_directory) {
-            $address[] = dirname($_SERVER['PHP_SELF']);
+            $address = dirname($_SERVER['PHP_SELF']);
         }
 
         $url = implode('', $address);
+        $url = preg_replace('@\\\@', '/', $url);
 
         if ($with_slash && !preg_match('/\/$/', $url)) {
             $url .= '/';
