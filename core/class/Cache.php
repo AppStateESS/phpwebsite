ea8b0ce8464a8c43dc49da53b8db3a14a15ad2d6
Matt McNaney <matt@tux.appstate.edu>
2008-01-03 15:32:29 +0000

    + Cache - added lifetime to get function. Thanks Verdon. + Updated Cache_Lite documentation. + Updated CREDITS file adding a little more info.

--- core/class/Cache.php
+++ core/class/Cache.php
@@ -9,35 +9,45 @@ require_once 'Cache/Lite.php';
 
 class PHPWS_Cache {
 
-    function &initCache($lifetime=CACHE_LIFETIME){
+    function initCache($lifetime=CACHE_LIFETIME)
+    {
         $options = array(
                          'cacheDir' => CACHE_DIRECTORY,
                          'lifeTime' => (int)$lifetime
                          );
-
         $cache = new Cache_Lite($options);
 
         return $cache;
     }
 
-    function isEnabled(){
-        if (defined('ALLOW_CACHE_LITE'))
+    function isEnabled()
+    {
+        if (defined('ALLOW_CACHE_LITE')) {
             return ALLOW_CACHE_LITE;
-        else
+        } else {
             return FALSE;
+        }
     }
 
-    function get($key){
+    /**
+     * Saves the cache content
+     * @param string key      Name of cache key.
+     * @param int    lifetime Seconds to retain cache
+     * @returns boolean TRUE on success, FALSE otherwise
+     */
+    function get($key, $lifetime=CACHE_LIFETIME)
+    {
         if (!PHPWS_Cache::isEnabled()) {
             return;
         }
 
-        $cache = & PHPWS_Cache::initCache();
+        $cache = PHPWS_Cache::initCache($lifetime);
         $key .= SITE_HASH . CURRENT_LANGUAGE;
         return $cache->get(md5($key));
     }
 
-    function writeIni($switch=0){
+    function writeIni($switch=0)
+    {
         PHPWS_Core::initCoreClass('File.php');
         $info = "cache = $switch\n";
         return PHPWS_File::writeFile(CACHE_DIRECTORY . 'phpws_cache.ini', $info, TRUE);
@@ -46,7 +56,7 @@ class PHPWS_Cache {
     function remove($key)
     {
         $key .= SITE_HASH . CURRENT_LANGUAGE;
-        $cache = & PHPWS_Cache::initCache();
+        $cache = PHPWS_Cache::initCache();
         return $cache->remove(md5($key));
     }
 
@@ -56,7 +66,15 @@ class PHPWS_Cache {
         $cache->clean();
     }
 
-    function save($key, $content, $lifetime=CACHE_LIFETIME){
+    /**
+     * Saves the cache content
+     * @param string key     Name of cache key.
+     * @param string content Content stored in the cache
+     * @param int    ignore  Used to be lifetime but not used anymore.
+     * @returns boolean TRUE on success, FALSE otherwise
+     */
+    function save($key, $content, $ignore=null)
+    {
         $key .= SITE_HASH . CURRENT_LANGUAGE;
 
         if (!PHPWS_Cache::isEnabled()) {
@@ -66,7 +84,7 @@ class PHPWS_Cache {
         if (!is_string($content)) {
             return PHPWS_Error::get(PHPWS_VAR_TYPE, 'core', __CLASS__ . '::' .__FUNCTION__);
         }
-        $cache = & PHPWS_Cache::initCache($lifetime);
+        $cache = PHPWS_Cache::initCache();
         return $cache->save($content, md5($key));
     }
 
