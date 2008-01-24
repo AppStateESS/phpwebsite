d97a69801ec8b8777689194c346900bd91eb450c
Matt McNaney <matt@tux.appstate.edu>
2008-01-24 20:31:17 +0000

    not used

--- mod/filecabinet/javascript/folder_contents/head.js
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript">
-    var error_message = '{error_message}';
-</script>
-<script type="text/javascript" src="./javascript/modules/filecabinet/folder_contents/scripts.js"></script>
