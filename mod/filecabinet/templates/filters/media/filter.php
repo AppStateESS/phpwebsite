b3f0050e588df497e524c1c5bc85811cc6652a2d
Matt McNaney <matt@tux.appstate.edu>
2008-01-25 14:00:00 +0000

    removing flash and mp3 directories. media replaces both

---
+++ mod/filecabinet/templates/filters/media/filter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version $Id$
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ */
+
+$floatbar       = false;
+$use_thumbnails = false;
+
+if ($is_video) {
+    if ($floatbar) {
+        $tpl['DISPLAYHEIGHT'] = $tpl['HEIGHT'];
+    } else {
+        $tpl['HEIGHT'] = $tpl['HEIGHT'] + 20;
+    }
+
+    if ($use_thumbnails) {
+        $tpl['THUMBNAIL'] = $thumbnail;
+    }
+} else {
+    $tpl['HEIGHT'] = 20;
+    $tpl['WIDTH'] = 260;
+}
+
+
+
+
+?>
