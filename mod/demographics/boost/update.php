3b47b71a7ef18f47c3e12772bb56278d3375a552
Matt McNaney <matt@tux.appstate.edu>
2008-03-12 14:40:54 +0000

    preparing update scripts

--- mod/demographics/boost/update.php
+++ mod/demographics/boost/update.php
@@ -24,6 +24,23 @@ function demographics_update(&$content, $version)
 + Implemented patch 1773655 from Eloi George. Fixes column type identification.</pre>
 ';
 
+    case version_compare($version, '1.1.0', '<'):
+        $db = new PHPWS_DB('demographics');
+        if (PHPWS_Error::logIfError($db->createTableIndex('user_id', null, true))) {
+            $content[] = 'Warning: A problems occurred when trying to create a unique index on the demographics table.';
+        }
+
+        $content[] = '<pre>1.1.0 changes
+--------------
++ Made user_id column unique
++ Added _base_id and _extend_id. Allows demographics and extended
+  object to work independently of one another.
++ Added parameter to delete function to prevent deletion of all user
+  information.
++ Demographics loads itself above the extended class.
+</pre>
+';
+
     }
     return true;
 }
