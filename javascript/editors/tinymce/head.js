839d68796fa396d2476215ba5c4a2a3565e9d06b
Matt McNaney <matt@tux.appstate.edu>
2008-03-14 15:14:33 +0000

    + Updated tinymce head file for 3.0+ version

--- javascript/editors/tinymce/head.js
+++ javascript/editors/tinymce/head.js
@@ -3,9 +3,12 @@
     tinyMCE.init({
         mode : "specific_textareas",
         strict_loading_mode : true,
+        theme : 'advanced',
+        
         textarea_trigger : "mceEditor",
-            plugins : "table,advimage,print,preview,searchreplace",
-            theme_advanced_buttons2 : "bullist,numlist,outdent,indent,separator,undo,redo,link,unlink,anchor,image,cleanup,preview",
-            theme_advanced_buttons3 : "table,search,replace,separator,code hr,removeformat,visualaid,sub,sup,charmap,print,help"
+        plugins : "safari,table,advimage,print,preview,searchreplace,spellchecker,fullscreen,paste",
+        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontsizeselect",
+        theme_advanced_buttons2 : "undo,redo,|,table,bullist,numlist,outdent,indent,link,unlink,anchor,image,cleanup,preview",
+        theme_advanced_buttons3 : "code hr,removeformat,visualaid,sub,sup,charmap,|,cut,copy,paste,pastetext,pasteword,|,print,fullscreen,spellchecker,search,replace"
 });
 </script>
