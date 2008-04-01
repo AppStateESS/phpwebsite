f2d993f0e5ea0c9b4057015566a33807c40a6177
Matt McNaney <matt@tux.appstate.edu>
2008-04-01 15:24:35 +0000

    fixed rewrite bug

--- mod/menu/class/Menu_Link.php
+++ mod/menu/class/Menu_Link.php
@@ -196,8 +196,8 @@ class Menu_Link {
             $redirect_url = preg_quote(PHPWS_Core::getCurrentUrl());
         }
 
-        if ( preg_match("/$current_url$/", $this->url) ||
-             preg_match("/$redirect_url$/", $this->url) ) {
+        if ( preg_match("@$current_url$@", $this->url) ||
+             preg_match("@$redirect_url$@", $this->url) ) {
             return true;
         } else {
             return false;
