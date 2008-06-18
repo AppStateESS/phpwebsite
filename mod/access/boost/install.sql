1f02e9dd29705e6fe3e1b31cfc5f491a3c63f421
Matt McNaney <matt@tux.appstate.edu>
2008-06-18 17:22:51 +0000

    removed unsigned

--- mod/access/boost/install.sql
+++ mod/access/boost/install.sql
@@ -2,7 +2,7 @@
 -- @version $Id$
 
 CREATE TABLE access_shortcuts (
-  id int unsigned(11) NOT NULL default 0,
+  id int NOT NULL default 0,
   keyword varchar(40) NOT NULL,
   url varchar(255) NOT NULL,
   active smallint NOT NULL default 0,
