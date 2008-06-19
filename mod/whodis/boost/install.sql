fe14133e9190754ee10fb906ed5c12b4650fc026
Matt McNaney <matt@tux.appstate.edu>
2008-06-19 20:32:28 +0000

    + Changed url to a text field from a varchar. + Update script needed.

--- mod/whodis/boost/install.sql
+++ mod/whodis/boost/install.sql
@@ -2,7 +2,7 @@ CREATE TABLE whodis (
   id int NOT NULL default 0,
   created int NOT NULL default 0,
   updated int NOT NULL default 0,
-  url varchar(255) NOT NULL,
+  url text,
   visits int NOT NULL default 0,
   PRIMARY KEY  (id)
 );
