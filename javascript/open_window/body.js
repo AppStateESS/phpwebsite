ce07166b4379ddedf8f1a2caf0c736ad2aaca39b
Matt McNaney <matt@tux.appstate.edu>
2008-09-05 17:27:48 +0000

    + open_window can now have its id set.

--- javascript/open_window/body.js
+++ javascript/open_window/body.js
@@ -1,1 +1,1 @@
-<a href="#" class="{class}" onclick="javascript:open_window('{address}', {width}, {height}, '{window_name}'); return false" title="{link_title}">{label}</a>
+<a href="#" id="{id}" class="{class}" onclick="javascript:open_window('{address}', {width}, {height}, '{window_name}'); return false" title="{link_title}">{label}</a>
