97120e9718d6561cd6c6e450c8b578ef5fddb5ef
Matt McNaney <matt@tux.appstate.edu>
2008-10-01 17:05:10 +0000

    added delay to mouseover

--- mod/controlpanel/javascript/subpanel/head.js
+++ mod/controlpanel/javascript/subpanel/head.js
@@ -1,16 +1,27 @@
 <script type="text/javascript">
 //<![CDATA[
 
+var wait_a_sec = 0;
+
 $(document).ready(function()
 {
     $('#cp-subpanel').hide();
     $('#cp-panel-link').hover(
          function() 
          {
-             $('#cp-subpanel').show('fast');
+             if (wait_a_sec != 0) {
+                 clearTimeout(wait_a_sec);
+             }
+             wait_a_sec = setTimeout(function() {
+                wait_a_sec = 0;
+                $('#cp-subpanel').show('fast');
+	     }, 500);
          },
          function()
          {
+             if(wait_a_sec != 0) {
+                 clearTimeout(wait_a_sec);
+             }
              $('#cp-subpanel').hide();
          }
     );
