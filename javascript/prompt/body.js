48cc7a804ccd160f9e3cea43e60e28ea27627bcc
Matt McNaney <matt@tux.appstate.edu>
2008-10-02 18:24:41 +0000

    removed javascript from onclick call

--- javascript/prompt/body.js
+++ javascript/prompt/body.js
@@ -1,1 +1,1 @@
-<a href="#" onclick="javascript:show_prompt('{question}', '{address}', '{answer}', '{value_name}'); return false">{link}</a>
+<a href="#" onclick="show_prompt('{question}', '{address}', '{answer}', '{value_name}'); return false">{link}</a>
