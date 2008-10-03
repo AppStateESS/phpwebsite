a54bba04252f3da82d0714bceab053dfa1c1a861
Matt McNaney <matt@tux.appstate.edu>
2008-10-03 13:04:32 +0000

    update package

--- mod/breadcrumb/boost/boost.php
+++ mod/breadcrumb/boost/boost.php
@@ -6,7 +6,7 @@
    */
 
 $proper_name  = 'Breadcrumb';
-$version      = '2.0.0';
+$version      = '2.1.0';
 $version_http = 'http://phpwebsite.appstate.edu/downloads/modules/breadcrumb/check.xml';
 $priority     = 50;
 $about        = true;
