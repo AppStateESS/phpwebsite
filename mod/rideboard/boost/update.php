ba6617bf533cc76a49f3fdf00a57ff5ec984122b
Matt McNaney <matt@tux.appstate.edu>
2008-10-06 13:05:20 +0000

    update package

--- mod/rideboard/boost/update.php
+++ mod/rideboard/boost/update.php
@@ -18,8 +18,23 @@ function rideboard_update(&$content, $version)
         $content[] = "\n1.0.1 Version
 -------------
 + Settings allows menu link creation.</pre>";
-        
-        
+
+
+    case version_compare($version, '1.1.0', '<'):
+        $content[] = '<pre>';
+        $files = array('templates/carpool_view.tpl',
+                       'templates/carpools.tpl',
+                       'templates/settings.tpl',
+                       'templates/edit_carpool.tpl');
+
+        if (PHPWS_Boost::updateFiles($files, 'rideboard')) {
+            $content[] = '--- Local templates updated.';
+        } else {
+            $content[] = '--- Failed to update local templates.';
+        }
+        $content[] = "\n1.1.0 Version
+-------------
++ Added a carpooling component.</pre>";
     }
     return true;
 }
