e65df1625fec93ee6d3ce763f2ba1bf87b1a98f1
Matt McNaney <matt@tux.appstate.edu>
2008-12-19 18:52:52 +0000

    Partial fix for drag and drop images and links in fckeditor

--- javascript/editors/fckeditor/head.js
+++ javascript/editors/fckeditor/head.js
@@ -2,5 +2,21 @@
 <script type="text/javascript">
 //<![CDATA[
 var basepath = './javascript/editors/fckeditor/';
+/* This code only works on a source code submission. Hoping for a
+//change in the editor
+*/
+$(document).ready(function() {
+    $('form').submit(function() {
+        $('textarea').map(function(){
+            ta_name = $(this).attr('name');
+            oEditor = FCKeditorAPI.GetInstance(ta_name);
+            html_content = oEditor.GetData();
+            new_html = html_content.replace(/="..\/..\/..\/..\//, '="./');
+            $(this).text(new_html);
+            oEditor.SetData(new_html);
+        });
+    });
+});
+
 //]]>
 </script>
