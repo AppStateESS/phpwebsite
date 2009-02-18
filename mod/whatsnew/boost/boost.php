3445aef9e35cec26984e050979d555ca12f08cfc
Verdon Vaillancourt <verdon>
2009-02-18 19:51:19 +0000

    fixed version number in boost

--- mod/whatsnew/boost/boost.php
+++ mod/whatsnew/boost/boost.php
@@ -18,12 +18,12 @@
     * along with this program; if not, write to the Free Software
     * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
     *
-    * @version $Id:$
+    * @version $Id$
     * @author Verdon Vaillancourt <verdonv at users dot sourceforge dot net>
 */
 
 $proper_name  = "What's New";
-$version      = '0.1.0';
+$version      = '1.0.1';
 $import_sql   = false;
 $version_http = 'http://verdon.ca/downloads/modules/whatsnew/check.xml';
 $about        = true;
