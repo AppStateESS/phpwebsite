70b6671e4096245be95219fecb41eaa32ac9ef04
Matt McNaney <matt@tux.appstate.edu>
2009-03-10 19:28:51 +0000

    dot files added

--- mod/filecabinet/inc/known_types.php
+++ mod/filecabinet/inc/known_types.php
@@ -9,7 +9,7 @@ $known_images = array('jpg', 'png', 'gif');
 $known_media  = array('asf', 'mov', 'flv', 'mp4', 'qt', 'mp3', 'mp4', 'mpg', 'ogg', 'rm',
                       'swf', 'wav', 'wmv', 'avi');
 
-$known_documents = array('css', 'csv', 'doc', 'docx', 'mid', 'mp3', 'odb', 'odp',
+$known_documents = array('css', 'csv', 'dot', 'dotx', 'doc', 'docx', 'mid', 'mp3', 'odb', 'odp',
                    'ods', 'odt', 'ogg', 'pdf', 'ppt', 'pptx', 'pptm', 'rpm', 'rtf',
                    'tar', 'tgz', 'txt', 'wav', 'xls', 'xlsx', 'xml', 'zip', 'gz');
 ?>
