50793bb0fc3d51400d596862ea4122f5129453dd
Jeremy Booker <jbooker@tux.appstate.edu>
2009-04-30 15:13:24 +0000

    Renamed recaptcha_settings.php to avoid clobbering config files.

---
+++ javascript/captcha/recaptcha/recaptcha_settings.dist.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Settings for the ReCaptcha captcha library
+ *
+ * @author Jeremy Booker <jbooker at tux dot appstate dot edu>
+ */
+
+
+/**
+ * Get public and private API keys by going to http://recaptcha.net
+ * and signing up for an account.
+ */
+define('RECAPTCHA_PUBLIC_KEY',  '');
+define('RECAPTCHA_PRIVATE_KEY', '');
+define('RECAPTCHA_THEME',		'clean');
+
+
+?>
