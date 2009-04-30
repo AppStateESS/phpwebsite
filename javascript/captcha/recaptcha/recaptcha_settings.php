2277bc789e8434bb7e29110faa4e45e0585847e0
Jeremy Booker <jbooker@tux.appstate.edu>
2009-04-30 14:41:23 +0000

    Fixed syntax error (missing semicolon) in recaptcha_settings.php.

--- javascript/captcha/recaptcha/recaptcha_settings.php
+++ javascript/captcha/recaptcha/recaptcha_settings.php
@@ -11,9 +11,9 @@
  * Get a public an private key by going to http://recaptcha.net
  * and signing up for an account.
  */
-define('RECAPTCHA_PUBLIC_KEY',  '');
-define('RECAPTCHA_PRIVATE_KEY', '');
-define('RECAPTCHA_THEME',		'clean')
+define('RECAPTCHA_PUBLIC_KEY',  '6LdfMQMAAAAAAMbzrdem8kaDgXV7K9Ttjy4Mtpdu');
+define('RECAPTCHA_PRIVATE_KEY', '6LdfMQMAAAAAAFNXFZPkT6WEvZb0_nAjfuTATPC1');
+define('RECAPTCHA_THEME',		'clean');
 
 
 ?>
