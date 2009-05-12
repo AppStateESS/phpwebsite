5f21ead90c5248505dd76d0b2cbf272e13c6a035
Verdon Vaillancourt <verdon>
2009-05-12 19:00:40 +0000

    cleanup

--- mod/skeleton/index.php
+++ mod/skeleton/index.php
@@ -19,7 +19,7 @@
     * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
     *
     * @version $Id$
-    * @author Verdon Vaillancourt <verdonv at users dot sourceforge dot net> test
+    * @author Verdon Vaillancourt <verdonv at users dot sourceforge dot net>
 */
 
 if (!defined('PHPWS_SOURCE_DIR')) {
