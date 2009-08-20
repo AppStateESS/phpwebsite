09c00efa92768d48740a08e405b7347bc007a91c
Verdon Vaillancourt <verdon>
2009-08-20 19:40:54 +0000

    fix duplicate tab bug

--- mod/rolodex/class/RDX_Forms.php
+++ mod/rolodex/class/RDX_Forms.php
@@ -440,8 +440,8 @@ class Rolodex_Forms {
             }
         }
         if (Current_User::allow('rolodex', 'settings', null, null, true)) {
-            $vars['aop']  = 'new_location';
-            $ptags['ADD_LINK'] = PHPWS_Text::secureLink(dgettext('rolodex', 'Add Location'), 'rolodex', $vars);
+            $vars3['aop']  = 'new_location';
+            $ptags['ADD_LINK'] = PHPWS_Text::secureLink(dgettext('rolodex', 'Add Location'), 'rolodex', $vars3);
         }
         $pager->addPageTags($ptags);
         $pager->addToggle('class="toggle1"');
@@ -478,8 +478,8 @@ class Rolodex_Forms {
             }
         }
         if (Current_User::allow('rolodex', 'settings', null, null, true)) {
-            $vars['aop']  = 'new_feature';
-            $ptags['ADD_LINK'] = PHPWS_Text::secureLink(dgettext('rolodex', 'Add Feature'), 'rolodex', $vars);
+            $vars3['aop']  = 'new_feature';
+            $ptags['ADD_LINK'] = PHPWS_Text::secureLink(dgettext('rolodex', 'Add Feature'), 'rolodex', $vars3);
         }
         $pager->addPageTags($ptags);
         $pager->addToggle('class="toggle1"');
