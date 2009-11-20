2e4f49018a71811366082c1d0dd87686ffb97c19
Matt McNaney <matt@tux.appstate.edu>
2009-11-20 18:30:27 +0000

    so far so good

--- mod/filecabinet/javascript/jcarousel/head.js
+++ mod/filecabinet/javascript/jcarousel/head.js
@@ -1,3 +1,11 @@
+<style type="text/css">@import url("javascript/modules/filecabinet/lightbox/jquery.lightbox-0.5.css");</style>
+<script type="text/javascript" src="javascript/modules/filecabinet/lightbox/jquery.lightbox-0.5.min.js"></script>
+
+<script type="text/javascript">
+$(function() {
+    $('#gallery a').lightBox();
+});
+</script>
 <script type="text/javascript" src="javascript/modules/filecabinet/jcarousel/lib/jquery.jcarousel.pack.js"></script>
 
 <link rel="stylesheet" type="text/css" href="javascript/modules/filecabinet/jcarousel/lib/jquery.jcarousel.css" />
@@ -54,6 +62,7 @@
 }
 </style>
 <script type="text/javascript">
+
 jQuery(document).ready(function() {
     jQuery('#{CARO_ID} ul').jcarousel({
     vertical: {VERTICAL},
@@ -62,3 +71,29 @@ jQuery(document).ready(function() {
 });
 </script>
 <!-- END repeats -->
+
+
+<script type="text/javascript">
+$(function() {
+   $('div#caro-1 a').lightBox({
+        imageLoading: 'javascript/modules/filecabinet/lightbox/loading.gif',
+        imageBtnClose: 'javascript/modules/filecabinet/lightbox/close.gif',
+        imageBtnPrev: 'javascript/modules/filecabinet/lightbox/prev.gif',
+        imageBtnNext: 'javascript/modules/filecabinet/lightbox/next.gif',
+        imageBlank: 'javascript/modules/filecabinet/lightbox/blank.gif',
+        txtImage: 'Image',
+        txtOf: 'of'
+    });
+   
+   $('div#caro-2 a').lightBox({
+       imageLoading: 'javascript/modules/filecabinet/lightbox/loading.gif',
+       imageBtnClose: 'javascript/modules/filecabinet/lightbox/close.gif',
+       imageBtnPrev: 'javascript/modules/filecabinet/lightbox/prev.gif',
+       imageBtnNext: 'javascript/modules/filecabinet/lightbox/next.gif',
+       imageBlank: 'javascript/modules/filecabinet/lightbox/blank.gif',
+       txtImage: 'Image',
+       txtOf: 'of'
+   });
+   
+});
+</script>
