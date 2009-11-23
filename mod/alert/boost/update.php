a411526cac8a39b80ec4fdfe8feaf3a6918bbb57
Matt McNaney <matt@tux.appstate.edu>
2009-11-23 18:47:31 +0000

    update script written

--- mod/alert/boost/update.php
+++ mod/alert/boost/update.php
@@ -29,6 +29,18 @@ function alert_update(&$content, $version)
 + Updated to PHP 5 standard.
 + </pre>';
 
+    case version_compare($version, '1.3.0', '<'):
+        PHPWS_Boost::updateFiles(array('javascript/check_all/head.js'), 'alert');
+        $content[] = '<pre>1.3.0 Changes
+--------------
++ Fixed uninstall script
++ Fixed +/- check all links
++ items returned now for rss feeds if not shown on the home page.
++ Deleting an alert type removes the alerts.
++ Non-post alerts won\'t post now.
++ Fixed check all button.
++ Add and subtract buttons hidden if no types created
++ Fixed form select match for type in alerts</pre>';
 
     }
     return true;
