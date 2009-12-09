e63eb5733176d5b4ccdf9556bde12316aa96388c
Matt McNaney <matt@tux.appstate.edu>
2009-12-09 14:01:42 +0000

    incomplete icon config

---
+++ core/conf/icons.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * See docs/AUTHORS and docs/COPYRIGHT for relevant info.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * Defines default icons used by phpWebsite
+ *
+ * @version $Id$
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @package
+ * @license http://opensource.org/licenses/gpl-3.0.html
+ */
+
+$source = 'default/';
+
+
+$icons['active'] = array('src'=>'active.png', 'label'=>dgettext('core', 'Active'));
+/*
+ajax-wait
+bookmark
+calendar
+cancel
+check
+clear
+clip
+close
+close-folder
+comment
+copy
+cut
+deactive
+delete
+edit
+email
+exit
+fast-forward
+file
+forbidden
+*/
+$icons['help'] = array('src'=>'blank.png', 'class'=>'tango-icon', 'x'=>'-511', 'y'=>'-121', 'label'=>dgettext('core', 'Help'));
+/*
+image
+important
+lock
+magnify
+minus
+music
+new
+next
+open-folder
+paste
+permit
+pin
+play
+plus
+previous
+print
+refresh
+rewind
+rss
+save
+search
+sort
+sort-down
+sort-up
+sound
+stop-play
+switch
+tools
+undo
+unlock
+unpin
+users
+video
+*/
+?>
