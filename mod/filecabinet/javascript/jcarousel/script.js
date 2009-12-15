51b8ac5e560e2da0848c6a6d4adcb1998b0308d9
Matt McNaney <matt@tux.appstate.edu>
2009-12-15 19:08:14 +0000

    forcing some dimensions to help with display issues

--- mod/filecabinet/javascript/jcarousel/script.js
+++ mod/filecabinet/javascript/jcarousel/script.js
@@ -3,4 +3,15 @@ function carousel(caro_id, vertical_var, scroll_var, total_size) {
     initLightbox(id_tag);
     id_tag = 'div#' + caro_id + ' ul';
     $(id_tag).jcarousel({vertical: vertical_var,scroll: scroll_var});
+    if (vertical_var) {
+        sty1 = 'div#' + caro_id + ' .jcarousel-clip-vertical';
+        sty2 = 'div#' + caro_id + ' .jcarousel-container-vertical';
+        dim = 'height';
+    } else {
+        sty1 = 'div#' + caro_id + ' .jcarousel-clip-vertical';
+        sty2 = 'div#' + caro_id + ' .jcarousel-container-vertical';
+        dim = 'width';
+    }
+    $(sty1).css(dim, total_size);
+    $(sty2).css(dim, total_size);
 }
