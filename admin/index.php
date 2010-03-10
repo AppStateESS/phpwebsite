f6bf62352c89a1a733f46e5854d80a7bd85c745e
Matt McNaney <matt@tux.appstate.edu>
2010-03-10 14:35:24 +0000

    tidy

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,9 @@
 <?php
 
-  /**
-   * @author Matthew McNaney <mcnaney at gmail dot com>
-   * @version $id$
-   */
+/**
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @version $id$
+ */
 
 header('location: ../index.php?module=users&action=user&command=login_page');
 exit();
