9772d22a590e033059b46969a404c2dabec5cc3d
Matt McNaney <matt@tux.appstate.edu>
2010-03-10 17:53:48 +0000

    eclipse tidy

--- javascript/check_all/default.php
+++ javascript/check_all/default.php
@@ -9,12 +9,12 @@ $data['uncheck_label'] = _('Uncheck all');
 $data['check_label'] = _('Check all');
 
 switch (@$data['type']) {
- case 'checkbox':
-     $data['input_type'] = 'checkbox';
-     break;
-     
- default:
-     $data['input_type'] = 'button';
-     break;
-}     
+    case 'checkbox':
+        $data['input_type'] = 'checkbox';
+        break;
+         
+    default:
+        $data['input_type'] = 'button';
+        break;
+}
 ?>
