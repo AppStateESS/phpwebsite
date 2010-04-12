97be6ff8ac73985d87f41682d4feaf00c193ebdd
Matt McNaney <matt@tux.appstate.edu>
2010-04-12 15:28:29 +0000

    deleting

--- core/conf/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('PHPWS_SOURCE_DIR', '/var/www/workspace/phpwebsite/');
-define('PHPWS_HOME_DIR', '/var/www/workspace/phpwebsite/');
-define('PHPWS_SOURCE_HTTP', 'http://localhost/workspace/phpwebsite/');
-define('SITE_HASH', 'd7f46abf5e85ff863536e559985dd17f');
-define('PHPWS_DSN', 'mysql://matt:wozrel@localhost/phpwebsite');
-?>
