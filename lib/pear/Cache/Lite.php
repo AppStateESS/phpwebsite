147c9bfc027e02798fc8122558551e0f569a94ba
Matt McNaney <matt@tux.appstate.edu>
2010-04-28 14:23:06 +0000

    fixed strict warning

--- lib/pear/Cache/Lite.php
+++ lib/pear/Cache/Lite.php
@@ -535,7 +535,9 @@ class Cache_Lite
     function raiseError($msg, $code)
     {
         include_once('PEAR.php');
-        return PEAR::raiseError($msg, $code, $this->_pearErrorMode);
+        $pear = new PEAR;
+        
+        return $pear->raiseError($msg, $code, $this->_pearErrorMode);
     }
     
     /**
