b2e3a7c99423f7aacd10a2f54ac2e9d428ad5201
Matt McNaney <matt@tux.appstate.edu>
2010-05-03 18:01:05 +0000

    Fixed labels

--- core/class/Form.php
+++ core/class/Form.php
@@ -628,7 +628,7 @@ class PHPWS_Form {
     public function makeLabel($element, $label)
     {
         $required = $element->getRequired();
-        return sprintf('<label class="%s-label" id="%s-label" for="%s">%s</label>%s', $element->type, $element->name, $element->name, $label, $required);
+        return sprintf('<label class="%s-label" id="%s-label" for="%s">%s</label>%s', $element->type, $element->name, $element->id, $label, $required);
     }
 
 
