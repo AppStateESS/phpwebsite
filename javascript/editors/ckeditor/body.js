f5b845145f71fff79dd06fbf4a33ab81d4f9535a
Matt McNaney <matt@tux.appstate.edu>
2010-05-12 20:26:28 +0000

    file manager added

--- javascript/editors/ckeditor/body.js
+++ javascript/editors/ckeditor/body.js
@@ -1,9 +1,10 @@
 <textarea cols="80" id="{ID}" name="{NAME}" rows="10">{VALUE}</textarea>
 <script type="text/javascript">
 CKEDITOR.replace( '{ID}', {customConfig : '{source_http}javascript/editors/ckeditor/phpws/config.js',
+filebrowserBrowseUrl: '{source_http}javascript/editors/ckeditor/filemanager/index.php?sh=' + sh,
 //filebrowserBrowseUrl : source_http + 'phpws/browse.php',
 //filebrowserUploadUrl : source_http + 'phpws/upload.php',
-filebrowserImageWindowWidth : '300',
-filebrowserImageWindowHeight : '300'
+filebrowserImageWindowWidth : '1200',
+filebrowserImageWindowHeight : '600'
 } );
 </script>
