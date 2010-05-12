63a89e92adf7579af0dc517bd90eda9acdf5c934
Matt McNaney <matt@tux.appstate.edu>
2010-05-12 20:05:57 +0000

    ck filemanager

---
+++ javascript/editors/ckeditor/default.php
@@ -0,0 +1,5 @@
+<?php
+$_SESSION['ckeditor_image_directory'] = '/var/www/workspace/phpwebsite/images/';
+$_SESSION['ckeditor_http'] = 'http://localhost/workspace/phpwebsite/images/';
+$default['site_hash'] = SITE_HASH;
+?>
