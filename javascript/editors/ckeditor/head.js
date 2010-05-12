eb8bd0c5d862d730f1df9204130c75ccc06a0f5a
Matt McNaney <matt@tux.appstate.edu>
2010-05-12 20:25:50 +0000

    put site hash in there

--- javascript/editors/ckeditor/head.js
+++ javascript/editors/ckeditor/head.js
@@ -1,2 +1,2 @@
-<script type="text/javascript">var source_http = '{source_http}javascript/editors/ckeditor/'</script>
+<script type="text/javascript">var source_http = '{source_http}javascript/editors/ckeditor/'; var sh = '{site_hash}'</script>
 <script type="text/javascript" src="{source_http}javascript/editors/ckeditor/ckeditor.js"></script>
