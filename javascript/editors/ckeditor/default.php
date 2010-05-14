01060778fc13932291e5042013c39b018aa722eb
Matt McNaney <matt@tux.appstate.edu>
2010-05-14 15:34:15 +0000

    ck filemanager

--- javascript/editors/ckeditor/default.php
+++ javascript/editors/ckeditor/default.php
@@ -1,6 +1,6 @@
 <?php
-$_SESSION['ck_dir'] = PHPWS_SOURCE_DIR . 'images/ckeditor/';
+$_SESSION['ck_dir'] = PHPWS_HOME_DIR . 'images/ckeditor/';
 $_SESSION['ck_http'] = PHPWS_Core::getHomeHttp() . 'images/ckeditor/';
-$_SESSION['source_dir'] = PHPWS_SOURCE_DIR;
+$_SESSION['home_dir'] = PHPWS_HOME_DIR;
 $default['session_name'] = session_name();
 ?>
