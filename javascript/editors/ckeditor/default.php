3df5242bedfc32f53b253a7bd232e0a172d0983b
Matt McNaney <matt@tux.appstate.edu>
2010-05-20 12:47:06 +0000

    dropping

--- javascript/editors/ckeditor/default.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_SESSION['ck_dir'] = PHPWS_HOME_DIR . 'images/ckeditor/';
-$_SESSION['ck_http'] = PHPWS_Core::getHomeHttp() . 'images/ckeditor/';
-$_SESSION['home_dir'] = PHPWS_HOME_DIR;
-$default['session_name'] = session_name();
-?>
