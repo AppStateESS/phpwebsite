2bea70c8aa179a8d1f0e4fce3d4c572fd0fddec2
Matt McNaney <matt@tux.appstate.edu>
2010-11-24 15:11:31 +0000

    added source_http to spell checker

--- javascript/editors/tinymce/custom.js
+++ javascript/editors/tinymce/custom.js
@@ -2,7 +2,7 @@ tinyMCE.init({
         mode : "specific_textareas",
         strict_loading_mode : true,
         theme : 'advanced',
-        spellchecker_rpc_url : 'javascript/editors/tinymce/jscripts/tiny_mce/plugins/spellchecker/rpc.php',
+        spellchecker_rpc_url : source_http + 'javascript/editors/tinymce/jscripts/tiny_mce/plugins/spellchecker/rpc.php',
         textarea_trigger : "mceEditor",
         plugins : "safari,table,advimage,print,preview,searchreplace,spellchecker,fullscreen,paste,emotions",
         theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontsizeselect",
--- javascript/editors/tinymce/head.js
+++ javascript/editors/tinymce/head.js
@@ -1,3 +1,4 @@
+<script language="javascript" type="text/javascript">var source_http = '{source_http}';</script>
 <script language="javascript" type="text/javascript" src="{source_http}javascript/jquery/jquery.js"></script>
 <script language="javascript" type="text/javascript" src="{source_http}javascript/editors/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
 <script language="javascript" type="text/javascript" src="{source_http}javascript/editors/tinymce/{config}"></script>
