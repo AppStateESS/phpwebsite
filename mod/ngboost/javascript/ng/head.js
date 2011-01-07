48a2ab4025b9927c1af058e7ed64bf9b1bf20117
Matt McNaney <matt@tux.appstate.edu>
2011-01-07 20:08:45 +0000

    js fix

--- mod/ngboost/javascript/ng/head.js
+++ mod/ngboost/javascript/ng/head.js
@@ -7,9 +7,9 @@ var authkey = '{authkey}';
 <script type="text/javascript" src="{source_http}javascript/jquery/jquery.js"></script>
 <script type="text/javascript" src="{source_http}javascript/jquery_tab/ui.core.js"></script>
 <script type="text/javascript" src="{source_http}javascript/jquery_tab/ui.tabs.js"></script>
-<script type="text/javascript" src="{source_http}mod/ngboost/javascript/jqModal.js"></script>
-<script type="text/javascript" src="{source_http}mod/ngboost/javascript/fastconfirm/jquery.fastconfirm.js"></script>
-<script type="text/javascript" src="{source_http}mod/ngboost/javascript/ngBoost.js"></script>
+<script type="text/javascript" src="{source_http}mod/ngboost/javascript/ng/jqModal.js"></script>
+<script type="text/javascript" src="{source_http}mod/ngboost/javascript/ng/fastconfirm/jquery.fastconfirm.js"></script>
+<script type="text/javascript" src="{source_http}mod/ngboost/javascript/ng/ngBoost.js"></script>
 <link type="text/css" href="{source_http}javascript/jquery_tab/themes/base/ui.all.css" rel="stylesheet"></link>
-<link type="text/css" href="{source_http}mod/ngboost/javascript/fastconfirm/jquery.fastconfirm.css" rel="stylesheet"></link>
-<link type="text/css" href="{source_http}mod/ngboost/javascript/jqModal.css" rel="stylesheet"></link>
+<link type="text/css" href="{source_http}mod/ngboost/javascript/ng/fastconfirm/jquery.fastconfirm.css" rel="stylesheet"></link>
+<link type="text/css" href="{source_http}mod/ngboost/javascript/ng/jqModal.css" rel="stylesheet"></link>
