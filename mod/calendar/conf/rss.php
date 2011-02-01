9e388c6dbee050bc8ae5cb0ed1c41843c1af9671
Matt McNaney <matt@tux.appstate.edu>
2011-02-01 14:11:27 +0000

    added rss files for these modules

---
+++ mod/calendar/conf/rss.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * not used : link, pub_date, text_input
+ *
+ *
+ * @author Matthew McNaney <matt at tux dot appstate dot edu>
+ * @version $Id: rss.php 5472 2007-12-11 16:13:40Z jtickle $
+ */
+
+$channel['title'] = 'Events';
+$channel['description'] = 'Events of note at this site.';
+
+?>
