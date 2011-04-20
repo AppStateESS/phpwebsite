1bf973d34e081cfc8ad20ae7ea15c4846e25b3e5
Matt McNaney <matt@tux.appstate.edu>
2011-04-20 16:33:30 +0000

    my new qr module

---
+++ mod/qr/class/QR.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ */
+require_once QR_LIB_DIR . 'qrlib.php';
+
+class QR {
+
+    /**
+     * URL of the site getting a symbol
+     * @var string
+     */
+    private $url;
+    /**
+     * md5 hash of url
+     * @var string
+     */
+    private $tag;
+    /**
+     * Filename of image
+     * @var string
+     */
+    private $file;
+    private $error_correction = QR_ERROR_CORRECTION_LEVEL;
+    private $size = 6;
+
+    public function __construct($key_id, $size)
+    {
+        $this->key_id = (int) $key_id;
+        $this->setSize($size);
+        $this->load();
+    }
+
+    public function setSize($size) {
+        $size = (int)$size;
+        if ($size < 13 && $size > 2) {
+            $this->size = $size;
+        }
+    }
+
+    private function load()
+    {
+        if ($this->key_id) {
+            $key = new Key($this->key_id);
+        } else {
+            $key = Key::getHomeKey();
+        }
+
+        $this->url = PHPWS_Core::getHomeHttp() . $key->url;
+        $this->tag = md5($this->url);
+        $this->file = QR_IMAGE_DIR . $this->tag . '_' . $this->size . '.png';
+        $this->image = QR_IMAGE_HTTP . $this->tag . '_' . $this->size . '.png';
+    }
+
+    public function get()
+    {
+        if (!is_file($this->file)) {
+            QRcode::png($this->url, $this->file, $this->error_correction, $this->size, 2);
+        }
+        return '<img src="' . $this->image . '" />';
+    }
+}
+
+?>
