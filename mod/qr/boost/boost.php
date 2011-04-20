9e9433d92d8b16bb3425af200c24800a96540ec1
Matt McNaney <matt@tux.appstate.edu>
2011-04-20 16:33:19 +0000

    my new qr module

---
+++ mod/qr/boost/boost.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ */
+$proper_name  = 'QR';
+$version      = '1.0.0';
+$register     = false;
+$unregister   = false;
+$import_sql   = false;
+$version_http = 'http://phpwebsite.appstate.edu/downloads/modules/qr/check.xml';
+$about        = true;
+$priority     = 50;
+$dependency   = false;
+$image_dir    = true;
+
+?>
