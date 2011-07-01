3f693421a3b5a7c1cb58c400b2bde23ae39d2854
Matt McNaney <matt@tux.appstate.edu>
2011-07-01 15:16:56 +0000

    added boost file

---
+++ mod/help/boost/boost.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @version $Id: boost.php 7993 2011-06-24 15:45:31Z matt $
+ */
+
+$proper_name  = 'Help';
+$version      = '1.0.0';
+$register     = false;
+$unregister   = false;
+$import_sql   = false;
+$version_http = 'http://phpwebsite.appstate.edu/downloads/modules/help/check.xml';
+$about        = false;
+$priority     = 50;
+$dependency   = false;
+$image_dir    = false;
+
+?>
