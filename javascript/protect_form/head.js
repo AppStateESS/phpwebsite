90e586373b112341e3e5e6a459f0c37df3dd52d2
Jeff Tickle <jtickle@tux.appstate.edu>
2012-01-26 18:02:24 +0000

    Added "onbeforeunload" javascript for Form class

---
+++ javascript/protect_form/head.js
@@ -0,0 +1,38 @@
+<script type="text/javascript">
+//<![CDATA[
+    $(function() {
+        var isPrevented = false;
+        
+        handleNavigate = function() {
+            return "Your changes have not been saved.  Are you sure you want to leave this page?";
+        }
+        
+        doPreventNavigate = function() {
+            if(isPrevented) return;
+            
+            $(window).on('beforeunload', handleNavigate);
+            isPrevented = true;
+        };
+
+        undoPreventNavigate = function() {
+            if(!isPrevented) return;
+            
+            $(window).off('beforeunload', handleNavigate);
+            isPrevented = false;
+        };
+        
+        $('.form-protected').each(function() {
+            $(this).find('input').change(function() {
+                alert('changed');
+                doPreventNavigate();
+            });
+            $(this).submit(function() {
+                undoPreventNavigate();
+            });
+            $(this).bind('reset', function() {
+                undoPreventNavigate();
+            });
+        });
+    });
+//]]>
+</script>
