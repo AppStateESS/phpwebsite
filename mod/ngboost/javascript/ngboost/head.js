ef7c3549c4d90a9cc0b0a64a0902466a3ce9ccb8
Matt McNaney <matt@tux.appstate.edu>
2012-02-07 18:37:05 +0000

    tidy

---
+++ mod/ngboost/javascript/ngboost/head.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+//<![CDATA[
+var source_http = '{source_http}';
+var authkey = '{authkey}';
+var ngboostmsg011 = '{ngboostmsg011}';
+var ngboostmsg012 = '{ngboostmsg012}';
+var ngboostmsg020 = '{ngboostmsg020}';
+var ngboostmsg030 = '{ngboostmsg030}';
+var ngboostmsg040 = '{ngboostmsg040}';
+var ngboostmsg050 = '{ngboostmsg050}';
+//]]>
+</script>
+<script type="text/javascript" src="{source_http}javascript/jquery_tab/ui.core.js"></script>
+<script type="text/javascript" src="{source_http}javascript/jquery_tab/ui.tabs.js"></script>
+<script type="text/javascript" src="{source_http}mod/ngboost/javascript/ngboost/jqModal.js"></script>
+<script type="text/javascript" src="{source_http}mod/ngboost/javascript/fastconfirm/jquery.fastconfirm.js"></script>
+<script type="text/javascript" src="{source_http}mod/ngboost/javascript/ngboost/ngBoost.js"></script>
+<link type="text/css" href="{source_http}javascript/jquery_tab/themes/base/ui.all.css" rel="stylesheet"></link>
+<link type="text/css" href="{source_http}mod/ngboost/javascript/fastconfirm/jquery.fastconfirm.css" rel="stylesheet"></link>
+<link type="text/css" href="{source_http}mod/ngboost/javascript/ngboost/jqModal.css" rel="stylesheet"></link>
