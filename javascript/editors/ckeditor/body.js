10267eeed96d0b62ad0c782e9cca6f458a8228c5
Matt McNaney <matt@tux.appstate.edu>
2012-06-29 15:47:47 +0000

    fixed

--- javascript/editors/ckeditor/body.js
+++ javascript/editors/ckeditor/body.js
@@ -1,8 +1,4 @@
 <textarea cols="80" id="{ID}" name="{NAME}" rows="10">{VALUE}</textarea>
 <script type="text/javascript">
-CKEDITOR.replace( '{ID}', {customConfig : 'phpws_config.js'/* ,
-filebrowserBrowseUrl: '{source_http}javascript/editors/ckeditor/filemanager/index.php?sn=' + sn,
-filebrowserImageWindowWidth : '1200',
-filebrowserImageWindowHeight : '600' */
-} );
+CKEDITOR.replace( '{ID}', {customConfig : 'phpws_config.js'} );
 </script>
