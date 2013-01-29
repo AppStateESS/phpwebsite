0aaee5ffc91a114879e80024980415579fbd9332
Matt McNaney <matt@tux.appstate.edu>
2013-01-29 14:50:56 +0000

    added class and title

--- javascript/prompt/body2.js
+++ javascript/prompt/body2.js
@@ -1,1 +1,1 @@
-<input type="button" value="{link}" onclick="javascript:show_prompt('{question}', '{address}', '{answer}', '{value_name}'); return false" />
+<input class="{class}" title="{title}" type="button" value="{link}" onclick="javascript:show_prompt('{question}', '{address}', '{answer}', '{value_name}'); return false" />
--- javascript/prompt/default.php
+++ javascript/prompt/default.php
@@ -1,14 +1,13 @@
 <?php
 
-  /**
-   * @version $Id$
-   * @author Matthew McNaney <mcnaney at gmail dot com>
-   */
-
-$default['question']      = _('Change to');
-$default['answer']        = '';
-$default['value_name']    = 'prompt';
-$default['link']          = 'Prompt!';
+/**
+ * @version $Id$
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ */
+$default['question'] = _('Change to');
+$default['answer'] = '';
+$default['value_name'] = 'prompt';
+$default['link'] = 'Prompt!';
 if (isset($data['type'])) {
     if ($data['type'] == 'button') {
         $bodyfile = PHPWS_SOURCE_DIR . 'javascript/prompt/body2.js';
@@ -22,4 +21,7 @@ if (isset($data['answer'])) {
     $data['answer'] = str_replace("&#039;", "\\'", $data['answer']);
 }
 
+if (!isset($data['title'])) {
+    $data['title'] = $data['link'];
+}
 ?>
