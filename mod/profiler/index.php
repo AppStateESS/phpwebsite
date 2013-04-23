7307a64f4958dacdb947af62b3b9d1a42a34b7a1
jlbooker <jeremy.booker@gmail.com>
2013-04-23 12:56:22 -0400

    Add deprecation warning to Profiler module. #7

--- mod/profiler/index.php
+++ mod/profiler/index.php
@@ -1,5 +1,13 @@
 <?php
 /**
+ * 
+ * WARNING: This module has been deprecated. It will no longer be
+ * maintained by phpwebsite and no further bug/security patches will
+ * be released. It will be removed from the phpWebsite distribution
+ * at some point in the future.
+ * 
+ * @deprecated
+ * 
  * @author Matthew McNaney <mcnaney at gmail dot com>
  * @version $Id$
  */
