44b1f8cf04607041e018ac34a503f84de61db16b
Stardog <mcnaney@gmail.com>
2013-05-02 09:40:52 -0400

    Pagesmith update package

--- mod/pagesmith/boost/update.php
+++ mod/pagesmith/boost/update.php
@@ -1,10 +1,9 @@
 <?php
+
 /**
  * @version $Id$
  * @author Matthew McNaney <mcnaney at gmail dot com>
  */
-
-
 function pagesmith_update(&$content, $currentVersion)
 {
     $home_dir = PHPWS_Boost::getHomeDir();
@@ -14,7 +13,8 @@ function pagesmith_update(&$content, $currentVersion)
             $content[] = '<pre>';
 
             $db = new PHPWS_DB('ps_page');
-            $result = $db->addTableColumn('front_page', 'smallint NOT NULL default 0');
+            $result = $db->addTableColumn('front_page',
+                    'smallint NOT NULL default 0');
             if (PHPWS_Error::logIfError($result)) {
                 $content[] = "--- Unable to create table column 'front_page' on ps_page table.</pre>";
                 return false;
@@ -22,7 +22,7 @@ function pagesmith_update(&$content, $currentVersion)
                 $content[] = "--- Created 'front_page' column on ps_page table.";
             }
             $files = array('templates/page_list.tpl', 'javascript/update/head.js', 'conf/error.php',
-                       'templates/page_templates/simple/page.tpl', 'templates/page_templates/twocolumns/page.tpl');
+                'templates/page_templates/simple/page.tpl', 'templates/page_templates/twocolumns/page.tpl');
             pagesmithUpdateFiles($files, $content);
 
             if (!PHPWS_Boost::inBranch()) {
@@ -32,7 +32,7 @@ function pagesmith_update(&$content, $currentVersion)
 
         case version_compare($currentVersion, '1.0.2', '<'):
             $content[] = '<pre>';
-            $dest_dir   = $home_dir . 'javascript/modules/pagesmith/passinfo/';
+            $dest_dir = $home_dir . 'javascript/modules/pagesmith/passinfo/';
 
             if (!is_dir($dest_dir)) {
                 if (is_writable($home_dir . 'javascript/modules/pagesmith/') && @mkdir($dest_dir)) {
@@ -63,7 +63,7 @@ function pagesmith_update(&$content, $currentVersion)
             $content[] = '<pre>';
 
             $source_dir = PHPWS_SOURCE_DIR . 'mod/pagesmith/templates/page_templates/text_only/';
-            $dest_dir   = $home_dir . 'templates/pagesmith/page_templates/text_only/';
+            $dest_dir = $home_dir . 'templates/pagesmith/page_templates/text_only/';
 
             if (PHPWS_File::copy_directory($source_dir, $dest_dir)) {
                 $content[] = "--- Successfully copied $source_dir\n    to $dest_dir\n";
@@ -97,8 +97,9 @@ function pagesmith_update(&$content, $currentVersion)
 
         case version_compare($currentVersion, '1.0.5', '<'):
             $content[] = '<pre>';
-            pagesmithUpdateFiles(array('templates/page_templates/text_only/page.tpl'), $content);
-            $content[] ='1.0.5 changes
+            pagesmithUpdateFiles(array('templates/page_templates/text_only/page.tpl'),
+                    $content);
+            $content[] = '1.0.5 changes
 ----------------
 + Changed wording on move to front functionality
 + Added move to front to miniadmin
@@ -129,10 +130,10 @@ function pagesmith_update(&$content, $currentVersion)
             Cabinet::convertImagesToFileAssoc('ps_block', 'type_id');
             $content[] = '--- Images converted for File Cabinet 2.0.0.';
             pagesmithUpdateFiles(array('javascript/passinfo/head.js',
-                                   'templates/page_templates/threesec/page.css',
-                                   'templates/page_templates/threesec/page.tpl',
-                                   'templates/page_templates/threesec/structure.xml',
-                                   'templates/page_templates/threesec/threesec.png'), $content);
+                'templates/page_templates/threesec/page.css',
+                'templates/page_templates/threesec/page.tpl',
+                'templates/page_templates/threesec/structure.xml',
+                'templates/page_templates/threesec/threesec.png'), $content);
 
             $content[] = '1.1.0 changes (unreleased)
 -------------
@@ -144,29 +145,33 @@ function pagesmith_update(&$content, $currentVersion)
             $content[] = '<pre>';
 
             $source_tpl = PHPWS_SOURCE_DIR . 'mod/pagesmith/templates/page_templates/';
-            $local_tpl  = $home_dir . 'templates/pagesmith/page_templates/';
-            $backup     = $home_dir . 'templates/pagesmith/_page_templates/';
+            $local_tpl = $home_dir . 'templates/pagesmith/page_templates/';
+            $backup = $home_dir . 'templates/pagesmith/_page_templates/';
             $source_img = PHPWS_SOURCE_DIR . 'mod/pagesmith/img/folder_icons/';
-            $local_img  = $home_dir . 'images/mod/pagesmith/folder_icons/';
+            $local_img = $home_dir . 'images/mod/pagesmith/folder_icons/';
 
-            if (is_dir($backup) || @PHPWS_File::copy_directory($local_tpl, $backup)) {
+            if (is_dir($backup) || @PHPWS_File::copy_directory($local_tpl,
+                            $backup)) {
                 $content[] = '--- Local page templates backed up to: ' . $backup;
             } else {
-                $content[] = sprintf('--- Could not backup directory "%s" to "%s"</pre>', $local_tpl, $backup);
+                $content[] = sprintf('--- Could not backup directory "%s" to "%s"</pre>',
+                        $local_tpl, $backup);
                 return false;
             }
 
             if (@PHPWS_File::copy_directory($source_tpl, $local_tpl)) {
                 $content[] = '--- Local page templates updated.';
             } else {
-                $content[] = sprintf('--- Could not copy directory "%s" to "%s"</pre>', $source_tpl, $local_tpl);
+                $content[] = sprintf('--- Could not copy directory "%s" to "%s"</pre>',
+                        $source_tpl, $local_tpl);
                 return false;
             }
 
             if (@PHPWS_File::copy_directory($source_img, $local_img)) {
                 $content[] = '--- New page template icons copied locally.';
             } else {
-                $content[] = sprintf('--- Could not copy directory "%s" to "%s"</pre>', $source_img, $local_img);
+                $content[] = sprintf('--- Could not copy directory "%s" to "%s"</pre>',
+                        $source_img, $local_img);
                 return false;
             }
 
@@ -178,7 +183,7 @@ function pagesmith_update(&$content, $currentVersion)
             }
 
             $files = array('templates/pick_folder.tpl', 'templates/pick_template.tpl',
-                       'templates/style.css', 'conf/folder_icons.php');
+                'templates/style.css', 'conf/folder_icons.php');
             pagesmithUpdateFiles($files, $content);
 
 
@@ -209,12 +214,14 @@ function pagesmith_update(&$content, $currentVersion)
             $db->dropTableColumn('btype');
 
             $db = new PHPWS_DB('ps_page');
-            if (PHPWS_Error::logIfError($db->addTableColumn('parent_page', 'int NOT NULL default 0'))) {
+            if (PHPWS_Error::logIfError($db->addTableColumn('parent_page',
+                                    'int NOT NULL default 0'))) {
                 $content[] = 'Could not create ps_page.parent_page column.';
                 return false;
             }
 
-            if (PHPWS_Error::logIfError($db->addTableColumn('page_order', 'smallint NOT NULL default 0'))) {
+            if (PHPWS_Error::logIfError($db->addTableColumn('page_order',
+                                    'smallint NOT NULL default 0'))) {
                 $content[] = 'Could not create ps_page.page_order column.';
                 return false;
             }
@@ -222,40 +229,41 @@ function pagesmith_update(&$content, $currentVersion)
             $db = new PHPWS_DB('ps_text');
 
             if (PHPWS_DB::getDBType() == 'mysql' ||
-            PHPWS_DB::getDBType() == 'mysqli') {
-                if (PHPWS_Error::logIfError($db->alterColumnType('content', 'longtext NOT NULL'))) {
+                    PHPWS_DB::getDBType() == 'mysqli') {
+                if (PHPWS_Error::logIfError($db->alterColumnType('content',
+                                        'longtext NOT NULL'))) {
                     $content[] = 'Could not alter ps_text.content column.';
                 }
             }
 
             $content[] = '<pre>';
             $files = array('javascript/passinfo/head.js', 'templates/page_form.tpl',
-                       'javascript/delete_orphan/',
-                       'javascript/confirm_delete/',
-                       'javascript/update/head.js',
-                       'templates/page_templates/threesec-tbl/',
-                       'templates/orphans.tpl',
-                       'templates/page_form.tpl',
-                       'templates/page_frame.tpl',
-                       'templates/page_list.tpl',
-                       'templates/style.css',
-                       'templates/sublist.tpl',
-                       'templates/upload_template.tpl',
-                       'img/add.png', 'img/delete.png',
-                       'img/back.png', 'img/front.png'
-                       );
-                       pagesmithUpdateFiles($files, $content);
+                'javascript/delete_orphan/',
+                'javascript/confirm_delete/',
+                'javascript/update/head.js',
+                'templates/page_templates/threesec-tbl/',
+                'templates/orphans.tpl',
+                'templates/page_form.tpl',
+                'templates/page_frame.tpl',
+                'templates/page_list.tpl',
+                'templates/style.css',
+                'templates/sublist.tpl',
+                'templates/upload_template.tpl',
+                'img/add.png', 'img/delete.png',
+                'img/back.png', 'img/front.png'
+            );
+            pagesmithUpdateFiles($files, $content);
 
-                       if (!PHPWS_Boost::inBranch()) {
-                           $content[] = file_get_contents(PHPWS_SOURCE_DIR . 'mod/pagesmith/boost/changes/1_3_0.txt');
-                       }
-                       $content[] = '</pre>';
+            if (!PHPWS_Boost::inBranch()) {
+                $content[] = file_get_contents(PHPWS_SOURCE_DIR . 'mod/pagesmith/boost/changes/1_3_0.txt');
+            }
+            $content[] = '</pre>';
 
         case version_compare($currentVersion, '1.3.1', '<'):
             $content[] = '<pre>';
             $files = array('templates/page_templates/threesec/page.tpl',
-                       'templates/page_templates/threesec-tbl/page.tpl',
-                       'templates/settings.tpl');
+                'templates/page_templates/threesec-tbl/page.tpl',
+                'templates/settings.tpl');
             pagesmithUpdateFiles($files, $content);
             $content[] = '1.3.1 changes
 ---------------
@@ -279,7 +287,8 @@ function pagesmith_update(&$content, $currentVersion)
 
         case version_compare($currentVersion, '1.3.3', '<'):
             $db = new PHPWS_DB('ps_text');
-            if (PHPWS_Error::logIfError($db->alterColumnType('content', 'longtext'))) {
+            if (PHPWS_Error::logIfError($db->alterColumnType('content',
+                                    'longtext'))) {
                 $content[] = 'Could not alter ps_text.content column.';
                 return false;
             } else {
@@ -287,7 +296,7 @@ function pagesmith_update(&$content, $currentVersion)
             }
             $content[] = '<pre>';
             pagesmithUpdateFiles(array('javascript/disable_links/',
-                                   'javascript/update/head.js'), $content);
+                'javascript/update/head.js'), $content);
             $content[] = 'Version 1.3.3
 --------------------------------------------------------------------
 + Made ps_text.content null instead of not null
@@ -321,19 +330,24 @@ function pagesmith_update(&$content, $currentVersion)
 + Added default installation page.
 + PHP 5 strict fixes.</pre>';
 
-            case version_compare($currentVersion, '1.4.1', '<'):
+        case version_compare($currentVersion, '1.4.1', '<'):
             $content[] = '<pre>1.4.1 changes
 -------------
 Fixed bug causing blank editors on edit.</pre>';
 
-                case version_compare($currentVersion, '1.4.2', '<'):
+        case version_compare($currentVersion, '1.4.2', '<'):
             $content[] = '<pre>1.4.2 changes
 -------------
 + Added conditionals to try and prevent blank page posting.
 + Updated javascript to jquery code.
 + Added some new templates.
 .</pre>';
-
+        case version_compare($currentVersion, '1.5.0', '<'):
+            $content[] = '<pre>1.5.0 changes
+---------------
++ New method of editing text. Click on text instead of edit image.
++ Javascript window replaced with Jquery dialog.
+</pre>';
     } // end switch
 
     return true;
