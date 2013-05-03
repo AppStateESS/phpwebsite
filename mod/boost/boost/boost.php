9d1abece904903ec98a3c2417cbd2fce9e4caa51
Stardog <mcnaney@gmail.com>
2013-05-03 14:49:18 -0400

    Update notes finished

--- mod/boost/boost/boost.php
+++ mod/boost/boost/boost.php
@@ -5,7 +5,7 @@
  */
 
 $proper_name  = 'Boost';
-$version      = '2.3.5';
+$version      = '2.3.6';
 $version_http = 'http://phpwebsite.appstate.edu/downloads/modules/boost/check.xml';
 $dependency   = true;
 $about        = true;
