b7101e7a7669b0aef86d71a85362a715983aa5c0
Stardog <mcnaney@gmail.com>
2013-05-14 11:01:43 -0400

    Revert "Updating jquery and jquery-ui"
    This reverts commit a64b08c822b152969a736b4edb9b3e2b3b063a01.

--- javascript/jquery/head.js
+++ javascript/jquery/head.js
@@ -1,1 +1,1 @@
-<script type="text/javascript" src="{source_http}javascript/jquery/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="{source_http}javascript/jquery/jquery-1.8.2.min.js"></script>
--- javascript/jquery_ui/head.js
+++ javascript/jquery_ui/head.js
@@ -1,2 +1,2 @@
 <link type="text/css" href="{source_http}javascript/jquery_ui/css/smoothness/jquery-ui-1.8.24.custom.css" rel="stylesheet" />
-<script type="text/javascript" src="{source_http}javascript/jquery_ui/jquery-ui-1.10.3.custom.min.js"></script>
+<script type="text/javascript" src="{source_http}javascript/jquery_ui/jquery-ui-1.8.24.custom.min.js"></script>
