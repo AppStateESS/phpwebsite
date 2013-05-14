c9e5ce9df58597e619f06c28ea0dca9fd1dc5724
Stardog <mcnaney@gmail.com>
2013-05-14 14:06:00 -0400

    mod/layout/boost/install.php - Default theme for installation is now bootstrap.
    mod/miniadmin/templates/mini_admin.tpl
    - added a class to the mini admin to allow theming
    
    mod/users/class/Form.php
    - removed user menu error check. Easier solution.
    
    themes/default/
    - deleted

--- mod/layout/boost/install.php
+++ mod/layout/boost/install.php
@@ -6,22 +6,12 @@
 
 function layout_install(&$content, $branchInstall=FALSE)
 {
-    // Removed response install
-    /*
-    if (isset($_POST['process_layout'])) {
-    if (empty($_POST['page_title'])) {
-    $error  = dgettext('layout', 'Please enter a page title.');
-    } else {
-    $page_title = strip_tags($_POST['page_title']);
-    $default_theme = $_POST['theme'];
-    }
-    */
     $page_title = 'My phpWebSite';
-    $default_theme = 'simple';
+    $default_theme = 'bootstrap';
 
     if (!isset($error)) {
         $db = new PHPWS_DB('layout_config');
-        $db->addValue('default_theme', trim($default_theme));
+        $db->addValue('default_theme', $default_theme);
         $db->addValue('page_title', $page_title);
         $db->update();
         $content[] = dgettext('layout', 'Layout settings updated.');
