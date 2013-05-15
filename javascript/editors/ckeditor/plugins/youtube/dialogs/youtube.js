dc6f7bf290a78159d1ed7d3d7201f80608e01526
Stardog <mcnaney@gmail.com>
2013-05-15 16:13:23 -0400

    Added youtube button to editor.

---
+++ javascript/editors/ckeditor/plugins/youtube/dialogs/youtube.js
@@ -0,0 +1,28 @@
+﻿(function() {
+    CKEDITOR.dialog.add('youtube',
+            function(editor)
+            {
+                return{title: editor.lang.youtube.title, minWidth: CKEDITOR.env.ie && CKEDITOR.env.quirks ? 368 : 350, minHeight: 240,
+                    onShow: function() {
+                        this.getContentElement('general', 'content').getInputElement().setValue('');
+                    },
+                    onOk: function() {
+                        val = this.getContentElement('general', 'content').getInputElement().getValue();
+                        val = val.replace(/.*v\=(\w+).*/gi, '$1');
+                        console.log(val);
+                        var text = '<iframe title="YouTube video player" class="youtube-player" type="text/html" width="480" height="390" src="http://www.youtube.com/embed/'
+                                //+this.getContentElement('general','content').getInputElement().getValue()
+                                + val
+                                + '?rel=0" frameborder="0"></iframe>';
+                        this.getParentEditor().insertHtml(text);
+                    },
+                    contents: [{label: editor.lang.common.generalTab, id: 'general', elements:
+                                    [{type: 'html', id: 'pasteMsg', html: '<div style="white-space:normal;width:500px;"><img style="margin:5px auto;" src="'
+                                                    + CKEDITOR.getUrl(CKEDITOR.plugins.getPath('youtube')
+                                                    + 'images/youtube_large.png')
+                                                    + '"><br />' + editor.lang.youtube.pasteMsg
+                                                    + '</div>'}, {type: 'html', id: 'content', style: 'width:340px;height:90px', html: '<input size="100" style="' + 'border:1px solid black;' + 'background:white">', focus: function() {
+                                                this.getElement().focus()
+                                            }}]}]}
+            });
+})();
---
+++ javascript/editors/ckeditor/plugins/youtube/lang/uk.js
@@ -0,0 +1,9 @@
+CKEDITOR.plugins.setLang('vimeo', 'en',
+{
+  vimeo : 
+  {
+    title : "Embed Vimeo Video",
+    button : "Embed Video",
+    pasteMsg : "Please copy and paste the Youtube URL here"
+  }
+});
