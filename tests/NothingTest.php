148fc41c8361c038f27ad9ec295b66f21ee3f8ca
Jeff Tickle <jeff@jefftickle.com>
2013-06-04 10:53:37 -0400

    Fixed phpunit bootstrap, added some bogus tests

---
+++ tests/NothingTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Description
+ * @author Jeff Tickle <jtickle at tux dot appstate dot edu>
+ */
+
+class NothingTest extends PHPUnit_Framework_TestCase
+{
+    public function testNothing()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+    }
+}
+
+?>
