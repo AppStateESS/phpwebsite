5512dd112d772487dafc8fd32e7f7447b3427ee4
Jeff Tickle <jeff@jefftickle.com>
2013-06-12 17:27:55 -0400

    Working on Response system

--- Global/View.php
+++ Global/View.php
@@ -7,34 +7,8 @@
  * @package Global
  * @license http://opensource.org/licenses/lgpl-3.0.html
  */
-abstract class View extends Data {
-
-    /**
-     * Returns the data in this object as XML
-     * @return string
-     */
-    public function getXML()
-    {
-
-    }
-
-    /**
-     * Returns the data in this object in JSON encoded format
-     * @return string
-     */
-    public function getJSON()
-    {
-        return json_encode($this->getVars());
-    }
-
-    /**
-     * Returns the data in this object as HTML
-     * @return string
-     */
-    public function getHTML()
-    {
-
-    }
+abstract class View {
+    public abstract function render($data);
 }
 
 ?>
