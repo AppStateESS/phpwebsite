965e99175a2944845ac6f89d700c8e19d22ed51b
Stardog <mcnaney@gmail.com>
2013-07-18 10:49:42 -0400

    Incomplete: Calling format.js to allow manipulation by jquery dependent on window width.

---
+++ themes/bootstrap/javascript/format.js
@@ -0,0 +1,13 @@
+$(window).load(function() {
+    fbs = new formatBootstrap;
+});
+
+function formatBootstrap() {
+    var window_width = $(window).width();
+    $(window).resize(function() {
+        var window_width = $(window).width();
+    });
+}
+
+
+
--- themes/bootstrap/theme.php
+++ themes/bootstrap/theme.php
@@ -2,10 +2,10 @@
 
 javascript('bootstrap');
 Layout::plug(Layout::getPageTitle(TRUE), 'SITE_TITLE');
+Layout::addJSHeader('<script type="text/javascript" src="' . PHPWS_SOURCE_HTTP . 'themes/bootstrap/javascript/format.js"></script>');
 
 $key = Key::getCurrent();
-if(!is_null($key)) {
+if (!is_null($key)) {
     Layout::plug('active', 'CONTENT_PAGE');
 }
-
 ?>
