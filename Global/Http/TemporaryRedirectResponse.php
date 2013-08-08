97d8d223ffb847e66db74f6d92913e476d5df816
Jeff Tickle <jeff@jefftickle.com>
2013-08-08 12:25:49 -0400

    Separated Responses out into the different types of HTTP Responses

---
+++ Global/Http/TemporaryRedirectResponse.php
@@ -0,0 +1,18 @@
+<?php
+namespace Http;
+/**
+ * Use this to redirect to another URI temporarily.  Explicitly does alert the 
+ * user after a POST; if you don't want that, use SeeOtherResponse.
+ *
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ */
+abstract class TemporaryRedirectResponse extends RedirectResponse
+{
+    protected function getHttpResponseCode()
+    {
+        return 302;
+    }
+}
+
+?>
