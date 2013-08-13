4d78c4f288e4068ae9dde537185c90c2b179cb9d
Stardog <mcnaney@gmail.com>
2013-08-13 09:40:25 -0400

    double negative in render was not working

--- Global/Http/HtmlErrorView.php
+++ Global/Http/HtmlErrorView.php
@@ -6,36 +6,37 @@ namespace Http;
  * TODO: This should all be handled in templates that are 100% user-editable.
  * @author Jeff Tickle <jtickle at tux dot appstate dot edu>
  */
+class HtmlErrorView extends \Template {
 
-class HtmlErrorView extends \Template
-{
     protected $code;
 
     public function __construct(\Request $request, \Http\ErrorResponse $response)
     {
         $vars = array();
-        $vars['url']       = $request->getUrl();
-        $vars['method']    = $request->getMethod();
-        $vars['module']    = $request->getModule();
-        $vars['code']      = $response->getCode();
-        $vars['phrase']    = $response->getPhrase();
+        $vars['url'] = $request->getUrl();
+        $vars['method'] = $request->getMethod();
+        $vars['module'] = $request->getModule();
+        $vars['code'] = $response->getCode();
+        $vars['phrase'] = $response->getPhrase();
         $vars['backtrace'] = $response->getBacktrace();
         $vars['exception'] = $response->getException();
 
         $this->code = $vars['code'];
 
-        parent::__construct($vars, PHPWS_SOURCE_DIR . 'Global/Templates/Http/HtmlError.html', false);
+        parent::__construct($vars,
+                PHPWS_SOURCE_DIR . 'Global/Templates/Http/HtmlError.html', false);
     }
 
     public function render()
     {
         // If not defined, assume the most secure bet
-        if(!defined('DISPLAY_ERRORS') || !DISPLAY_ERRORS) {
+        if (defined('DISPLAY_ERRORS') && DISPLAY_ERRORS) {
             return parent::render();
         } else {
             return \Error::errorPage($this->code);
         }
     }
+
 }
 
 ?>
