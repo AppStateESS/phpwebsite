97a7deb3a3ee34b1f6e647be0b667f3b331f1f58
Stardog <mcnaney@gmail.com>
2013-08-26 14:02:42 -0400

    Fixed menu options disappearing on link hover Removing javascript for submenu.

--- themes/bootstrap/theme.php
+++ themes/bootstrap/theme.php
@@ -2,7 +2,7 @@
 
 javascript('bootstrap');
 Layout::plug(Layout::getPageTitle(TRUE), 'SITE_TITLE');
-Layout::addJSHeader('<script type="text/javascript" src="' . PHPWS_SOURCE_HTTP . 'themes/bootstrap/javascript/format.js"></script>');
+//Layout::addJSHeader('<script type="text/javascript" src="' . PHPWS_SOURCE_HTTP . 'themes/bootstrap/javascript/format.js"></script>');
 
 $key = Key::getCurrent();
 if (!is_null($key)) {
