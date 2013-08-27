185fec0d17e192ab92494cef5eaf46ae3de61933
Stardog <mcnaney@gmail.com>
2013-08-27 10:28:56 -0400

    updated to bootstrap 3

--- themes/bootstrap/theme.php
+++ themes/bootstrap/theme.php
@@ -1,8 +1,7 @@
 <?php
-
-javascript('bootstrap');
+javascript('jquery');
 Layout::plug(Layout::getPageTitle(TRUE), 'SITE_TITLE');
-//Layout::addJSHeader('<script type="text/javascript" src="' . PHPWS_SOURCE_HTTP . 'themes/bootstrap/javascript/format.js"></script>');
+Layout::addJSHeader('<script type="text/javascript" src="' . PHPWS_SOURCE_HTTP . 'themes/bootstrap/js/bootstrap.min.js"></script>');
 
 $key = Key::getCurrent();
 if (!is_null($key)) {
