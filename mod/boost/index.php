fd3161c0277610ed3fffac0e48bdc2a4de7940e2
Stardog <mcnaney@gmail.com>
2013-09-17 13:36:15 -0400

    Removed repeat content.

--- mod/boost/index.php
+++ mod/boost/index.php
@@ -1,9 +1,9 @@
 <?php
+
 /**
  * @version $Id$
  * @author Matthew McNaney <mcnaney at gmail dot com>
  */
-
 if (!defined('PHPWS_SOURCE_DIR')) {
     include '../../core/conf/404.html';
     exit();
@@ -34,16 +34,17 @@ $boostPanel = new PHPWS_Panel('boost');
 $boostPanel->enableSecure();
 Boost_Form::setTabs($boostPanel);
 
-$vars = array('action'=>'admin', 'tab'=>$boostPanel->getCurrentTab());
-$backToBoost = PHPWS_Text::secureLink(dgettext('boost', 'Return to Boost'), 'boost', $vars);
+$vars = array('action' => 'admin', 'tab' => $boostPanel->getCurrentTab());
+$backToBoost = PHPWS_Text::secureLink(dgettext('boost', 'Return to Boost'),
+                'boost', $vars);
 
-switch ($_REQUEST['action']){
+switch ($_REQUEST['action']) {
     case 'admin':
         $content[] = Boost_Form::listModules(Boost_Form::boostTab($boostPanel));
         break;
 
     case 'check':
-        $content[] =  $backToBoost . '<br />';
+        $content[] = $backToBoost . '<br />';
         $content[] = Boost_Action::checkupdate($_REQUEST['opmod']);
         break;
 
@@ -60,21 +61,22 @@ switch ($_REQUEST['action']){
     case 'install':
         $js = javascriptEnabled();
         if (!$js) {
-            $content[] =  $backToBoost . '<br />';
+            $content[] = $backToBoost . '<br />';
         }
 
         $result = Boost_Action::installModule($_REQUEST['opmod']);
         if (PHPWS_Error::isError($result)) {
             PHPWS_Error::log($result);
-            $content[] = dgettext('boost', 'An error occurred while installing this module.') .
-             ' ' . dgettext('boost', 'Please check your error logs.');
+            $content[] = dgettext('boost',
+                            'An error occurred while installing this module.') .
+                    ' ' . dgettext('boost', 'Please check your error logs.');
         } else {
             $content[] = $result;
         }
         break;
 
     case 'uninstall':
-        $content[] =  $backToBoost . '<br />';
+        $content[] = $backToBoost . '<br />';
         $content[] = '<br />';
 
         if (@$_REQUEST['confirm'] == $_REQUEST['opmod']) {
@@ -85,17 +87,17 @@ switch ($_REQUEST['action']){
         break;
 
     case 'update_core':
-        $content[] =  $backToBoost . '<br />';
+        $content[] = $backToBoost . '<br />';
         $content[] = Boost_Action::updateModule('core');
         break;
 
     case 'update':
         $js = javascriptEnabled();
         if (!$js) {
-            $content[] =  $backToBoost . '<br />';
+            $content[] = $backToBoost . '<br />';
         } else {
             $content[] = sprintf('<p style="text-align : center"><input type="button" onclick="closeWindow(); return false" value="%s" /></p>',
-            dgettext('boost', 'Close window'));
+                    dgettext('boost', 'Close window'));
         }
         $content[] = Boost_Action::updateModule($_REQUEST['opmod']);
         break;
@@ -111,14 +113,13 @@ switch ($_REQUEST['action']){
 }// End area switch
 
 if ($js) {
-    javascript('close_refresh', array('use_link'=>true));
+    javascript('close_refresh', array('use_link' => true));
     $content[] = sprintf('<p style="text-align : center"><input type="button" onclick="closeWindow(); return false" value="%s" /></p>',
-    dgettext('boost', 'Close window'));
+            dgettext('boost', 'Close window'));
     Layout::nakedDisplay(implode('', $content));
 } else {
     $boostPanel->setContent(implode('', $content));
     $finalContent = $boostPanel->display();
     Layout::add(PHPWS_ControlPanel::display($finalContent));
 }
-
 ?>
