b30606b14f5df139cd2dbccefe7112883d5971f7
Stardog <mcnaney@gmail.com>
2013-09-25 09:31:28 -0400

    Overwrite of old close file.

---
+++ mod/filecabinet/inc/close.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * This empty file overwrites the old close.
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @package Global
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ */
+?>
