459a890dc7e289f4b62c63d655c7e353f66a20c4
Stardog <mcnaney@gmail.com>
2013-09-26 13:51:30 -0400

    added index to bootstrap js call incase it needs to be called earlier

--- themes/bootstrap/theme.php
+++ themes/bootstrap/theme.php
@@ -1,7 +1,7 @@
 <?php
 javascript('jquery');
 Layout::plug(Layout::getPageTitle(TRUE), 'SITE_TITLE');
-Layout::addJSHeader('<script type="text/javascript" src="' . PHPWS_SOURCE_HTTP . 'themes/bootstrap/js/bootstrap.min.js"></script>');
+Layout::addJSHeader('<script type="text/javascript" src="' . PHPWS_SOURCE_HTTP . 'themes/bootstrap/js/bootstrap.min.js"></script>', 'bootstrap');
 
 $key = Key::getCurrent();
 if (!is_null($key)) {
