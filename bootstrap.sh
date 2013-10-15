fb8a732756f302ed0db35f59c616977daa3a7dec
Jeff Tickle <jeff@jefftickle.com>
2013-10-15 09:27:05 -0400

    Had a vision last night of how to make Vagrant work with phpWS through clever use of mount --bind

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,11 @@ DBUSER=phpwebsite
 DBPASS=phpwebsite
 DBNAME=phpwebsite
 
+CONFIG=/var/phpws/config
+FILES=/var/phpws/files
+IMAGES=/var/phpws/images
+LOGS=/var/phpws/logs
+
 echo "Installing Packages..."
 yum -y install httpd php-cli php-pgsql php-pecl-xdebug php-pdo php php-mbstring php-common php-mysql php-soap php-gd php-xml php-pecl-apc mysql-server mysql postgresql-server postgresql > /dev/null 2>&1
 
@@ -15,8 +20,8 @@ yum -y install httpd php-cli php-pgsql php-pecl-xdebug php-pdo php php-mbstring 
 echo "Setting up MySQL..."
 service mysqld start > /dev/null 2>&1
 mysql -u root <<MySQL
-CREATE DATABASE '$DBNAME';
-GRANT ALL ON '$DBNAME'.* TO '$DBUSER'@localhost IDENTIFIED BY '$DBPASS';
+CREATE DATABASE $DBNAME;
+GRANT ALL ON $DBNAME.* TO $DBUSER@localhost IDENTIFIED BY '$DBPASS';
 MySQL
 
 # PostgreSQL
@@ -34,6 +39,14 @@ ln -sf /vagrant /var/www/html
 echo "Configuring Firewall..."
 iptables -I INPUT 5 -p tcp -m state --state=NEW --dport 80 -j ACCEPT
 
+echo "Establishing Writable Mounts..."
+mkdir -p "$CONFIG/core" "$FILES" "$IMAGES" "$LOGS"
+chown -R apache:apache "$CONFIG" "$FILES" "$IMAGES" "$LOGS"
+mount --bind "$CONFIG" /vagrant/config
+mount --bind "$FILES" /vagrant/files
+mount --bind "$IMAGES" /vagrant/images
+mount --bind "$LOGS" /vagrant/logs
+
 # Helpful Information
 cat << USAGE
 ===============================================================================
@@ -50,6 +63,12 @@ cat << USAGE
           Database User: phpwebsite
       Database Password: phpwebsite
      Host Specification: localhost
+
+ Please note that remote paths have been mounted on top of config, files,
+ images, and logs.  Anything created within Vagrant in these directories will
+ not appear locally, and anything created locally will not appear in Vagrant.
+ Additionally, all server-side contents of these directories WILL BE LOST after
+ you vagrant destroy.
 
  So head on over to http://localhost:7479 and get started!
 
