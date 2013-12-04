ce4a0c2fe7d2a6d76db23fc6af19f7661d4d62a0
Stardog <mcnaney@gmail.com>
2013-12-04 10:59:25 -0500

    setValue now checks the format. Important to conform with HTML5 standards.

--- Global/Form/Input/Date.php
+++ Global/Form/Input/Date.php
@@ -10,7 +10,12 @@ namespace Form\Input;
  * @license http://opensource.org/licenses/lgpl-3.0.html
  */
 class Date extends Text {
-
+    public function setValue($value) {
+        if (!preg_match('/\d{4}-\d{2}-\d{2}/', $value)) {
+            throw new \Exception(t('Date format is YYYY-MM-DD: %s', $value));
+        }
+        parent::setValue($value);
+    }
 }
 
 ?>
