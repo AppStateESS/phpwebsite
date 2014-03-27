88f0733506f5d6d14c97622a3abf65e626e46b34
Matthew McNaney <mcnaney@gmail.com>
2014-03-27 09:55:00 -0400

    Remove deprecated modules.

--- mod/clipboard/conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @version $Id$
- * @author Matthew McNaney <mcnaney at gmail dot com>
- */
-
-//define('CLIPBOARD_DROP_LINK', dgettext('clipboard', 'Drop'));
-define('CLIPBOARD_DROP_LINK', Icon::show('delete', dgettext('clipboard', 'Drop')));
-?>
