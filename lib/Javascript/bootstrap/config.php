2402a0816cd0c9fab9a48d85eda051ad834557ec
Matthew McNaney <mcnaney@gmail.com>
2014-04-09 13:56:43 -0400

    New Javascript class

---
+++ lib/Javascript/bootstrap/config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ *
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @package Global
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ */
+$dependencies = ['jquery'];
+$path = 'lib/Javascript/bootstrap/bootstrap.min.js';
+$compressed = true;
+?>
---
+++ lib/Javascript/jquery_ui/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ *
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @package Global
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+$dependencies = ['jquery'];
+$path = 'lib/Javascript/jquery_ui/js/jquery-ui-1.10.4.min.js';
+$compressed = true;
+$include_css = ['lib/Javascript/jquery_ui/css/smoothness/jquery-ui-1.10.4.custom.min.css'];
+?>
