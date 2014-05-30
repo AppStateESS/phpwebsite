5ab1040ce27cd38d1b2e150f25e610eb75d4f1fa
Jeremy Booker <jeremy.booker@gmail.com>
2014-05-30 09:52:17 -0400

    Updated datepciker javascript to use newer version of bootstrap-datepicker from http://github.com/eternicode/bootstrap-datepicker

--- javascript/datepicker/default.php
+++ javascript/datepicker/default.php
@@ -9,13 +9,23 @@ function loadDatepicker() {
     javascript('jquery');
     $source_http = PHPWS_SOURCE_HTTP;
     $script = <<<EOF
-<script type="text/javascript">{$source_http}javascript/datepicker/js/bootstrap-datepicker.js</script>
+<script type="text/javascript" src="{$source_http}javascript/datepicker/js/bootstrap-datepicker.js"></script>
 <script type="text/javascript">
 $(window).load(function() {
-    $('.datepicker').datepicker();
+    /*
+    $('.datepicker').datepicker().on('changeDate', function(ev){
+            $('.datepicker').datepicker('hide');
+        });
+    */
+
+    $('.datepicker').datepicker({
+        autoclose: true
+    })
+
 });
 </script>
 EOF;
 \Layout::addJSHeader($script);
+\Layout::addStyle('datepicker', '../../../javascript/datepicker/css/datepicker.css');
 }
 ?>
