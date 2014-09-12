f9b5c1b11d91a3764cea870b4808045bebd8cf64
Matthew McNaney <mcnaney@gmail.com>
2014-09-12 11:15:51 -0400

    Date accepts null value

--- Global/Form/Input/Date.php
+++ Global/Form/Input/Date.php
@@ -10,11 +10,66 @@ namespace Form\Input;
  * @license http://opensource.org/licenses/lgpl-3.0.html
  */
 class Date extends Text {
+
+    private $min;
+
+    private $max;
+
+    protected $step;
+
+    /**
+     *
+     * @param string $value Date in format YYYY-MM-DD
+     * @return type
+     * @throws \Exception
+     */
     public function setValue($value) {
+        if (empty($value)) {
+            return;
+        }
+        if (is_int($value)) {
+            $value = strftime('%Y-%m-%d', strtotime($value));
+        }
+
         if (!preg_match('/\d{4}-\d{2}-\d{2}/', $value)) {
             throw new \Exception(t('Date format is YYYY-MM-DD: %s', $value));
         }
         parent::setValue($value);
+    }
+
+    /**
+     *
+     * @param string $min Date in format YYYY-MM-DD
+     * @throws \Exception
+     */
+    public function setMin($min)
+    {
+        if (!preg_match('/\d{4}-\d{2}-\d{2}/', $min)) {
+            throw new \Exception(t('Date format is YYYY-MM-DD: %s', $min));
+        }
+        $this->min = $min;
+    }
+
+    /**
+     *
+     * @param string $max Date in format YYYY-MM-DD
+     * @throws \Exception
+     */
+    public function setMax($max)
+    {
+        if (!preg_match('/\d{4}-\d{2}-\d{2}/', $max)) {
+            throw new \Exception(t('Date format is YYYY-MM-DD: %s', $max));
+        }
+        $this->max = $max;
+    }
+
+    /**
+     *
+     * @param integer $step
+     */
+    public function setStep($step)
+    {
+        $this->step = (int) $step;
     }
 }
 
