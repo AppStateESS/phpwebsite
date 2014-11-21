e5b6f2edd16d23006b46942b9d2c9e527964973a
Matthew McNaney <mcnaney@gmail.com>
2014-11-21 11:00:18 -0500

    Stub file so PHPWS_File works with autoloader

---
+++ core/class/PHPWS_File.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ *
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ */
+require_once PHPWS_SOURCE_DIR . 'core/class/File.php';
+
+?>
