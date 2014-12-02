168ca3cbd4e93b2ec493de5b2377946a7586ada1
Matthew McNaney <mcnaney@gmail.com>
2014-12-02 09:37:24 -0500

    Fixed #198
    Ignoring double negative in report.

--- mod/block/inc/close.php
+++ mod/block/inc/close.php
@@ -9,12 +9,17 @@ if (Current_User::allow('block')) {
 
     $key = Key::getCurrent();
     if (Key::checkKey($key) && javascriptEnabled()) {
-        $js_address = PHPWS_SOURCE_HTTP . 'mod/block/javascript/addblock/script.js';
         javascript('jquery');
-        javascript('jquery_ui');
+        javascript('ckeditor');
+        $js_address = PHPWS_SOURCE_HTTP . 'mod/block/javascript/addblock/script.js';
         Layout::addJSHeader('<script src="'.$js_address.'" type="text/javascript"></script>', 'addblock');
 
-        Layout::add('<div id="block-form-dialog" style="display : none"></div>');
+        $modal = new \Modal('block-form-modal', '<div id="block-form-dialog"></div>', 'Add block here');
+        $modal->sizeLarge();
+        $save_button = '<button class="btn btn-success" id="save-block">Save</button>';
+        $modal->addButton($save_button);
+        Layout::add((string)$modal);
+
         MiniAdmin::add('block', '<a style="cursor:pointer" data-auth-key="' . Current_User::getAuthKey() .
                 '" data-key-id="'.$key->id.'" id="add-block"><i class="fa fa-plus"></i> Add block here</a>');
     }
--- mod/block/javascript/addblock/script.js
+++ mod/block/javascript/addblock/script.js
@@ -3,39 +3,23 @@
  * and open the template in the editor.
  */
 
-$(window).ready(function() {
+$(window).load(function () {
     var editor_called = false;
-    var dblock = $('#block-form-dialog');
-    dblock.dialog(
-            {
-                title : 'Add block to this page',
-                autoOpen: false,
-                width: 650,
-                buttons: [
-                    {
-                        text: 'Save',
-                        click: function() {
-                            $('#block-form').submit();
-                        }
-                    },
-                    {
-                        text: 'Cancel',
-                        click: function() {
-                            dblock.dialog('close');
-                        }
-                    }
-                ]
-            });
-    $('#add-block').click(function() {
+    $('#save-block').click(function () {
+        $('#block-form').submit();
+    });
+
+    $('#add-block').click(function () {
         link = 'index.php?module=block&action=js_block_edit&key_id=' +
                 $(this).data('key-id') + '&authkey=' + $(this).data('auth-key');
-        $.get(link, function(data) {
-            dblock.html(data);
+        $.get(link, function (data) {
+            $('#block-form-modal .modal-body').html(data);
+            $('#block-form-modal').modal('show');
             if (!editor_called) {
                 CKEDITOR.replace('block-form_block_content');
                 editor_called = true;
             }
-            dblock.dialog('open');
+
         });
     });
 });
