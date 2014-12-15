ee5cf2160f6869a4b1313c2d45dfc7f1c412c095
Jeremy Booker <jeremy.booker@gmail.com>
2014-12-15 10:45:02 -0500

    Update datepicker js.

---
+++ javascript/datepicker/head.js
@@ -0,0 +1,10 @@
+<link href="{source_http}javascript/datepicker/css/datepicker.css" rel="stylesheet" />
+<script type="text/javascript" src="{source_http}javascript/datepicker/js/bootstrap-datepicker.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+    $('.datepicker').datepicker({
+        autoclose: true
+    })
+
+});
+</script>
