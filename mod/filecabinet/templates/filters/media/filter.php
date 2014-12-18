2fab2df4abd7e49ea9e56c3bd74e2bf8159f763f
Stardog <mcnaney@gmail.com>
2014-12-18 12:48:30 -0500

    MediaElement implemented

--- mod/filecabinet/templates/filters/media/filter.php
+++ mod/filecabinet/templates/filters/media/filter.php
@@ -4,19 +4,9 @@
  * @author Matthew McNaney <mcnaney at gmail dot com>
  */
 
-$floatbar       = false;
-$use_thumbnails = true;
 
 if ($is_video) {
-    if ($floatbar) {
-        $tpl['DISPLAYHEIGHT'] = $tpl['HEIGHT'];
-    } else {
-        $tpl['HEIGHT'] = $tpl['HEIGHT'] + 20;
-    }
-
-    if ($use_thumbnails) {
         $tpl['THUMBNAIL'] = $thumbnail;
-    }
 } else {
     $tpl['HEIGHT'] = 20;
     $tpl['WIDTH'] = 260;
