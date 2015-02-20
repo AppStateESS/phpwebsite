6ac5b2ccddee0810eeb088909c9b09c86301e430
Stardog <mcnaney@gmail.com>
2015-02-20 14:59:47 -0500

    Fixed date format on pager listing.
    Year only showed first 2 digits of year.

--- mod/blog/conf/config.php
+++ mod/blog/conf/config.php
@@ -6,7 +6,7 @@
  */
 
 define('BLOG_VIEW_DATE_FORMAT', '%l:%M%P on %A, %B %e %Y');
-define('BLOG_PAGER_DATE_FORMAT', '%R - %e %b, %C');
+define('BLOG_PAGER_DATE_FORMAT', '%R - %e %b, %Y');
 define('MAX_BLOG_CACHE_PAGES', 3);
 
 ?>
