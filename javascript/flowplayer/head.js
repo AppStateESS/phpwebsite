fe9258afa267f93e5f10d9f874731b7538288781
Stardog <mcnaney@gmail.com>
2015-04-10 13:55:38 -0400

    Calling flowplayer on window load

--- javascript/flowplayer/head.js
+++ javascript/flowplayer/head.js
@@ -1,1 +1,6 @@
 <script type="text/javascript" src="{source_http}javascript/flowplayer/flowplayer-3.2.13.min.js"></script>
+<script type="text/javascript">
+$(window).load(function () {
+   $(".flowplayer").flowplayer();
+});
+</script>
