5380d6d67443e0f097a1d787a7aad6c0c101bf38
Stardog <mcnaney@gmail.com>
2015-04-27 13:44:56 -0400

    Updated jquery_ui

--- javascript/jquery_ui/head.js
+++ javascript/jquery_ui/head.js
@@ -1,2 +1,2 @@
-<link type="text/css" href="{source_http}javascript/jquery_ui/css/smoothness/jquery-ui-1.10.3.custom.min.css" rel="stylesheet" />
-<script type="text/javascript" src="{source_http}javascript/jquery_ui/jquery-ui-1.10.3.custom.min.js"></script>
+<link type="text/css" href="{source_http}javascript/jquery_ui/jquery-ui.min.css" rel="stylesheet" />
+<script type="text/javascript" src="{source_http}javascript/jquery_ui/jquery-ui.min.js"></script>
