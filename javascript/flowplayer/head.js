de16107eb42cd91787300daf4ff358b7d68ad2bd
Stardog <mcnaney@gmail.com>
2015-06-09 09:21:14 -0400

    Prevent flowplayer javascript error.

--- javascript/flowplayer/head.js
+++ javascript/flowplayer/head.js
@@ -1,6 +1,8 @@
 <script type="text/javascript" src="{source_http}javascript/flowplayer/flowplayer-3.2.13.min.js"></script>
 <script type="text/javascript">
 $(window).load(function () {
-   $(".flowplayer").flowplayer();
+   if ($('.flowplayer').length) {
+        $(".flowplayer").flowplayer();
+   }
 });
 </script>
