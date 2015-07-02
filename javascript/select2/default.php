6b5a8d0a60dc8801db10095c93291faf11e45ac9
Stardog <mcnaney@gmail.com>
2015-07-02 09:40:46 -0400

    Updated select2 script from https://select2.github.io/

--- javascript/select2/default.php
+++ javascript/select2/default.php
@@ -10,10 +10,7 @@ function select2Script()
 {
     $script = '<script type="text/javascript" src="' . PHPWS_SOURCE_HTTP . 'javascript/select2/select2.min.js"></script>';
     \Layout::addJSHeader($script);
-    \Layout::addToStyleList('javascript/select2/select2.css');
+    \Layout::addToStyleList('javascript/select2/select2.min.css');
     \Layout::addToStyleList('javascript/select2/select2-bootstrap.css');
 }
 select2Script();
-
-
-?>
