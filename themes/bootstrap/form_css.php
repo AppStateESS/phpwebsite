64da4a49ec23e016f71c81bc56435a1943af7084
Stardog <mcnaney@gmail.com>
2015-08-03 17:01:40 -0400

    Restored form_css.php

---
+++ themes/bootstrap/form_css.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ *
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ * @package Global
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+$input_classes = array('form-control');
+$label_classes = array('control-label');
+$group_classes = array('form-group');
+$button_classes = array('btn', 'btn-default');
+?>
