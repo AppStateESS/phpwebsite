f632d8a15b9ef86f0b786fbc80e0f136f2409543
Stardog <mcnaney@gmail.com>
2015-10-12 13:10:37 -0400

    Added some indexes.

--- javascript/react/default.php
+++ javascript/react/default.php
@@ -4,23 +4,19 @@
  * @license http://opensource.org/licenses/lgpl-3.0.html
  * @author Matthew McNaney <mcnaney at gmail dot com>
  */
-loadReact($data);
-function loadReact($data)
-{
-    javascript('jquery');
-    $home_http = PHPWS_SOURCE_HTTP;
-    if (isset($data['development']) && $data['development']) {
-        if (isset($data['addons']) && $data['addons']) {
-            Layout::addJSHeader("<script src='{$home_http}javascript/react/build/react-with-addons.js'></script>");
-        } else {
-            Layout::addJSHeader("<script src='{$home_http}javascript/react/build/react.js'></script>");
-        }
-        Layout::addJSHeader("<script src='{$home_http}javascript/react/build/JSXTransformer.js'></script>");
+javascript('jquery');
+$home_http = PHPWS_SOURCE_HTTP;
+if (isset($data['development']) && $data['development']) {
+    if (isset($data['addons']) && $data['addons']) {
+        Layout::addJSHeader("<script src='{$home_http}javascript/react/build/react-with-addons.js'></script>", 'drwao');
     } else {
-        if (isset($data['addons']) && $data['addons']) {
-            Layout::addJSHeader("<script src='{$home_http}javascript/react/build/react-with-addons.min.js'></script>");
-        } else {
-            Layout::addJSHeader("<script src='{$home_http}javascript/react/build/react.min.js'></script>");
-        }
+        Layout::addJSHeader("<script src='{$home_http}javascript/react/build/react.js'></script>", 'drwoao');
+    }
+    Layout::addJSHeader("<script src='{$home_http}javascript/react/build/JSXTransformer.js'></script>", 'jsxtrans');
+} else {
+    if (isset($data['addons']) && $data['addons']) {
+        Layout::addJSHeader("<script src='{$home_http}javascript/react/build/react-with-addons.min.js'></script>", 'rwao');
+    } else {
+        Layout::addJSHeader("<script src='{$home_http}javascript/react/build/react.min.js'></script>", 'rwoao');
     }
 }
