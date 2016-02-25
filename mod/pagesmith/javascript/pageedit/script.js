d3c1ab1e54b3bcdf50ebe01e3f7fa55a86764454
Matthew McNaney <matt@ip-152010142161.ess.appstate.edu>
2016-02-25 15:52:46 -0500

    Fixed #255
    Tommy's modal fix.

--- mod/pagesmith/javascript/pageedit/script.js
+++ mod/pagesmith/javascript/pageedit/script.js
@@ -66,7 +66,7 @@ function enforceFocus()
 {
     $.fn.modal.Constructor.prototype.enforceFocus = function() {
         modal_this = this
-        $(document).on('focusin.modal', function(e) {
+        $(document).on('shown.bs.modal', function(e) {
             if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length
                     && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select')
                     && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
