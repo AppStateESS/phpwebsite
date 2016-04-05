dab5762db39bd49b96755a163e0d46c9931aef89
Matthew McNaney <mcnaney@gmail.com>
2016-04-05 14:31:14 -0400

    Facade from src/phpws2

--- Global/Form/Input/Button.php
+++ Global/Form/Input/Button.php
@@ -1,21 +1,3 @@
 <?php
 
-namespace Form\Input;
-
-/**
- *
- * @author Matthew McNaney <mcnaney at gmail dot com>
- * @package Global
- * @subpackage Form
- * @subpackage Input
- * @license http://opensource.org/licenses/lgpl-3.0.html
- */
-class Button extends \Form\Input {
-
-    /**
-     * @see Form\Base::$label_location
-     * @var integer
-     */
-    protected $label_location = 0;
-
-}
+require_once PHPWS_SOURCE_DIR . 'src/phpws2/Form/Input/' . basename(__FILE__);
