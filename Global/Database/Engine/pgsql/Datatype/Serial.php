295970d500cd8baf5703840bfe879cbf12aa7627
Matthew McNaney <mcnaney@gmail.com>
2016-04-05 14:27:19 -0400

    Facade from src/phpws2

--- Global/Database/Engine/pgsql/Datatype/Serial.php
+++ Global/Database/Engine/pgsql/Datatype/Serial.php
@@ -1,33 +1,3 @@
 <?php
 
-namespace Database\Engine\pgsql\Datatype;
-
-/*
- * See docs/AUTHORS and docs/COPYRIGHT for relevant info.
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * @author Matthew McNaney <mcnaney at gmail dot com>
- *
- * @license http://opensource.org/licenses/lgpl-3.0.html
- */
-
-class Serial extends \Database\Datatype\Integer {
-
-    protected $is_number = true;
-    protected $default = null;
-
-    public function __construct(\Database\Table $table, $name)
-    {
-        parent::__construct($table, $name);
-        $this->default = null;
-    }
-}
-
-?>
+require_once PHPWS_SOURCE_DIR . 'src/phpws2/Database/Engine/pgsql/Datatype/' . basename(__FILE__);
