d19248dc15dffcf1a3835a527c24e22218355a7e
Matthew McNaney <mcnaney@gmail.com>
2016-04-05 14:24:02 -0400

    Facade from src/phpws2

--- Global/Database/Engine.php
+++ Global/Database/Engine.php
@@ -1,22 +1,3 @@
 <?php
 
-namespace Database;
-
-/**
- * Description of Engine
- *
- * @author matt
- */
-abstract class Engine {
-
-    protected $db;
-
-    abstract public function addPrimaryIndexId(\Database\Table $table);
-    abstract public function getDBType();
-    abstract public function getDelimiter();
-
-    public function __construct(\DB $db)
-    {
-        $this->db = $db;
-    }
-}
+require_once PHPWS_SOURCE_DIR . 'src/phpws2/Database/' . basename(__FILE__);
