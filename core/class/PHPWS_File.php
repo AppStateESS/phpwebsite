b8e9b12f95740acb5b87a4fbe2b6d7c0e58b93c5
Matthew McNaney <mcnaney@gmail.com>
2016-04-05 12:47:23 -0400

    Stubbed out to load each files src/phpws/ counterpart.

--- core/class/PHPWS_File.php
+++ core/class/PHPWS_File.php
@@ -1,10 +1,3 @@
 <?php
 
-/**
- *
- * @author Matthew McNaney <mcnaney at gmail dot com>
- * @license http://opensource.org/licenses/lgpl-3.0.html
- */
-require_once PHPWS_SOURCE_DIR . 'core/class/File.php';
-
-?>
+require_once PHPWS_SOURCE_DIR . 'src/phpws/PHPWS_File.php';
--- core/class/Time.php
+++ core/class/Time.php
@@ -1,13 +1,3 @@
 <?php
 
-/**
- * The PHPWS_Time class is mainly for the parsing of timestamps
- * into and out of UTC time.
- *
- * @author Matthew McNaney <mcnaney at gmail dot com>
- * @version $Id$
- */
-
-require_once PHPWS_SOURCE_DIR . 'core/class/PHPWS_Time.php';
-
-?>
+require_once PHPWS_SOURCE_DIR . 'src/phpws/PHPWS_Time.php';
