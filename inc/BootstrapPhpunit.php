25ebf2d23d8f187b28756937be6f3548acc4d90b
Matthew McNaney <mcnaney@gmail.com>
2016-04-05 13:43:59 -0400

    Updated for new locations.

--- inc/BootstrapPhpunit.php
+++ inc/BootstrapPhpunit.php
@@ -5,8 +5,7 @@ $val = ini_get('include_path');
 define('PHPWS_SOURCE_DIR', __DIR__ . '/../');
 ini_set('display_errors', 'on');
 ini_set('error_reporting', E_ALL);
-require_once PHPWS_SOURCE_DIR . 'Global/Functions.php';
 
-require_once PHPWS_SOURCE_DIR . 'Global/Implementations.php';
+require_once PHPWS_SOURCE_DIR . 'src/phpws2/Implementations.php';
 
 ini_set('include_path', $val);
