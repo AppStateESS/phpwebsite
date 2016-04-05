0b684462decff781bc298d469c2c9a2c97eb91c3
Matthew McNaney <mcnaney@gmail.com>
2016-04-05 13:28:53 -0400

    stubbed old global files for compatibility.

--- Global/Implementations.php
+++ Global/Implementations.php
@@ -1,12 +1,4 @@
 <?php
 
-/*
- *
- * @author Matthew McNaney <mcnaney at gmail dot com>
- * @license http://opensource.org/licenses/lgpl-3.0.html
- */
+require_once PHPWS_SOURCE_DIR . 'src/phpws2/' . basename(__FILE__);
 
-interface SettingDefaults {
-
-    public function getSettingDefaults();
-}
--- Global/Message.php
+++ Global/Message.php
@@ -1,33 +1,3 @@
 <?php
 
-/**
- *
- * @author Matthew McNaney <mcnaney at gmail dot com>
- * @license http://opensource.org/licenses/lgpl-3.0.html
- */
-class Message {
-
-    public static function set($message)
-    {
-        \Session::getInstance()->message = $message;
-    }
-
-    public static function get()
-    {
-        if (isset(\Session::getInstance()->message)) {
-            $message = \Session::getInstance()->message;
-        } else {
-            $message = null;
-        }
-        unset(\Session::getInstance()->message);
-        return $message;
-    }
-
-    public static function forward($message, $url)
-    {
-        self::set($message);
-        header('location: ' . $url);
-        exit();
-    }
-
-}
+require_once PHPWS_SOURCE_DIR . 'src/phpws2/' . basename(__FILE__);
