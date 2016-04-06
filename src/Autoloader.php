c7610299136017a10a697af2c7aaa0b867f0f919
Matthew McNaney <mcnaney@gmail.com>
2016-04-06 09:22:41 -0400

    Separated into to two different autoloaders: old and new.

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -1,25 +1,43 @@
 <?php
 
 /*
-spl_autoload_register(function($class){
-    $directory = PHPWS_SOURCE_DIR . 'src/';
-    $class_name = str_replace('\\', '/', $class);
-    $core_class_file = $directory . 'phpws/' . $class_name . '.php';
-    $global_class_file = $directory . 'phpws2/' . $class_name . '.php';
-    echo $core_class_file;
-    echo '<br>';
-    echo $global_class_file;
-    if (is_file($core_class_file)) {
-        require_once $core_class_file;
-    } else if (is_file($global_class_file)) {
-        require_once $global_class_file;
+  spl_autoload_register(function($class){
+  $directory = PHPWS_SOURCE_DIR . 'src/';
+  $class_name = str_replace('\\', '/', $class);
+  $core_class_file = $directory . 'phpws/' . $class_name . '.php';
+  $global_class_file = $directory . 'phpws2/' . $class_name . '.php';
+  echo $core_class_file;
+  echo '<br>';
+  echo $global_class_file;
+  if (is_file($core_class_file)) {
+  require_once $core_class_file;
+  } else if (is_file($global_class_file)) {
+  require_once $global_class_file;
+  }
+  });
+ */
+
+spl_autoload_register('phpwsOldLoad');
+spl_autoload_register('phpwsNewLoad');
+
+function phpwsNewLoad($class_name)
+{
+    static $files_found = array();
+
+    if (isset($files_found[$class_name])) {
+        return;
     }
-});
-*/
+    $class_name = preg_replace('@^/|/$@', '', str_replace('\\', '/', $class_name));
 
-spl_autoload_register('phpwsAutoload');
+    $base_dir = PHPWS_SOURCE_DIR . 'src/';
 
-function phpwsAutoload($class_name)
+    $class_path = $base_dir . $class_name . '.php';
+    if (is_file($class_path)) {
+        require_once $class_path;
+    }
+}
+
+function phpwsOldload($class_name)
 {
     // stores previously found requires
     static $files_found = array();
@@ -29,13 +47,10 @@ function phpwsAutoload($class_name)
         require_once $files_found[$class_name];
         return;
     }
-    $class_name = preg_replace('@^/|/$@', '',
-            str_replace('\\', '/', $class_name));
-    $new_mod_file = PHPWS_SOURCE_DIR . preg_replace('|^([^/]+)/([\w/]+)|',
-                    'mod/\\1/class/\\2.php', $class_name);
+    $class_name = preg_replace('@^/|/$@', '', str_replace('\\', '/', $class_name));
+    $new_mod_file = PHPWS_SOURCE_DIR . preg_replace('|^([^/]+)/([\w/]+)|', 'mod/\\1/class/\\2.php', $class_name);
     $global_file = PHPWS_SOURCE_DIR . 'Global/' . $class_name . '.php';
     $class_file = PHPWS_SOURCE_DIR . 'core/class/' . $class_name . '.php';
-
     if (is_file($new_mod_file)) {
         $files_found[$class_name] = $new_mod_file;
         require_once $new_mod_file;
@@ -48,15 +63,6 @@ function phpwsAutoload($class_name)
     } elseif (isset($_REQUEST['module'])) {
         $module = preg_replace('/\W/', '', $_REQUEST['module']);
 
-        /**
-         * If the module name is the first token in the class name,
-         * then remove it. Example:
-         * Class name: 'Intern/Command/ShowAddInternship'
-         * Should map to file: 'phpwebsite/mod/intern/class/Command/ShowAddInternship.php'
-         *
-         * In this example, we need to remove 'Intern/' from the class name in order to generate
-         * the correct file name.
-         */
         if (preg_match("/^$module\//i", $class_name)) {
             $class_name = preg_replace("/^$module\//i", '', $class_name);
         }
