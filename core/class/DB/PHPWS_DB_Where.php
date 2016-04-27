df737df93182cd005b4c2ce8458c3934d889441f
Jeremy Booker <jeremy.booker@gmail.com>
2016-04-27 11:20:02 -0400

    Add facade class for namespacing in core/class/DB/PHPWS_DB_Where

--- core/class/DB/PHPWS_DB_Where.php
+++ core/class/DB/PHPWS_DB_Where.php
@@ -1,3 +1,5 @@
 <?php
 
-require_once PHPWS_SOURCE_DIR . 'src/phpws/DB/PHPWS_DB_Where.php';
+require_once PHPWS_SOURCE_DIR . 'src/phpws/src/DB/PHPWS_DB_Where.php';
+
+class PHPWS_DB_Where extends \phpws\DB\PHPWS_DB_Where {}
