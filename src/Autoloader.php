3e38af17f19cb4d61d33cf2dd9c0252e0542a2f9
Matthew McNaney <mcnaney@gmail.com>
2016-04-28 16:22:01 -0400

    Having an autoloading class is no longer required.

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -2,18 +2,21 @@
 
 loadAutoloaders();
 
-function loadAutoloaders() {
-    
+function loadAutoloaders()
+{
+
     include PHPWS_SOURCE_DIR . 'src/config/Autoloaders.php';
     if (!isset($autoloaders)) {
         throw new \Exception('Missing autoloader configuration file.');
     }
-    
+
     if (!is_array($autoloaders) || empty($autoloaders)) {
         throw new \Exception('Autoloader configuration not formatted correctly.');
     }
     foreach ($autoloaders as $file => $autoload_function) {
         require_once PHPWS_SOURCE_DIR . 'Autoloaders/' . $file . '.php';
-        spl_autoload_register($autoload_function);
+        if (!empty($autoload_function)) {
+            spl_autoload_register($autoload_function);
+        }
     }
 }
