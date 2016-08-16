bd2b26b0327aa2498e4bb91d55fd0b9a64a0e8c9
Matthew McNaney <mcnaney@gmail.com>
2016-08-16 15:17:08 -0400

    Swiftmailer and getid3 moved to root.
    Multimedia updated accordingly.

--- mod/filecabinet/class/Multimedia.php
+++ mod/filecabinet/class/Multimedia.php
@@ -62,7 +62,6 @@ class PHPWS_Multimedia extends File_Common
 
     public function getID3()
     {
-        require_once PHPWS_SOURCE_DIR . 'lib/vendor/autoload.php';
         $getID3 = new getID3;
 
         // File to get info from
