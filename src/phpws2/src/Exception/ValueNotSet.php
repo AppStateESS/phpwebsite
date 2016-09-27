e82cdfd6ada19551b0e9822e3ba156fb8de0aaac
Matthew McNaney <mcnaney@gmail.com>
2016-09-27 09:24:28 -0400

    Adding common exception type
    - Plain Exception used often when we could leverage try/catch with
    common exception types. ValueNotSet one such new exception.

---
+++ src/phpws2/src/Exception/ValueNotSet.php
@@ -0,0 +1,20 @@
+<?php
+namespace \phpws2\Exception;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ValueNotSet
+ *
+ * @author matt
+ */
+class ValueNotSet extends \Exception
+{
+    public function __construct()
+    {
+        parent::__construct('Value not set', 1);
+    }
+}
