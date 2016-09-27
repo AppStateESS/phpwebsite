ea4f718eefed4c23e6489462857ef748f5613d5b
Matthew McNaney <mcnaney@gmail.com>
2016-09-27 09:43:19 -0400

    Removed slash

--- src/phpws2/src/Exception/ValueNotSet.php
+++ src/phpws2/src/Exception/ValueNotSet.php
@@ -1,5 +1,5 @@
 <?php
-namespace \phpws2\Exception;
+namespace phpws2\Exception;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
