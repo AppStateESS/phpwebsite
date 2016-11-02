01124af65295c09d4db7d6ded08721656198d36d
Jeremy Booker <jeremy.booker@gmail.com>
2016-11-02 16:29:22 -0400

    Fix namespace issue in Pulse module.

--- mod/pulse/Module.php
+++ mod/pulse/Module.php
@@ -6,7 +6,7 @@ namespace pulse;
  * @license http://opensource.org/licenses/lgpl-3.0.html
  * @author Matthew McNaney <mcnaney at gmail dot com>
  */
-class Module extends \Module implements \SettingDefaults
+class Module extends \Canopy\Module implements \Canopy\SettingDefaults
 {
 
     public function __construct()
@@ -16,7 +16,7 @@ class Module extends \Module implements \SettingDefaults
         $this->setProperName('Pulse');
     }
 
-    public function getController(\Request $request)
+    public function getController(\Canopy\Request $request)
     {
         $cmd = $request->shiftCommand();
         if ($cmd == 'admin' && \Current_User::isDeity()) {
@@ -38,7 +38,7 @@ class Module extends \Module implements \SettingDefaults
             exit;
         }
     }
-    
+
     public function getSettingDefaults()
     {
         return array('allow_web_access'=>1);
