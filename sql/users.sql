72c17885b2ad2c9768a65a8b7c3fa7fc37236a35
Jeremy Booker <jeremy.booker@gmail.com>
2016-11-17 11:32:18 -0500

    Add new sql file for new users files. Use underscores because postgresql can't figure out case sensitivity. Still WIP.

--- sql/users.sql
+++ sql/users.sql
@@ -1,19 +1,19 @@
 CREATE TABLE users_new (
-    id int NOT NULL,
+    id int NOT NULL AUTO_INCREMENT,
     username varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
-    fullName varchar(255) NOT NULL,
-    isDeity int NOT NULL default 0,
-    authenticationMethodName varchar(255) NOT NULL,
-    authorizationMethodName varchar(255) NOT NULL,
-    lastLoginTime integer,
-    loginCount integer NOT NULL,
-    createdOnTime integer NOT NULL,
-    lastModifiedTime integer NOT NULL,
+    full_name varchar(255) NOT NULL,
+    is_deity int NOT NULL default 0,
+    authentication_method_name varchar(255) NOT NULL,
+    authorization_method_name varchar(255) NOT NULL,
+    last_login_time integer,
+    login_count integer NOT NULL,
+    created_on_time integer NOT NULL,
+    last_modified_time integer NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE users_passwords (
+CREATE TABLE users_local_authentication (
     user_id int NOT NULL,
     password_hash varchar(255),
     PRIMARY KEY (user_id)
