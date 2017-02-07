184257fb198b93b991c77dbde501a13b0727ac53
Matthew McNaney <mcnaney@gmail.com>
2017-02-07 10:30:46 -0500

    Renamed Variable classes to prevent restricted overlap

---
+++ src-phpws2/src/Variable/DecimalVar.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace phpws2\Variable;
+
+/**
+ * Description of Decimal
+ *
+ * @author matt
+ */
+class DecimalVar extends FloatVar
+{
+
+    /**
+     * Checks to see if value is a decimal.
+     * @param float $value
+     * @return boolean | \Error
+     */
+    protected function verifyValue($value)
+    {
+        // mysql returns strings so "1" fails
+        // the below is the work around
+        $value = $value + 0.0;
+        if (!is_float($value)) {
+            throw new \Exception(t('Value is not a decimal. Type:' . gettype($value)));
+        }
+        return true;
+    }
+
+}
