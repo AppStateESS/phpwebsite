e52909a1e0b1564beaaa3ef0aed938a6c2e7d61c
Matthew McNaney <mcnaney@gmail.com>
2017-02-14 13:53:41 -0500

    Added ability to prevent rehashing of value.

--- src-phpws2/src/Variable/Password.php
+++ src-phpws2/src/Variable/Password.php
@@ -12,21 +12,39 @@ namespace phpws2\Variable;
 class Password extends StringVar {
 
     private $salt;
+    
+    /**
+     * If set to true, the set password will be hashed. Otherwise, it will
+     * be returned as set. This is helpful when a password is not updated
+     * and you don't want to hash the hash.
+     * @var boolean
+     */
+    private $hash_result = true;
 
     public function __construct($value = null, $varname = null, $salt=null)
     {
         parent::__construct($value, $varname);
         $this->salt = $salt;
     }
-
+    
     public function __toString()
     {
         return $this->get();
     }
 
+    
+    public function setHashResult($val)
+    {
+        $this->hash_result = (bool) $val;
+    }
+    
     public function get()
     {
-        return hash('sha256', $this->salt . $this->value);
+        if ($this->hash_result) {
+            return hash('sha256', $this->salt . $this->value);
+        } else {
+            return $this->value;
+        }
     }
 
     public function setSalt($salt)
