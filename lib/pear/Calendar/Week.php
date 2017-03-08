8d046e69c8d9adeec209a9f6e34078fe58cd681c
Matthew McNaney <mcnaney@gmail.com>
2017-03-08 14:42:04 -0500

    Fixed php 7 issues

--- lib/pear/Calendar/Week.php
+++ lib/pear/Calendar/Week.php
@@ -135,10 +135,10 @@ class Calendar_Week extends Calendar
      *
      * @access public
      */
-    function Calendar_Week($y, $m, $d, $firstDay = null)
+    function __construct($y, $m, $d, $firstDay = null)
     {
         include_once CALENDAR_ROOT.'Table/Helper.php';
-        Calendar::Calendar($y, $m, $d);
+        parent::__construct($y, $m, $d);
         $this->firstDay    = $this->defineFirstDayOfWeek($firstDay);
         $this->tableHelper = new Calendar_Table_Helper($this, $this->firstDay);
         $this->thisWeek    = $this->tableHelper->getWeekStart($y, $m, $d, $this->firstDay);
