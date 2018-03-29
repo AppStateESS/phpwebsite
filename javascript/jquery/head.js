043ea94062d97fb9a26ea19697a63af76aebb60c
Matthew McNaney <mcnaney@gmail.com>
2018-03-29 10:08:54 -0400

    Changed jquery to only include the 2.1.0 library if the jQuery class...
    is unknown.

--- javascript/jquery/head.js
+++ javascript/jquery/head.js
@@ -1,1 +1,4 @@
-<script type="text/javascript" src="{source_http}javascript/jquery/jquery-2.1.0.min.js"></script>
+<script type="text/javascript">
+  window.jQuery || document.write('<script src="{source_http}javascript/jquery/jquery-2.1.0.min.js"></scr'+'ipt>')
+</script>
+
