f8387874aa70e5bc433e254a93bce56838b47c9e
Matthew McNaney <mcnaney@gmail.com>
2018-04-04 11:46:35 -0400

    Updated edit icon

--- mod/pagesmith/inc/close.php
+++ mod/pagesmith/inc/close.php
@@ -15,7 +15,7 @@ if (Current_User::allow('pagesmith', 'edit_page')) {
         unset($vars['tpl']);
         unset($vars['pid']);
         MiniAdmin::add('pagesmith',
-                \phpws\PHPWS_Text::secureLink('<i class="fas fa-pencil-alt-square-o"></i> ' . dgettext('pagesmith',
+                \phpws\PHPWS_Text::secureLink('<i class="fas fa-pencil-alt"></i> ' . dgettext('pagesmith',
                                 'Edit current page'), 'pagesmith', $vars));
     }
 }
