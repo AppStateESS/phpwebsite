604eed835e3999ba324937de1eb11bd95e441202
Matthew McNaney <mcnaney@gmail.com>
2018-04-13 12:42:50 -0400

    Added some code to prevent duplicate inclusion

--- javascript/jquery_ui/head.js
+++ javascript/jquery_ui/head.js
@@ -1,2 +1,5 @@
 <link type="text/css" href="{source_http}javascript/jquery_ui/jquery-ui.min.css" rel="stylesheet" />
-<script type="text/javascript" src="{source_http}javascript/jquery_ui/jquery-ui.min.js"></script>
+
+<script type="text/javascript">
+  jQuery.ui || document.write('<script type="text/javascript" src="{source_http}javascript/jquery_ui/jquery-ui.min.js"></scr'+ 'ipt>')
+</script>
