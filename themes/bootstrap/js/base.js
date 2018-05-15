0365436968a8c5f25439bb1d98221813f5f9d709
Matthew McNaney <mcnaney@gmail.com>
2018-05-15 09:12:22 -0400

    Revert "Updated default theme"
    This reverts commit e5d25fb5890d9337fcfbdc280e22077f454e77b9.

--- themes/bootstrap/js/base.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-/* global $, jQuery */
-import 'bootstrap'
-import '@fortawesome/fontawesome'
-import '@fortawesome/fontawesome-free-solid'
-import '@fortawesome/fontawesome-free-regular'
-import '@fortawesome/fontawesome-free-brands'
-jQuery.fn.load = function(callback){ $(window).on("load", callback) }
