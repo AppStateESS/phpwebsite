59299f10460afd199071a16520229e8137700754
Matthew McNaney <mcnaney@gmail.com>
2018-06-14 09:00:57 -0400

    Added style sheet

--- mod/rss/inc/runtime.php
+++ mod/rss/inc/runtime.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * @version $Id$
  * @author Matthew McNaney <mcnaney at gmail dot com>
  */
-
 \phpws\PHPWS_Core::initModClass('rss', 'RSS.php');
 
 if (!isset($_REQUEST['module'])) {
+    \Layout::addStyle('rss');
     RSS::showFeeds();
 }
--- mod/rss/index.php
+++ mod/rss/index.php
@@ -4,13 +4,14 @@
  * @author Matthew McNaney <mcnaney at gmail dot com>
  * @version $Id$
  */
-
 if (!defined('PHPWS_SOURCE_DIR')) {
     include '../../core/conf/404.html';
     exit();
 }
 
-if ( ( isset($_REQUEST['command']) || isset($_REQUEST['tab']) ) && Current_User::allow('rss')) {
+if (isset($_GET['feed'])) {
+    Layout::add(RSS::viewFeed($_GET['feed']));
+} elseif (( isset($_REQUEST['command']) || isset($_REQUEST['tab']) ) && Current_User::allow('rss')) {
     \phpws\PHPWS_Core::initModClass('rss', 'Admin.php');
     RSS_Admin::main();
 } elseif (isset($_GET['mod_title'])) {
